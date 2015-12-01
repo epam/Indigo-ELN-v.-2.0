8be06321e208ca1241737e17b45b6da73ee0db38
Vladislav Alekseev <vladislav_alekseev@epam.com>
2015-12-01 18:31:13 +0300

    Initial project structure

---
+++ src/main/java/com/epam/indigoeln/Application.java
@@ -0,0 +1,14 @@
+package com.epam.indigoeln;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+}
