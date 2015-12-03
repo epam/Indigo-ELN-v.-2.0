f33203520cb7bc5e2f59c6d021f17bb0aeeefebf
Vladislav Alekseev <vldrus@gmail.com>
2015-12-03 23:19:10 +0300

    Make Bingo Service thread-safe

--- BingoService/src/main/java/com/epam/indigoeln/bingoservice/repository/BingoRepository.java
+++ BingoService/src/main/java/com/epam/indigoeln/bingoservice/repository/BingoRepository.java
@@ -6,53 +6,80 @@ import com.epam.indigo.IndigoObject;
 
 public abstract class BingoRepository {
 
+    private final Object lock = new Object();
+
     protected Bingo database;
 
     public IndigoObject getById(Integer id) {
-        return database.getRecordById(id);
+        synchronized (lock) {
+            return database.getRecordById(id);
+        }
     }
 
     public Integer insert(IndigoObject record) {
-        return database.insert(record);
+        synchronized (lock) {
+            return database.insert(record);
+        }
     }
 
-    public Integer insert(IndigoObject record, Integer id) {
-        return database.insert(record, id);
+    public void update(IndigoObject record, Integer id) {
+        synchronized (lock) {
+            database.delete(id);
+            database.insert(record, id);
+        }
     }
 
     public void delete(Integer id) {
-        database.delete(id);
+        synchronized (lock) {
+            database.delete(id);
+        }
     }
 
     public BingoObject searchExact(IndigoObject query) {
-        return database.searchExact(query);
+        synchronized (lock) {
+            return database.searchExact(query);
+        }
     }
 
     public BingoObject searchExact(IndigoObject query, String options) {
-        return database.searchExact(query, options);
+        synchronized (lock) {
+            return database.searchExact(query, options);
+        }
     }
 
     public BingoObject searchSub(IndigoObject query) {
-        return database.searchSub(query);
+        synchronized (lock) {
+            return database.searchSub(query);
+        }
     }
 
     public BingoObject searchSub(IndigoObject query, String options) {
-        return database.searchSub(query, options);
+        synchronized (lock) {
+            return database.searchSub(query, options);
+        }
     }
 
     public BingoObject searchSim(IndigoObject query, Float min, Float max) {
-        return database.searchSim(query, min, max);
+        synchronized (lock) {
+            return database.searchSim(query, min, max);
+        }
     }
 
     public BingoObject searchSim(IndigoObject query, Float min, Float max, String metric) {
-        return database.searchSim(query, min, max, metric);
+        synchronized (lock) {
+            return database.searchSim(query, min, max, metric);
+        }
     }
 
     public BingoObject searchMolFormula(String query) {
-        return database.searchMolFormula(query);
+        synchronized (lock) {
+            return database.searchMolFormula(query);
+        }
     }
 
     public BingoObject searchMolFormula(String query, String options) {
-        return database.searchMolFormula(query, options);
+        synchronized (lock) {
+            return database.searchMolFormula(query, options);
+        }
     }
 }
--- BingoService/src/main/java/com/epam/indigoeln/bingoservice/service/BingoService.java
+++ BingoService/src/main/java/com/epam/indigoeln/bingoservice/service/BingoService.java
@@ -26,8 +26,7 @@ public class BingoService {
     }
 
     public void updateMolecule(Integer id, String molecule) {
-        moleculeRepository.delete(id);
-        moleculeRepository.insert(indigo.loadMolecule(molecule), id);
+        moleculeRepository.update(indigo.loadMolecule(molecule), id);
     }
 
     public void deleteMolecule(Integer id) {
@@ -43,8 +42,7 @@ public class BingoService {
     }
 
     public void updateReaction(Integer id, String reaction) {
-        reactionRepository.delete(id);
-        reactionRepository.insert(indigo.loadReaction(reaction), id);
+        reactionRepository.update(indigo.loadReaction(reaction), id);
     }
 
     public void deleteReaction(Integer id) {
