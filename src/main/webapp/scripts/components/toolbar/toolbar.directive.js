e82b3fb9875d6e7ddf625756cf7ada4696986974
Elena Polyakova <elena_polyakova@epam.com>
2015-12-26 00:04:45 +0300

    JSHint refactoring

--- src/main/webapp/scripts/components/toolbar/toolbar.directive.js
+++ src/main/webapp/scripts/components/toolbar/toolbar.directive.js
@@ -1,36 +1,37 @@
-(function(){
+(function () {
+    'use strict';
 
-angular.module('indigoeln').directive('toolbar', toolbar);
+    angular.module('indigoeln').directive('toolbar', toolbar);
 
-toolbar.$inject = ['$uibModal', '$rootScope'];
+    toolbar.$inject = ['$uibModal', '$rootScope'];
 
-function toolbar($uibModal, $rootScope) {
-    return {
-    	scope : {},
-        restrict : 'E',
-        templateUrl : 'scripts/components/toolbar/toolbar.html',
-        link : function(scope, elem, attrs) {
-            scope.newExperiment = newExperiment;
-            scope.newNotebook = newNotebook;
-            
-            function newExperiment(event) {
-                $rootScope.$broadcast('new-experiment', {});
-                event.preventDefault();
-            }
-            
-            function newNotebook(event) {
-                var modalInstance = $uibModal.open({
-                  animation: true,
-                  templateUrl: 'scripts/app/entities/notebook/new-notebook.html',
-                  controller: 'NewNotebookCtrl'
-                });
-                
-                modalInstance.result.then(function (notebook) {
-                    $rootScope.$broadcast('created-notebook', {notebook : notebook});
-                }, function () {
-                });                
+    function toolbar($uibModal, $rootScope) {
+        return {
+            scope: {},
+            restrict: 'E',
+            templateUrl: 'scripts/components/toolbar/toolbar.html',
+            link: function (scope, elem, attrs) {
+                scope.newExperiment = newExperiment;
+                scope.newNotebook = newNotebook;
+
+                function newExperiment(event) {
+                    $rootScope.$broadcast('new-experiment', {});
+                    event.preventDefault();
+                }
+
+                function newNotebook(event) {
+                    var modalInstance = $uibModal.open({
+                        animation: true,
+                        templateUrl: 'scripts/app/entities/notebook/new-notebook.html',
+                        controller: 'NewNotebookCtrl'
+                    });
+
+                    modalInstance.result.then(function (notebook) {
+                        $rootScope.$broadcast('created-notebook', {notebook: notebook});
+                    }, function () {
+                    });
+                }
             }
         }
     }
-}
 })();
