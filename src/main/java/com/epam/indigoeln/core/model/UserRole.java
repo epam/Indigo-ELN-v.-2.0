96a698be36d8c29b7e413d31b64e8cdb8c1a032d
Elena Polyakova <elena_polyakova@epam.com>
2016-01-08 23:39:36 +0300

    https://jirapct.epam.com/jira/browse/EPMLSOPELN-10 Create Main Page, removed stub

--- src/main/java/com/epam/indigoeln/core/model/UserRole.java
+++ src/main/java/com/epam/indigoeln/core/model/UserRole.java
@@ -39,4 +39,26 @@ public class UserRole {
     public void setRoleId(String roleId) {
         this.roleId = roleId;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UserRole)) return false;
+
+        UserRole userRole = (UserRole) o;
+
+        if (id != null ? !id.equals(userRole.id) : userRole.id != null) return false;
+        if (roleId != null ? !roleId.equals(userRole.roleId) : userRole.roleId != null) return false;
+        if (userId != null ? !userId.equals(userRole.userId) : userRole.userId != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (userId != null ? userId.hashCode() : 0);
+        result = 31 * result + (roleId != null ? roleId.hashCode() : 0);
+        return result;
+    }
 }
--- src/main/java/com/epam/indigoeln/web/controller/ExperimentController.java
+++ src/main/java/com/epam/indigoeln/web/controller/ExperimentController.java
@@ -1,7 +1,9 @@
 package com.epam.indigoeln.web.controller;
 
 import com.epam.indigoeln.core.model.Experiment;
+import com.epam.indigoeln.core.model.User;
 import com.epam.indigoeln.core.repository.experiment.ExperimentRepository;
+import com.epam.indigoeln.core.repository.user.UserRepository;
 import com.epam.indigoeln.core.service.experiment.ExperimentService;
 import com.epam.indigoeln.web.dto.ExperimentTablesDTO;
 import com.epam.indigoeln.web.model.security.CustomUserDetails;
@@ -13,6 +15,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/service")
@@ -26,19 +29,23 @@ public class ExperimentController {
     @Autowired
     private ExperimentRepository experimentRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
+
     @RequestMapping(value = "/experiments",
             method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public Collection<Experiment> getAllExperiments(@AuthenticationPrincipal CustomUserDetails userDetails) {
-        //TODO: filter by current user
-        return experimentRepository.getAllExperiments();
+        User user = userRepository.getUser(userDetails.getUserInfo().getUsername());
+        return experimentRepository.findUserExperiments(user);
     }
 
     @RequestMapping(value = "/experiments/{id}",
             method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public Experiment getExperiment(@PathVariable String id) {
-        return experimentRepository.getExperiment(id);
+        return experimentRepository.findExperiment(id);
     }
 
     @RequestMapping(value = "/experiments",
@@ -48,13 +55,26 @@ public class ExperimentController {
         return experimentRepository.saveExperiment(experiment);
     }
 
-    /* Tables with experiments on the home page, current user is not an author of all experiments in the list */
+    /* Tables with experiments on the home page, where current user is an author, witness, or signature requested */
     @RequestMapping(value = "/experiments/tables",
             method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ExperimentTablesDTO getExperimentsList(@AuthenticationPrincipal CustomUserDetails userDetails) {
-        return experimentService.getExperimentTables();
+    public ExperimentTablesDTO getExperimentTables(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        User user = userRepository.getUser(userDetails.getUserInfo().getUsername());
+        ExperimentTablesDTO dto = new ExperimentTablesDTO();
+
+        Collection<Experiment> userExperiments = experimentRepository.findUserExperiments(user);
+        Collection<Experiment> allExperiments = experimentRepository.findAllExperiments();
+
+        dto.setOpenAndCompletedExp(userExperiments.stream()
+                .filter(exp -> exp.getStatus().equals("Open") || exp.getStatus().equals("Completed") || exp.getStatus().equals("Archived"))
+                .collect(Collectors.toList()));
+        dto.setSubmittedAndSigningExp(userExperiments.stream()
+                .filter(exp -> exp.getStatus().equals("Submitted") || exp.getStatus().equals("Signing"))
+                .collect(Collectors.toList()));
+        dto.setWaitingSignatureExp(allExperiments.stream()
+                .filter(exp -> exp.getCoAuthors().contains(user) /* || exp.getAuthor().equals(user) && in template for signing indicated that author's signature is required*/)
+                .collect(Collectors.toList()));
+        return dto;
     }
-
-
 }
