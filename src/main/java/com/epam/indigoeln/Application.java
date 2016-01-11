9dfd55016d9639bba73a15496b7de9aa81c82728
Stepan Litvinov <stepan_litvinov@epam.com>
2016-01-11 18:14:21 +0300

    refactoring

--- src/main/java/com/epam/indigoeln/Application.java
+++ src/main/java/com/epam/indigoeln/Application.java
@@ -1,16 +1,31 @@
 package com.epam.indigoeln;
 
+import com.epam.indigoeln.config.Constants;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.core.env.SimpleCommandLinePropertySource;
 import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
 @EnableJms
-public class Application extends SpringBootServletInitializer {
+public class Application {
+    /**
+     * Main method, used to run the application.
+     */
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Application.class);
+        SimpleCommandLinePropertySource source = new SimpleCommandLinePropertySource(args);
+        addDefaultProfile(app, source);
+        app.run(args);
+    }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(Application.class);
+    /**
+     * If no profile has been configured, set by default the "release" profile.
+     */
+    private static void addDefaultProfile(SpringApplication app, SimpleCommandLinePropertySource source) {
+        if (!source.containsProperty("spring.profiles.active") &&
+                !System.getenv().containsKey("SPRING_PROFILES_ACTIVE")) {
+            app.setAdditionalProfiles(Constants.SPRING_PROFILE_RELEASE);
+        }
     }
 }
