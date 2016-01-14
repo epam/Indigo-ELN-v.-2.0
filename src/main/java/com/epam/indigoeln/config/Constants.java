2e3557649b645af0d340601dc9773277627f955c
Stepan Litvinov <stepan_litvinov@epam.com>
2016-01-14 12:21:07 +0300

    https://jirapct.epam.com/jira/browse/EPMLSOPELN-36 Error handling: logging

--- src/main/java/com/epam/indigoeln/config/Constants.java
+++ src/main/java/com/epam/indigoeln/config/Constants.java
@@ -4,7 +4,7 @@ package com.epam.indigoeln.config;
  * Application constants.
  */
 public final class Constants {
-    public static final String SPRING_PROFILE_RELEASE = "release";
+    public static final String SPRING_PROFILE_DEVELOPMENT = "dev";
 
     private Constants() {
     }
