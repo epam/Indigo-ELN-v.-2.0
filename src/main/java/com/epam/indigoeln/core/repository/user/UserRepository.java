d7042f6962f1a8bdf41e8b6246865ea38848ccc7
Stepan Litvinov <stepan_litvinov@epam.com>
2016-01-14 11:49:17 +0300

    example how to use spring data mongodb

--- src/main/java/com/epam/indigoeln/core/repository/user/UserRepository.java
+++ src/main/java/com/epam/indigoeln/core/repository/user/UserRepository.java
@@ -1,37 +1,12 @@
 package com.epam.indigoeln.core.repository.user;
 
+
 import com.epam.indigoeln.core.model.User;
-import org.bson.types.ObjectId;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.Collection;
-
-@Repository
-public class UserRepository {
-
-    @Autowired
-    private MongoTemplate mongoTemplate;
-
-    public void saveUser(User user) {
-        user.setName(user.getName().toUpperCase());
-        mongoTemplate.save(user);
-    }
-
-    public void deleteUser(String id) {
-        Query query = new Query(Criteria.where("_id").is(new ObjectId(id)));
-        mongoTemplate.remove(query);
-    }
-
-    public User getUser(String name) {
-        Query searchQuery = new Query(Criteria.where("name").is(name));
-        return mongoTemplate.findOne(searchQuery, User.class);
-    }
-
-    public Collection<User> getUsers() {
-        return mongoTemplate.findAll(User.class);
-    }
+/**
+ * Spring Data MongoDB repository for the User entity.
+ */
+public interface UserRepository extends MongoRepository<User, String> {
+    User findByName(String name);
 }
--- src/main/java/com/epam/indigoeln/core/repository/user/UserRoleRepository.java
+++ src/main/java/com/epam/indigoeln/core/repository/user/UserRoleRepository.java
@@ -1,39 +1,12 @@
 package com.epam.indigoeln.core.repository.user;
 
 import com.epam.indigoeln.core.model.UserRole;
-import org.bson.types.ObjectId;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Collection;
 
-@Repository
-public class UserRoleRepository {
+public interface UserRoleRepository extends MongoRepository<UserRole, String> {
+    UserRole findByUserIdAndRoleId(String userId, String roleId);
 
-    @Autowired
-    private MongoTemplate mongoTemplate;
-
-    public void saveUserRole(UserRole userRole) {
-        mongoTemplate.save(userRole);
-    }
-
-    public void deleteUserRole(String id) {
-        Query query = new Query(Criteria.where("_id").is(new ObjectId(id)));
-        mongoTemplate.remove(query, UserRole.class);
-    }
-
-    public UserRole getUserRole(String userId, String roleId) {
-        Query searchQuery = new Query(
-                Criteria.where("user_id").is(new ObjectId(userId))
-                        .andOperator(Criteria.where("role_id").is(new ObjectId(roleId))));
-        return mongoTemplate.findOne(searchQuery, UserRole.class);
-    }
-
-    public Collection<UserRole> getUserRoles(String userId) {
-        Query searchQuery = new Query(Criteria.where("user_id").is(new ObjectId(userId)));
-        return mongoTemplate.find(searchQuery, UserRole.class);
-    }
+    Collection<UserRole> findByUserId(String userId);
 }
