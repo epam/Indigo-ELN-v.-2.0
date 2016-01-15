61abbe570b9ab3eab543976c684387e33bd8a652
Sergei_Bolbin <Sergei_Bolbin@epam.com>
2016-01-15 18:40:23 +0300

    Fix sonar violation

--- src/main/java/com/epam/indigoeln/web/rest/dto/BatchDTO.java
+++ src/main/java/com/epam/indigoeln/web/rest/dto/BatchDTO.java
@@ -36,6 +36,25 @@ public class BatchDTO implements Serializable, Comparable<BatchDTO> {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BatchDTO batchDTO = (BatchDTO) o;
+
+        if (id != null ? !id.equals(batchDTO.id) : batchDTO.id != null) return false;
+        return batchNumber != null ? batchNumber.equals(batchDTO.batchNumber) : batchDTO.batchNumber == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (batchNumber != null ? batchNumber.hashCode() : 0);
+        return result;
+    }
+
+    @Override
     public String toString() {
         return "BatchDTO{" +
                 "id='" + id + '\'' +
