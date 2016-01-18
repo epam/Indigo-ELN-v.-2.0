826e1afd3a3aeeb8dbbaf66e4c60ec8029f756ab
Sergei_Bolbin <Sergei_Bolbin@epam.com>
2016-01-18 15:51:53 +0300

    Small changes

--- src/main/java/com/epam/indigoeln/web/rest/dto/BatchDTO.java
+++ src/main/java/com/epam/indigoeln/web/rest/dto/BatchDTO.java
@@ -1,18 +1,13 @@
 package com.epam.indigoeln.web.rest.dto;
 
-import com.epam.indigoeln.core.model.Batch;
-
 import java.io.Serializable;
 
-public class BatchDTO implements Serializable, Comparable<BatchDTO> {
+public class BatchDTO implements Serializable {
 
-    private static final long serialVersionUID = -7878801074903420228L;
+    private static final long serialVersionUID = 13994253258215137L;
 
     private String id;
     private String batchNumber;
-
-    public BatchDTO() {
-    }
 
     public String getId() {
         return id;
@@ -29,31 +24,7 @@ public class BatchDTO implements Serializable, Comparable<BatchDTO> {
     public void setBatchNumber(String batchNumber) {
         this.batchNumber = batchNumber;
     }
-
-    @Override
-    public int compareTo(BatchDTO obj) {
-        return obj.getBatchNumber() != null ? batchNumber.compareTo(obj.getBatchNumber()) : 1;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        BatchDTO batchDTO = (BatchDTO) o;
-
-        if (id != null ? !id.equals(batchDTO.id) : batchDTO.id != null) return false;
-        return batchNumber != null ? batchNumber.equals(batchDTO.batchNumber) : batchDTO.batchNumber == null;
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (batchNumber != null ? batchNumber.hashCode() : 0);
-        return result;
-    }
-
+    
     @Override
     public String toString() {
         return "BatchDTO{" +
