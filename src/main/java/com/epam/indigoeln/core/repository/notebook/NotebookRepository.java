d47f7ebc26921569ba0875db1417644cc20df094
Eduard_Burkov <Eduard_Burkov@epam.com>
2016-01-20 14:38:37 +0300

    EPMLSOPELN-28: ver. 1

--- src/main/java/com/epam/indigoeln/core/repository/notebook/NotebookRepository.java
+++ src/main/java/com/epam/indigoeln/core/repository/notebook/NotebookRepository.java
@@ -3,5 +3,9 @@ package com.epam.indigoeln.core.repository.notebook;
 import com.epam.indigoeln.core.model.Notebook;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface NotebookRepository extends MongoRepository<Notebook, String> {
+
+    Optional<Notebook> findOneByName(String name);
 }
