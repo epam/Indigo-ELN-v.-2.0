af01f61aa3749f0f704e35bd2d6c60256a4e02b9
Stepan Litvinov <stepan_litvinov@epam.com>
2016-01-22 14:40:09 +0300

    csrf protection with remember me implementation

--- IndigoELN/src/main/java/com/epam/indigoeln/config/security/WebSecurityConfig.java
+++ IndigoELN/src/main/java/com/epam/indigoeln/config/security/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.epam.indigoeln.config.security;
 
+import com.epam.indigoeln.core.security.*;
+import com.epam.indigoeln.web.rest.filter.CsrfCookieGeneratorFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,12 +16,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.data.repository.query.SecurityEvaluationContextExtension;
-
-import com.epam.indigoeln.core.security.AjaxAuthenticationFailureHandler;
-import com.epam.indigoeln.core.security.AjaxAuthenticationSuccessHandler;
-import com.epam.indigoeln.core.security.AjaxLogoutSuccessHandler;
-import com.epam.indigoeln.core.security.AuthoritiesConstants;
-import com.epam.indigoeln.core.security.Http401UnauthorizedEntryPoint;
+import org.springframework.security.web.authentication.RememberMeServices;
+import org.springframework.security.web.csrf.CsrfFilter;
 
 
 @Configuration
@@ -44,6 +42,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserDetailsService userDetailsService;
+
+    @Autowired
+    private RememberMeServices rememberMeServices;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -70,9 +71,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable() //todo: enable later
+                .csrf()
+                .and()
+                .addFilterAfter(new CsrfCookieGeneratorFilter(), CsrfFilter.class)
                 .exceptionHandling()
+                .accessDeniedHandler(new CustomAccessDeniedHandler())
                 .authenticationEntryPoint(authenticationEntryPoint)
+                .and()
+                .rememberMe()
+                .rememberMeServices(rememberMeServices)
+                .rememberMeParameter("remember-me")
+                .key(env.getProperty("indigoeln.security.rememberme.key"))
                 .and()
                 .formLogin()
                 .loginProcessingUrl("/api/authentication")
@@ -85,7 +94,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout()
                 .logoutUrl("/api/logout")
                 .logoutSuccessHandler(ajaxLogoutSuccessHandler)
-                .deleteCookies("JSESSIONID")
+                .deleteCookies("JSESSIONID", "CSRF-TOKEN")
                 .permitAll()
                 .and()
                 .headers()
