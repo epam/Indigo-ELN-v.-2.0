e4c4b037e0f0f0f15d4c5540f24636d5d11a8479
Stepan Litvinov <stepan_litvinov@epam.com>
2016-01-26 17:33:47 +0300

    refactoring

--- src/main/webapp/scripts/app/entities/template/template-dialog.controller.js
+++ src/main/webapp/scripts/app/entities/template/template-dialog.controller.js
@@ -1,36 +1,30 @@
 'use strict';
 
 angular.module('indigoeln').controller('TemplateDialogController',
-    ['$scope', '$stateParams', '$uibModalInstance', 'entity', 'Template',
-        function ($scope, $stateParams, $uibModalInstance, entity, Template) {
+    function ($scope, $stateParams, entity, Template, $state) {
 
-            $scope.template = entity;
-            $scope.load = function (id) {
-                Template.get({id: id}, function (result) {
-                    $scope.template = result;
-                });
-            };
+        $scope.template = entity;
+        $scope.load = function (id) {
+            Template.get({id: id}, function (result) {
+                $scope.template = result;
+            });
+        };
 
-            var onSaveSuccess = function (result) {
-                $scope.$emit('indigoeln:templateUpdate', result);
-                $uibModalInstance.close(result);
-                $scope.isSaving = false;
-            };
+        var onSaveSuccess = function (result) {
+            $state.go('template');
+            $scope.isSaving = false;
+        };
 
-            var onSaveError = function (result) {
-                $scope.isSaving = false;
-            };
+        var onSaveError = function (result) {
+            $scope.isSaving = false;
+        };
 
-            $scope.save = function () {
-                $scope.isSaving = true;
-                if ($scope.template.id != null) {
-                    Template.update($scope.template, onSaveSuccess, onSaveError);
-                } else {
-                    Template.save($scope.template, onSaveSuccess, onSaveError);
-                }
-            };
-
-            $scope.clear = function () {
-                $uibModalInstance.dismiss('cancel');
-            };
-        }]);
+        $scope.save = function () {
+            $scope.isSaving = true;
+            if ($scope.template.id != null) {
+                Template.update($scope.template, onSaveSuccess, onSaveError);
+            } else {
+                Template.save($scope.template, onSaveSuccess, onSaveError);
+            }
+        };
+    });
