9fa5994b8dfd6946a25ffea812288cf29f506c18
Stepan Litvinov <stepan_litvinov@epam.com>
2016-01-28 19:54:51 +0300

    https://jirapct.epam.com/jira/browse/EPMLSOPELN-52 ui for template

--- IndigoELN/src/main/webapp/scripts/app/entities/template/template-detail.controller.js
+++ IndigoELN/src/main/webapp/scripts/app/entities/template/template-detail.controller.js
@@ -1,16 +1,17 @@
 'use strict';
 
 angular.module('indigoeln')
-    .controller('TemplateDetailController', function ($scope, $rootScope, $stateParams, entity, Template) {
+    .controller('TemplateDetailController', function ($scope, $rootScope, $stateParams, entity, Template, $builder) {
         $scope.template = entity;
+        $builder.forms['default'].length = 0;
+        _.each($scope.template.templateContent, function (item) {
+            if (item) {
+                $builder.addFormObject("default", item);
+            }
+        });
         $scope.load = function (id) {
             Template.get({id: id}, function (result) {
                 $scope.template = result;
             });
         };
-        //var unsubscribe = $rootScope.$on('indigoeln:templateUpdate', function (event, result) {
-        //    $scope.template = result;
-        //});
-        //$scope.$on('$destroy', unsubscribe);
-
     });
