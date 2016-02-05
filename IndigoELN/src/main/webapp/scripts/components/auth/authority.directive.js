fca490ff4783734bd3efa05ae0fe021f2cf97144
Elena Polyakova <elena_polyakova@epam.com>
2016-02-05 15:57:02 +0300

    https://jirapct.epam.com/jira/browse/EPMLSOPELN-67 Adjust Tree to show all Projects for all users

--- IndigoELN/src/main/webapp/scripts/components/auth/authority.directive.js
+++ IndigoELN/src/main/webapp/scripts/components/auth/authority.directive.js
@@ -64,36 +64,4 @@ angular.module('indigoeln')
                 }
             }
         };
-    }]).directive('hasAnyAuthorityExcept', ['Principal', function (Principal) {
-    return {
-        restrict: 'A',
-        link: function (scope, element, attrs) {
-            var setVisible = function () {
-                    element.removeClass('hidden');
-                },
-                setHidden = function () {
-                    element.addClass('hidden');
-                },
-                defineVisibility = function (reset) {
-
-                    if (reset) {
-                        setVisible();
-                    }
-
-                    Principal.hasAnyAuthorityExcept(authority)
-                        .then(function (result) {
-                            if (result) {
-                                setVisible();
-                            } else {
-                                setHidden();
-                            }
-                        });
-                },
-                authority = attrs.hasAnyAuthorityExcept.replace(/\s+/g, '');
-
-            if (authority.length > 0) {
-                defineVisibility(true);
-            }
-        }
-    };
     }]);
