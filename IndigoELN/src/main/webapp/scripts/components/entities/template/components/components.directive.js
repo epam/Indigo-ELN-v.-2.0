65274a93ae4bb8a7535941c6bffcacd3d9cf584c
Stepan Litvinov <stepan_litvinov@epam.com>
2016-02-10 16:14:19 +0300

    https://jirapct.epam.com/jira/browse/EPMLSOPELN-24 tabs

--- IndigoELN/src/main/webapp/scripts/components/entities/template/components/components.directive.js
+++ IndigoELN/src/main/webapp/scripts/components/entities/template/components/components.directive.js
@@ -5,10 +5,10 @@
 
 angular.module('indigoeln')
     .constant('Components', [
-        {name: 'Concept Details', id: "concept-details", desc: "Fake description"},
-        {name: 'Product Batch Details', id: "product-batch-details", desc: "Fake description"}
+        {name: 'Concept Details', id: "concept-details", tab: 'Fake tab 1', desc: "Fake description"},
+        {name: 'Product Batch Details', id: "product-batch-details", tab: 'Fake tab 2', desc: "Fake description"}
     ])
-    .directive('myComponent', function (Components) {
+    .directive('myComponent', function () {
         return {
             restrict: 'A',
             replace: true,
@@ -21,4 +21,22 @@ angular.module('indigoeln')
             '<div ng-switch-when="product-batch-summary"><product-batch-summary /></div>' +
             '</div>'
         }
-    });
+    }).directive('myComponents', function () {
+    return {
+        restrict: 'E',
+        replace: true,
+        scope: {
+            myTemplate: '=',
+            myDisabled: '=',
+            myModel: '='
+        },
+        link: function ($scope) {
+            $scope.model = $scope.myModel; //for capability
+        },
+        template: '<fieldset ng-disabled="myDisabled"><uib-tabset justified="true">' +
+        '<uib-tab heading="{{key}}" ng-repeat="(key,value) in myTemplate | groupBy: \'tab\'">' +
+        '<div ng-repeat="component in value" my-component={{component.id}}></div>' +
+        '</uib-tab>' +
+        '</uib-tabset></fieldset>'
+    }
+});
