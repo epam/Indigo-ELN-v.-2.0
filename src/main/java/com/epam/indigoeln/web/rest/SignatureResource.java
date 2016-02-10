1ddafdda7d076117964e11f53a0ba199fb76fe8e
Evgeny Romanov <evgeny_romanov@epam.com>
2016-02-10 14:04:05 +0300

    EPMLSOPELN-76: Signature repository, service and resource. All methods from old ELN.

--- src/main/java/com/epam/indigoeln/web/rest/SignatureResource.java
+++ src/main/java/com/epam/indigoeln/web/rest/SignatureResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/signature")
@@ -21,6 +22,23 @@ public class SignatureResource {
     @Autowired
     private SignatureService signatureService;
 
+    @RequestMapping(value = "/reason", method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getReasons() {
+        return ResponseEntity.ok(signatureService.getReasons());
+    }
+
+    @RequestMapping(value = "/status", method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getStatuses() {
+        return ResponseEntity.ok(signatureService.getStatuses());
+    }
+
+    @RequestMapping(value = "/status/final", method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getFinalStatus() {
+        return ResponseEntity.ok(signatureService.getFinalStatus());
+    }
 
     @RequestMapping(value = "/template", method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE)
@@ -33,6 +51,30 @@ public class SignatureResource {
     public ResponseEntity<String> uploadDocument(@RequestParam("templateId") String templateId,
                                                  @RequestParam("file") MultipartFile file) throws IOException {
         return ResponseEntity.ok(signatureService.uploadDocument(templateId, file.getOriginalFilename(), file.getBytes()));
+    }
+
+    @RequestMapping(value = "/document/info", method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getDocumentInfo(String documentId) {
+        return ResponseEntity.ok(signatureService.getDocumentInfo(documentId));
+    }
+
+    @RequestMapping(value = "/document/info", method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getDocumentsInfo(List<String> documentIds) {
+        return ResponseEntity.ok(signatureService.getDocumentsInfo(documentIds));
+    }
+
+    @RequestMapping(value = "/document", method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getDocuments() {
+        return ResponseEntity.ok(signatureService.getDocuments());
+    }
+
+    @RequestMapping(value = "/document/content", method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<byte[]> downloadDocument(String documentId) {
+        return ResponseEntity.ok(signatureService.downloadDocument(documentId));
     }
 
 }
