a7deb4258d930868e98bfa6560a6e595f21c71f6
Sergei_Bolbin <Sergei_Bolbin@epam.com>
2016-02-17 10:19:45 +0300

    Merge branch 'dev' of https://git.epam.com/epm-lsop/indigo-eln into SEQUENCE_AMENDMENT
    # Conflicts:
    #	IndigoELN/src/main/java/com/epam/indigoeln/core/model/ExperimentShort.java
    #	IndigoELN/src/main/java/com/epam/indigoeln/core/repository/experiment/ExperimentRepository.java
    #	IndigoELN/src/main/java/com/epam/indigoeln/core/service/experiment/ExperimentService.java
    #	IndigoELN/src/main/java/com/epam/indigoeln/core/service/project/ProjectService.java
    #	IndigoELN/src/main/java/com/epam/indigoeln/core/service/template/TemplateService.java
    #	IndigoELN/src/main/java/com/epam/indigoeln/web/rest/dto/BasicDTO.java
    #	IndigoELN/src/main/java/com/epam/indigoeln/web/rest/dto/ExperimentDTO.java
    #	IndigoELN/src/main/webapp/scripts/app/entities/experiment/experiment.js

--- IndigoELN/src/main/java/com/epam/indigoeln/web/rest/util/CustomDtoMapper.java
+++ IndigoELN/src/main/java/com/epam/indigoeln/web/rest/util/CustomDtoMapper.java
@@ -1,7 +1,20 @@
 package com.epam.indigoeln.web.rest.util;
 
-import com.epam.indigoeln.core.model.*;
-import com.epam.indigoeln.web.rest.dto.*;
+import com.epam.indigoeln.core.model.Component;
+import com.epam.indigoeln.core.model.Experiment;
+import com.epam.indigoeln.core.model.Notebook;
+import com.epam.indigoeln.core.model.Project;
+import com.epam.indigoeln.core.model.Role;
+import com.epam.indigoeln.core.model.Template;
+import com.epam.indigoeln.core.model.User;
+import com.epam.indigoeln.web.rest.dto.ComponentDTO;
+import com.epam.indigoeln.web.rest.dto.ExperimentDTO;
+import com.epam.indigoeln.web.rest.dto.ManagedUserDTO;
+import com.epam.indigoeln.web.rest.dto.NotebookDTO;
+import com.epam.indigoeln.web.rest.dto.ProjectDTO;
+import com.epam.indigoeln.web.rest.dto.RoleDTO;
+import com.epam.indigoeln.web.rest.dto.TemplateDTO;
+import com.epam.indigoeln.web.rest.dto.UserDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
