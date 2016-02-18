16ec9997fe88163e176917912f50a24cf4a92835
Stepan Litvinov <stepan_litvinov@epam.com>
2016-02-18 16:45:24 +0300

    fix validation for components

--- src/main/webapp/scripts/components/form/form.directive.js
+++ src/main/webapp/scripts/components/form/form.directive.js
@@ -11,19 +11,20 @@ angular.module('indigoeln')
                 }
             },
             showValidation: function ($formGroup, scope) {
-                if (scope.myValidationObj) {
-                    var $inputs = $formGroup.find('input[ng-model],textarea[ng-model],select[ng-model]');
-                    if ($inputs.length > 0) {
-                        $inputs.each(function () {
-                            var $input = $(this);
-                            scope.$watch(function () {
-                                return scope.myValidationObj.$invalid && scope.myValidationObj.$dirty
-                            }, function (isInvalid) {
-                                $formGroup.toggleClass('has-error', isInvalid);
+                $timeout(function () {
+                    if (scope.myValidationObj) {
+                        var $inputs = $formGroup.find('input[ng-model],textarea[ng-model],select[ng-model]');
+                        if ($inputs.length > 0) {
+                            $inputs.each(function () {
+                                scope.$watch(function () {
+                                    return scope.myValidationObj.$invalid && scope.myValidationObj.$dirty
+                                }, function (isInvalid) {
+                                    $formGroup.toggleClass('has-error', isInvalid);
+                                });
                             });
-                        });
+                        }
                     }
-                }
+                })
             }
         }
     }).directive('myInput', function (formUtils) {
