e82c7b6e224a6019870cd690cea5eb9872c503af
Sergei_Bolbin <Sergei_Bolbin@epam.com>
2016-02-18 14:57:43 +0300

    small fix

--- src/main/webapp/scripts/components/entities/notebook/notebook.service.js
+++ src/main/webapp/scripts/components/entities/notebook/notebook.service.js
@@ -4,8 +4,7 @@ angular
     .module('indigoeln')
     .factory('Notebook', function ($resource) {
         return $resource('api/projects/:projectId/notebooks/:id', {
-            projectId: '@projectId',
-            id: '@id'
+            projectId: '@projectId'
         }, {
             'query': {method: 'GET', isArray: true},
             'get': {
