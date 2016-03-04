5998fe74f9db025f27de93f5764ddcb32e2993fe
Elena Polyakova <elena_polyakova@epam.com>
2016-03-04 18:11:47 +0300

    https://jirapct.epam.com/jira/browse/EPMLSOPELN-71 service for printing - use phantomjs and itext for pdf creation

---
+++ IndigoELN/src/main/java/com/epam/indigoeln/core/service/util/TempFileUtil.java
@@ -0,0 +1,49 @@
+package com.epam.indigoeln.core.service.util;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+
+public class TempFileUtil {
+
+    private static final Logger log = LoggerFactory.getLogger(TempFileUtil.class);
+
+    public static final String TEMP_FILE_PREFIX = "eln-";
+
+
+    private static String getFileNameWithPrefix(String fileName) {
+        if (StringUtils.startsWith(fileName, TEMP_FILE_PREFIX)) {
+            return fileName;
+        }
+        return String.format("%s%s", TEMP_FILE_PREFIX, fileName);
+    }
+
+    public static File saveToTempDirectory(byte[] fileData, String fileName) {
+        String prefixedFileName = getFileNameWithPrefix(fileName);
+        File tempDir = FileUtils.getTempDirectory();
+        File newFile = new File(tempDir, prefixedFileName);
+
+        try {
+            FileUtils.copyInputStreamToFile(new ByteArrayInputStream(fileData), newFile);
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
+        return newFile;
+    }
+
+    public static File getFromTempDirectory(String fileName) {
+        String prefixedFileName = getFileNameWithPrefix(fileName);
+        File tempDir = FileUtils.getTempDirectory();
+        File file = new File(tempDir, prefixedFileName);
+
+        if (!file.exists()) {
+            return null;
+        }
+        return file;
+    }
+}
