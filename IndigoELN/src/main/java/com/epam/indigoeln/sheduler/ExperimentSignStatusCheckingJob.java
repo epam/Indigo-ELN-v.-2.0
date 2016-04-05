98ba49aaa8aad5569fabfe025546a7b2ad31b614
Evgeny Romanov <evgeny_romanov@epam.com>
2016-04-05 13:25:06 +0300

    EPMLSOPELN-183: Client side experiment status update via websocket.

--- IndigoELN/src/main/java/com/epam/indigoeln/sheduler/ExperimentSignStatusCheckingJob.java
+++ IndigoELN/src/main/java/com/epam/indigoeln/sheduler/ExperimentSignStatusCheckingJob.java
@@ -1,16 +1,21 @@
 package com.epam.indigoeln.sheduler;
 
+import com.epam.indigoeln.core.model.Experiment;
 import com.epam.indigoeln.core.model.ExperimentStatus;
-import com.epam.indigoeln.core.service.experiment.ExperimentService;
-import com.epam.indigoeln.web.rest.dto.ExperimentDTO;
+import com.epam.indigoeln.core.repository.experiment.ExperimentRepository;
+import com.epam.indigoeln.core.service.signature.SignatureService;
+import org.apache.commons.collections.MapUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
-import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Component
 public class ExperimentSignStatusCheckingJob {
@@ -20,20 +25,35 @@ public class ExperimentSignStatusCheckingJob {
     private static final long DELAY = 1000 * 60L; // 1 minute
 
     @Autowired
-    private ExperimentService experimentService;
+    private SignatureService signatureService;
+
+    @Autowired
+    private ExperimentRepository experimentRepository;
+
+    @Autowired
+    private SimpMessagingTemplate template;
 
     @Scheduled(fixedDelay = DELAY)
     public void execute() {
         LOGGER.debug("Experiment sign status checking job started");
-        final Collection<ExperimentDTO> experiments = experimentService.getExperimentsByStatuses(Arrays.asList(ExperimentStatus.SUBMITTED,
+        final List<Experiment> experiments = experimentRepository.findByStatuses(Arrays.asList(ExperimentStatus.SUBMITTED,
                 ExperimentStatus.SINGING));
-        experiments.forEach(e -> {
+        Map<String, ExperimentStatus> updatedExperimentsStatuses = new HashMap<>();
+        experiments.forEach(experiment -> {
             try {
-                experimentService.checkExperimentStatus(e);
+                final ExperimentStatus origStatus = experiment.getStatus();
+                final ExperimentStatus newStatus = signatureService.checkExperimentStatus(experiment);
+                if (!origStatus.equals(newStatus)) {
+                    updatedExperimentsStatuses.put(experiment.getId(), newStatus);
+                }
             } catch (Exception ex) {
                 LOGGER.error("Error executing experiment sign status checking job!", ex);
             }
         });
+
+        if (!MapUtils.isEmpty(updatedExperimentsStatuses)) {
+            template.convertAndSend("/topic/experiment_status", updatedExperimentsStatuses);
+        }
     }
 
 }
