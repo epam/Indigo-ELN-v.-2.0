fd8ae325ffe677897149509a56714c97d274f66f
Sergei_Bolbin <Sergei_Bolbin@epam.com>
2016-04-12 11:38:39 +0300

    [EPMLSOPELN-186] Initial implementaion. Some fixes for simple search cases

--- IndigoELN/src/main/java/com/epam/indigoeln/core/repository/search/SearchComponentsRepository.java
+++ IndigoELN/src/main/java/com/epam/indigoeln/core/repository/search/SearchComponentsRepository.java
@@ -5,15 +5,15 @@ import com.epam.indigoeln.core.service.bingo.BingoService;
 import com.epam.indigoeln.core.util.BatchComponentUtil;
 import com.epam.indigoeln.web.rest.dto.search.ProductBatchDetailsDTO;
 import com.epam.indigoeln.web.rest.dto.search.request.BatchSearchRequest;
-import com.epam.indigoeln.web.rest.dto.search.request.BatchSearchStructure;
 import com.google.common.collect.ImmutableMap;
+import com.mongodb.DBObject;
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.Aggregation;
 import org.springframework.data.mongodb.core.aggregation.AggregationOperation;
 import org.springframework.data.mongodb.core.aggregation.AggregationResults;
-import org.springframework.data.mongodb.core.aggregation.TypedAggregation;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Repository;
 
@@ -40,41 +40,45 @@ public class SearchComponentsRepository {
     private MongoTemplate mongoTemplate;
 
     public List<ProductBatchDetailsDTO> findBatches(BatchSearchRequest searchRequest) {
-        TypedAggregation<Component> aggregation = buildAggregation(searchRequest);
-        AggregationResults<Component> aggregate = mongoTemplate.aggregate(aggregation, Component.class);
-        return aggregate.getMappedResults().stream().map(SearchComponentsRepository::convertResult).collect(Collectors.toList());
+        Aggregation aggregation = buildAggregation(searchRequest);
+        AggregationResults<DBObject> aggregate = mongoTemplate.aggregate(aggregation, Component.class, DBObject.class);
+        return aggregate.getMappedResults().stream().map(
+                SearchComponentsRepository::convertResult
+        ).collect(Collectors.toList());
     }
 
-    private TypedAggregation<Component> buildAggregation(BatchSearchRequest searchRequest) {
+    private Aggregation buildAggregation(BatchSearchRequest searchRequest) {
         List<AggregationOperation> aggregationOperations = new ArrayList<>();
         aggregationOperations.add(Aggregation.match(Criteria.where("name").is("productBatchSummary"))); // filter by type
-        aggregationOperations.add(Aggregation.unwind("$content.batches")); // unwind (flat array of batches)
+        aggregationOperations.add(Aggregation.project("content"));
+        aggregationOperations.add(Aggregation.unwind("content.batches")); // unwind (flat array of batches)
 
-        if(searchRequest.getSearchQuery().isPresent()){ //if structure parameters required
-            BatchSearchStructure structureDTO = searchRequest.getStructure().get();
-            List<Integer> bingoIds;
-            if(structureDTO.getFormula().isPresent()) {
-                bingoIds = searchByBingoDb(structureDTO.getFormula().get(), CHEMISTRY_SEARCH_MOLFORMULA, Collections.emptyMap());
-            } else {
-                Map options = ImmutableMap.of("min", structureDTO.getSimilarity());
-                bingoIds = searchByBingoDb(structureDTO.getMolfile().orElse(""), structureDTO.getSearchMode(), options);
-            }
+        Optional.ofNullable(searchRequest.getStructure()).ifPresent(
+           structure -> {
+               List<Integer> bingoIds;
+               if(structure.getFormula()!= null) {
+                   bingoIds = searchByBingoDb(structure.getFormula(), CHEMISTRY_SEARCH_MOLFORMULA, Collections.emptyMap());
+               } else {
+                   Map options = ImmutableMap.of("min", structure.getSimilarity());
+                   bingoIds = searchByBingoDb(structure.getMolfile(), structure.getSearchMode(), options);
+               }
 
-            if(!bingoIds.isEmpty()) {
-                Criteria structureCriteria = Criteria.where("content.batches.structure.structureId").in(bingoIds);
-                aggregationOperations.add(Aggregation.match(structureCriteria));
-            }
-        }
+               if(!bingoIds.isEmpty()) {
+                   Criteria structureCriteria = Criteria.where("structure.structureId").in(bingoIds);
+                   aggregationOperations.add(Aggregation.match(structureCriteria));
+               }
 
-        if(!searchRequest.getCriteriaList().isEmpty()) { //if attribute parameters are specified
-            List<Criteria> fieldCriteriaList = searchRequest.getCriteriaList().stream().
+           });
+
+        if(!CollectionUtils.isEmpty(searchRequest.getAdvancedSearch())) { //if attribute parameters are specified
+            List<Criteria> fieldCriteriaList = searchRequest.getAdvancedSearch().stream().
                     map(BatchCriteriaConverter::convert).collect(Collectors.toList());
             Criteria[] criteriaList = fieldCriteriaList.toArray(new Criteria[fieldCriteriaList.size()]);
             Criteria andCriteria = new Criteria().andOperator(criteriaList);
             aggregationOperations.add(Aggregation.match(andCriteria));
         }
 
-        return Aggregation.newAggregation(Component.class, aggregationOperations);
+        return Aggregation.newAggregation(aggregationOperations);
     }
 
 
@@ -103,8 +107,8 @@ public class SearchComponentsRepository {
         } return bingoIds;
     }
 
-    private static ProductBatchDetailsDTO convertResult(Component component) {
-        Map map = component.getContent().toMap();
+    private static ProductBatchDetailsDTO convertResult(DBObject component) {
+        Map map = component.toMap();
         String fullBatchNumber = Optional.ofNullable(map.get(BatchComponentUtil.COMPONENT_FIELD_FULL_NBK_BATCH)).
                 map(Object::toString).orElse(null);
         return new ProductBatchDetailsDTO(fullBatchNumber, map);
