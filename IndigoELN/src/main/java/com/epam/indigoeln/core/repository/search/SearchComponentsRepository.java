817c06cd0fdb5fbd8c6675b7d452604ced84df51
Sergei_Bolbin <Sergei_Bolbin@epam.com>
2016-04-13 11:58:12 +0300

    [EPMLSOPELN-186] Refactored

--- IndigoELN/src/main/java/com/epam/indigoeln/core/repository/search/SearchComponentsRepository.java
+++ IndigoELN/src/main/java/com/epam/indigoeln/core/repository/search/SearchComponentsRepository.java
@@ -1,16 +1,12 @@
 package com.epam.indigoeln.core.repository.search;
 
 import com.epam.indigoeln.core.model.Component;
-import com.epam.indigoeln.core.service.bingo.BingoService;
 import com.epam.indigoeln.core.util.BatchComponentUtil;
 import com.epam.indigoeln.web.rest.dto.search.ProductBatchDetailsDTO;
 import com.epam.indigoeln.web.rest.dto.search.request.BatchSearchRequest;
-import com.epam.indigoeln.web.rest.dto.search.request.BatchSearchStructure;
 
-import com.google.common.collect.ImmutableMap;
 import com.mongodb.DBObject;
 import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang.StringUtils;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -18,37 +14,31 @@ import org.springframework.data.mongodb.core.aggregation.Aggregation;
 import org.springframework.data.mongodb.core.aggregation.AggregationResults;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import static com.epam.indigoeln.core.service.search.SearchServiceConstants.*;
 
 @Repository
 public class SearchComponentsRepository {
 
     @Autowired
-    private BingoService bingoService;
-
-    @Autowired
     private MongoTemplate mongoTemplate;
 
-    public List<ProductBatchDetailsDTO> findBatches(BatchSearchRequest searchRequest) {
-        Aggregation aggregation = buildAggregation(searchRequest);
+    public List<ProductBatchDetailsDTO> findBatches(BatchSearchRequest searchRequest, List<Integer> bingoIds) {
+        Aggregation aggregation = buildAggregation(searchRequest, bingoIds);
         AggregationResults<DBObject> aggregate = mongoTemplate.aggregate(aggregation, Component.class, DBObject.class);
         List<DBObject> mappedResults = aggregate.getMappedResults();
         return mappedResults.stream().map(convertResult).collect(Collectors.toList());
     }
 
-    private Aggregation buildAggregation(BatchSearchRequest request) {
+    private Aggregation buildAggregation(BatchSearchRequest request, List<Integer> bingoIds) {
         BatchSearchAggregationBuilder builder = BatchSearchAggregationBuilder.getInstance();
 
         Optional.ofNullable(request.getSearchQuery()).ifPresent(builder::withSearchQuery);
 
-        List<Integer> bingoIds = getBingoDbIds(request);
         if(!CollectionUtils.isEmpty(bingoIds)) {
             builder.withBingoIds(bingoIds);
         }
@@ -58,47 +48,6 @@ public class SearchComponentsRepository {
         }
 
         return builder.build();
-    }
-
-    private List<Integer> getBingoDbIds(BatchSearchRequest searchRequest) {
-        if(searchRequest.getStructure() == null) {
-            return Collections.emptyList();
-        }
-
-        BatchSearchStructure structure = searchRequest.getStructure();
-        List<Integer> result;
-        if(structure.getFormula()!= null) {
-            result = searchByBingoDb(structure.getFormula(), CHEMISTRY_SEARCH_MOLFORMULA, Collections.emptyMap());
-        } else {
-            Map options = ImmutableMap.of("min", structure.getSimilarity());
-            result = searchByBingoDb(structure.getMolfile(), structure.getSearchMode(), options);
-        }
-        return result;
-    }
-
-    @SuppressWarnings("unchecked")
-    private List<Integer> searchByBingoDb(String structure, String searchOperator, Map options) {
-        List<Integer> bingoIds;
-
-        switch (searchOperator) {
-            case CHEMISTRY_SEARCH_SUBSTRUCTURE:
-                bingoIds = bingoService.searchMoleculeSub(structure, StringUtils.EMPTY);
-                break;
-            case CHEMISTRY_SEARCH_EXACT:
-                bingoIds = bingoService.searchMoleculeExact(structure, StringUtils.EMPTY);
-                break;
-            case CHEMISTRY_SEARCH_SIMILARITY:
-                bingoIds = bingoService.searchMoleculeSim(structure,
-                        Float.valueOf(options.getOrDefault("min", 0f).toString()),
-                        Float.valueOf(options.getOrDefault("max", 100f).toString()), StringUtils.EMPTY);
-                break;
-            case CHEMISTRY_SEARCH_MOLFORMULA:
-                bingoIds = bingoService.searchMoleculeMolFormula(structure, StringUtils.EMPTY);
-                break;
-            default:
-                bingoIds = Collections.emptyList();
-        }
-        return bingoIds;
     }
 
     @SuppressWarnings("unchecked")
--- IndigoELN/src/main/java/com/epam/indigoeln/core/service/search/impl/CustomSearchService.java
+++ IndigoELN/src/main/java/com/epam/indigoeln/core/service/search/impl/CustomSearchService.java
@@ -1,14 +1,23 @@
 package com.epam.indigoeln.core.service.search.impl;
 
 import com.epam.indigoeln.core.repository.search.SearchComponentsRepository;
+import com.epam.indigoeln.core.service.bingo.BingoService;
 import com.epam.indigoeln.core.service.search.SearchServiceAPI;
 import com.epam.indigoeln.web.rest.dto.search.ProductBatchDetailsDTO;
-
 import com.epam.indigoeln.web.rest.dto.search.request.BatchSearchRequest;
+import com.epam.indigoeln.web.rest.dto.search.request.BatchSearchStructure;
+
+import com.google.common.collect.ImmutableMap;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import static com.epam.indigoeln.core.service.search.SearchServiceConstants.*;
 
 
 @Service("customSearchService")
@@ -17,12 +26,52 @@ public class CustomSearchService implements SearchServiceAPI {
     @Autowired
     private SearchComponentsRepository searchComponentsRepository;
 
+    @Autowired
+    private BingoService bingoService;
+
     @Override
     public Collection<ProductBatchDetailsDTO> searchBatches(BatchSearchRequest searchRequest) {
-        return searchComponentsRepository.findBatches(searchRequest);
+        return searchComponentsRepository.findBatches(searchRequest, getBingoDbIds(searchRequest));
     }
 
+    private List<Integer> getBingoDbIds(BatchSearchRequest searchRequest) {
+        if(searchRequest.getStructure() == null) {
+            return Collections.emptyList();
+        }
 
+        BatchSearchStructure structure = searchRequest.getStructure();
+        List<Integer> result;
+        if(structure.getFormula()!= null) {
+            result = searchByBingoDb(structure.getFormula(), CHEMISTRY_SEARCH_MOLFORMULA, Collections.emptyMap());
+        } else {
+            Map options = ImmutableMap.of("min", structure.getSimilarity());
+            result = searchByBingoDb(structure.getMolfile(), structure.getSearchMode(), options);
+        }
+        return result;
+    }
 
+    @SuppressWarnings("unchecked")
+    private List<Integer> searchByBingoDb(String structure, String searchOperator, Map options) {
+        List<Integer> bingoIds;
 
+        switch (searchOperator) {
+            case CHEMISTRY_SEARCH_SUBSTRUCTURE:
+                bingoIds = bingoService.searchMoleculeSub(structure, StringUtils.EMPTY);
+                break;
+            case CHEMISTRY_SEARCH_EXACT:
+                bingoIds = bingoService.searchMoleculeExact(structure, StringUtils.EMPTY);
+                break;
+            case CHEMISTRY_SEARCH_SIMILARITY:
+                bingoIds = bingoService.searchMoleculeSim(structure,
+                        Float.valueOf(options.getOrDefault("min", 0f).toString()),
+                        Float.valueOf(options.getOrDefault("max", 100f).toString()), StringUtils.EMPTY);
+                break;
+            case CHEMISTRY_SEARCH_MOLFORMULA:
+                bingoIds = bingoService.searchMoleculeMolFormula(structure, StringUtils.EMPTY);
+                break;
+            default:
+                bingoIds = Collections.emptyList();
+        }
+        return bingoIds;
+    }
 }
