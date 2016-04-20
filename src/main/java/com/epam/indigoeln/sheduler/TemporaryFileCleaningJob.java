39d3e600981fbfc59dae33a9d5a15f423a3c4b40
Evgeny Romanov <evgeny_romanov@epam.com>
2016-04-20 16:16:13 +0300

    Removed unused imports

--- src/main/java/com/epam/indigoeln/sheduler/TemporaryFileCleaningJob.java
+++ src/main/java/com/epam/indigoeln/sheduler/TemporaryFileCleaningJob.java
@@ -8,12 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.time.ZoneOffset;
 import java.time.temporal.ChronoUnit;
-import java.util.Date;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
