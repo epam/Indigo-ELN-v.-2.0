4e02389baffaa6216e85da718eb9dc9c0ae8505a
Elena Polyakova <elena_polyakova@epam.com>
2016-04-29 13:19:40 +0400

    Sonar refactoring

--- src/main/webapp/scripts/app/tabs/tab-manager.js
+++ src/main/webapp/scripts/app/tabs/tab-manager.js
@@ -13,28 +13,7 @@ angular.module('indigoeln')
                     }
                 },
                 resolve: {
-                    data: function ($state, $stateParams, EntitiesBrowser, TabManager, $q) {
-                        var deferred = $q.defer();
-                        //EntitiesBrowser
-                        //    .resolveTabs($stateParams)
-                        //    .then(function (tabs) {
-                        //        //var kind = EntitiesBrowser.getKind($stateParams);
-                        //        //if (kind === 'experiment') {
-                        //        //    EntitiesBrowser.resolveFromCache({
-                        //        //        projectId: $stateParams.projectId,
-                        //        //        notebookId: $stateParams.notebookId
-                        //        //    }).then(function () {
-                        //        //        deferred.resolve({
-                        //        //            entities: tabs
-                        //        //        });
-                        //        //    });
-                        //        //} else {
-                        //        //    deferred.resolve({
-                        //        //        entities: tabs
-                        //        //    });
-                        //        //}
-                        //    });
-                        //return deferred.promise;
+                    data: function () {
                     }
                 }
             });
