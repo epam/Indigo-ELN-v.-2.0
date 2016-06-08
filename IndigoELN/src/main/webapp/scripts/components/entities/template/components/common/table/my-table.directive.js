a7e386bd016b81beb128c70979c448a3cb4ca0ca
Elena Polyakova <elena_polyakova@epam.com>
2016-06-08 16:38:46 +0300

    https://jirapct.epam.com/jira/browse/EPMLSOPELN-226 Do not display value if it is not culculated

--- IndigoELN/src/main/webapp/scripts/components/entities/template/components/common/table/my-table.directive.js
+++ IndigoELN/src/main/webapp/scripts/components/entities/template/components/common/table/my-table.directive.js
@@ -29,9 +29,9 @@ angular.module('indigoeln')
                     }
                     return myTableCtrl.toggleEditable(null, null, null);
                 };
-                var isZeroValue = value => value === 0;
                 $scope.isEmpty = function (obj) {
-                    return (_.isObject(obj) && (_.isEmpty(obj) || isZeroValue(obj.value))) || _.isNull(obj) || _.isUndefined(obj) || isZeroValue(obj);
+                    return _.isNull(obj) || _.isUndefined(obj) || obj === 0 ||
+                        (_.isObject(obj) && (_.isEmpty(obj) || obj.value === 0));
                 };
                 $scope.popoverTitle = $scope.myRow[$scope.myColumn.id];
                 var image = $scope.myRow.structure ? $scope.myRow.structure.image : '';
