a0a83cafd68ddfd78220ec55ab366bc535ab6e6d
Elena Polyakova <elena_polyakova@epam.com>
2016-06-21 19:15:19 +0300

    code cleanup

--- src/main/webapp/scripts/components/sidebar/sidebar.directive.js
+++ src/main/webapp/scripts/components/sidebar/sidebar.directive.js
@@ -1,23 +1,5 @@
 angular.module('indigoeln')
-    .directive('activeLink', function(location) {
-        return {
-            restrict: 'A',
-            link: function (scope, element, attrs) {
-                var clazz = attrs.activeLink;
-                var path = attrs.href;
-                //hack because path does bot return including hashbang
-                path = path.substring(1);
-                scope.location = location;
-                scope.$watch('location.path()', function(newPath) {
-                    if (path === newPath) {
-                        element.addClass(clazz);
-                    } else {
-                        element.removeClass(clazz);
-                    }
-                });
-            }
-        };
-    }).directive('innerMenuToggleButton', function () {
+    .directive('innerMenuToggleButton', function () {
     return {
         restrict: 'C',
         link: function (scope, element) {
