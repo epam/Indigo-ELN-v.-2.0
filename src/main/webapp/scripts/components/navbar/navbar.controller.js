911da89bd6fb1ee433d4ac0121e123f5068283ed
Evgeny Romanov <evgeny_romanov@epam.com>
2016-06-22 12:51:31 +0300

    EPMLSOPELN-224: Dropdown menu, switching between tabs

--- src/main/webapp/scripts/components/navbar/navbar.controller.js
+++ src/main/webapp/scripts/components/navbar/navbar.controller.js
@@ -1,5 +1,5 @@
 angular.module('indigoeln')
-    .controller('NavbarController', function ($scope, $state, $rootScope, Principal, Auth) {
+    .controller('NavbarController', function ($scope, $state, $rootScope, $q, Principal, Auth, EntitiesBrowser) {
         $scope.CONTENT_EDITOR = 'CONTENT_EDITOR';
         $scope.PROJECT_CREATOR = 'PROJECT_CREATOR';
         $scope.NOTEBOOK_CREATOR = 'NOTEBOOK_CREATOR';
@@ -11,10 +11,61 @@ angular.module('indigoeln')
 
         $scope.Principal = Principal;
 
+        EntitiesBrowser.getTabs().then(function (tabs) {
+            $scope.entities = tabs;
+        });
+
+        $scope.getKind = function (fullId) {
+            return EntitiesBrowser.getKind(EntitiesBrowser.expandIds(fullId));
+        };
+        $scope.onEntityClick = function (fullId) {
+            EntitiesBrowser.goToTab(fullId);
+        };
+
         $scope.logout = function () {
             var userId = Principal.getIdentity().id;
             Auth.logout();
             $rootScope.$broadcast('user-logout', {id: userId});
             $state.go('login');
         };
+
+        function updateTabs(toParams) {
+            resolveTabs(toParams).then(function (data) {
+                $scope.entities = data.entities;
+                $scope.entityId = EntitiesBrowser.compactIds(toParams);
+            });
+        }
+
+        $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams) {
+            updateTabs(toParams);
+        });
+
+        function resolveTabs($stateParams) {
+            var params = {
+                projectId: $stateParams.projectId,
+                notebookId: $stateParams.notebookId,
+                experimentId: $stateParams.experimentId
+            };
+            var deferred = $q.defer();
+            EntitiesBrowser
+                .resolveTabs(params)
+                .then(function (tabs) {
+                    var kind = EntitiesBrowser.getKind(params);
+                    if (kind === 'experiment') {
+                        EntitiesBrowser.resolveFromCache({
+                            projectId: params.projectId,
+                            notebookId: params.notebookId
+                        }).then(function () {
+                            deferred.resolve({
+                                entities: tabs
+                            });
+                        });
+                    } else {
+                        deferred.resolve({
+                            entities: tabs
+                        });
+                    }
+                });
+            return deferred.promise;
+        }
     });
