677cc8c62292d15cadca3aa83e0e473b48d7e23b
Stepan Litvinov <stepan_litvinov@epam.com>
2016-08-02 18:13:45 +0300

    fix for user group

--- IndigoELN/src/main/webapp/scripts/components/user/user.service.js
+++ IndigoELN/src/main/webapp/scripts/components/user/user.service.js
@@ -1,8 +1,8 @@
 angular.module('indigoeln')
     .factory('User', function ($resource) {
-        function transformRequest(data) {
-            if (data.user.group) {
-                data.user.group = data.user.group.name;
+        function transformRequest(user) {
+            if (_.isObject(user.group)) {
+                user.group = user.group.name;
             }
         }
         return $resource('api/users/:login', {}, {
