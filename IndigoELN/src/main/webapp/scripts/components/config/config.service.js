6eea73c0ab4e47e588047e23fd6dffc9c22cd926
Stepan Litvinov <stepan_litvinov@epam.com>
2016-08-12 11:25:41 +0300

    https://jirapct.epam.com/jira/browse/EPMLSOPELN-103 load configs before init application

--- IndigoELN/src/main/webapp/scripts/components/config/config.service.js
+++
@@ -1,6 +0,0 @@
-angular.module('indigoeln')
-    .factory('Config', function ($resource) {
-        return $resource('api/client_configuration', {}, {
-            'load': { method: 'GET'}
-        });
-    });
