7be1e61f69f83e6e309bf0577c0ee6e5461f44b2
Elena_Chekanina <Elena_Chekanina@epam.com>
2016-08-31 17:03:06 +0300

    EPMLSOPELN-330

--- src/main/webapp/scripts/components/util/scroller.directive.js
+++ src/main/webapp/scripts/components/util/scroller.directive.js
@@ -42,4 +42,30 @@ angular.module('indigoeln')
                 });
             }
         };
+    }).directive('indelnScroll', function () {
+        return {
+            restrict: 'A',
+            link: function (scope, iElement, iAttrs) {
+                var scrollToTop = function () {
+                    var h = $(window).height();
+                    $(document).mousemove(function(e) {
+                        var mousePosition = e.pageY - $(window).scrollTop();
+                        var topRegion = 220;
+                        var bottomRegion = h - 220;
+                        if(e.which == 1 && (mousePosition < topRegion || mousePosition > bottomRegion)){    // e.wich = 1 => click down !
+                            var distance = e.clientY - h / 2;
+                            distance = distance * 0.1; // <- velocity
+                            $('#entities-content-id').scrollTop( distance + $(document).scrollTop()) ;
+                        }else{
+                            $('#entities-content-id').unbind('mousemove');
+                        }
+                    });
+                };
+               if(iAttrs.dragulaModel){
+                   scope.$on(iAttrs.dragulaModel + '.drag', function(el, source) {
+                       scrollToTop();
+                   });
+               }
+            }
+        };
     });
