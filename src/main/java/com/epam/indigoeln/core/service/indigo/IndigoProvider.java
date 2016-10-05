88792d9ca3118f142b0669b99eeb33f87c0dbc23
Vladislav Alekseev <vladislav_alekseev@epam.com>
2016-10-05 13:15:14 +0300

    EPMLSOPELN-355 Indigo library update crash Indigo application server: structure rendering is impossible now -- create Indigo object for each request

--- src/main/java/com/epam/indigoeln/core/service/indigo/IndigoProvider.java
+++ src/main/java/com/epam/indigoeln/core/service/indigo/IndigoProvider.java
@@ -7,29 +7,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class IndigoProvider {
 
-    private Indigo indigo;
-
-    private IndigoRenderer renderer;
-
-    public IndigoProvider() {
-
-        indigo = new Indigo();
+    public Indigo getIndigo() {
+        Indigo indigo = new Indigo();
         indigo.setOption("ignore-stereochemistry-errors", "true");
 
-        renderer = new IndigoRenderer(indigo);
+        return indigo;
+    }
+
+    public IndigoRenderer getRenderer(Indigo indigo) {
+        IndigoRenderer renderer = new IndigoRenderer(indigo);
 
         indigo.setOption("render-label-mode", "hetero");
         indigo.setOption("render-output-format", "svg");
         indigo.setOption("render-coloring", true);
         indigo.setOption("render-margins", 0, 0);
 
-    }
-
-    public Indigo getIndigo() {
-        return indigo;
-    }
-
-    public IndigoRenderer getRenderer() {
         return renderer;
     }
 }
