d9b173bdec25ce026635a8ba96715d21b8a65a26
Elena_Chekanina <Elena_Chekanina@epam.com>
2017-01-09 20:38:10 +0300

    some style content changes

--- src/main/webapp/scripts/components/navbar/navbar.controller.js
+++ src/main/webapp/scripts/components/navbar/navbar.controller.js
@@ -1,32 +1,32 @@
 angular.module('indigoeln')
     .controller('NavbarController', function ($scope, $state, $rootScope, $q, Principal, Auth, EntitiesBrowser, EntitiesCache) {
-        $scope.CONTENT_EDITOR = 'CONTENT_EDITOR';
-        $scope.PROJECT_CREATOR = 'PROJECT_CREATOR';
-        $scope.NOTEBOOK_CREATOR = 'NOTEBOOK_CREATOR';
-        $scope.EXPERIMENT_CREATOR = 'EXPERIMENT_CREATOR';
-        $scope.GLOBAL_SEARCH = 'GLOBAL_SEARCH';
-        $scope.PROJECT_CREATORS = [$scope.CONTENT_EDITOR, $scope.PROJECT_CREATOR].join(',');
-        $scope.NOTEBOOK_CREATORS = [$scope.CONTENT_EDITOR, $scope.NOTEBOOK_CREATOR].join(',');
-        $scope.EXPERIMENT_CREATORS = [$scope.CONTENT_EDITOR, $scope.EXPERIMENT_CREATOR].join(',');
-        $scope.ENTITY_CREATORS = [$scope.CONTENT_EDITOR, $scope.PROJECT_CREATOR, $scope.NOTEBOOK_CREATOR, $scope.EXPERIMENT_CREATOR].join(',');
+        //$scope.CONTENT_EDITOR = 'CONTENT_EDITOR';
+        //$scope.PROJECT_CREATOR = 'PROJECT_CREATOR';
+        //$scope.NOTEBOOK_CREATOR = 'NOTEBOOK_CREATOR';
+        //$scope.EXPERIMENT_CREATOR = 'EXPERIMENT_CREATOR';
+        //$scope.GLOBAL_SEARCH = 'GLOBAL_SEARCH';
+        //$scope.PROJECT_CREATORS = [$scope.CONTENT_EDITOR, $scope.PROJECT_CREATOR].join(',');
+        //$scope.NOTEBOOK_CREATORS = [$scope.CONTENT_EDITOR, $scope.NOTEBOOK_CREATOR].join(',');
+        //$scope.EXPERIMENT_CREATORS = [$scope.CONTENT_EDITOR, $scope.EXPERIMENT_CREATOR].join(',');
+        //$scope.ENTITY_CREATORS = [$scope.CONTENT_EDITOR, $scope.PROJECT_CREATOR, $scope.NOTEBOOK_CREATOR, $scope.EXPERIMENT_CREATOR].join(',');
 
         $scope.Principal = Principal;
 
         var userId =  Principal.getIdentity().id;
-        $scope.entities = EntitiesBrowser.tabs[userId];
-        $scope.activeTab = EntitiesBrowser.activeTab;
+        //$scope.entities = EntitiesBrowser.tabs[userId];
+        //$scope.activeTab = EntitiesBrowser.activeTab;
 
 
-        $scope.onEntityClick = function (entity) {
-            $rootScope.$broadcast('entity-clicked', entity);
-        };
-        $scope.onCloseEntityClick = function (entity) {
-            $rootScope.$broadcast('entity-closing', entity);
-        };
-        $scope.onCloseAllClick = function () {
-            $rootScope.$broadcast('entity-close-all');
-
-        };
+        //$scope.onEntityClick = function (entity) {
+        //    $rootScope.$broadcast('entity-clicked', entity);
+        //};
+        //$scope.onCloseEntityClick = function (entity) {
+        //    $rootScope.$broadcast('entity-closing', entity);
+        //};
+        //$scope.onCloseAllClick = function () {
+        //    $rootScope.$broadcast('entity-close-all');
+        //
+        //};
 
         $scope.openSearch = function () {
             $state.go('entities.search-panel')
