bb874292b12830fd4b6ee31adb38340e3b9feb50
averbovs <andrei_verbovskii@epam.com>
2017-04-19 20:56:04 +0300

    EPMLSOPELN-415

--- src/main/webapp/scripts/app/entities/notebook/notebook-select-parent.controller.js
+++ src/main/webapp/scripts/app/entities/notebook/notebook-select-parent.controller.js
@@ -1,5 +1,5 @@
 angular.module('indigoeln')
-    .controller('NotebookSelectParentController', function ($scope, $uibModalInstance, parents) {
+    .controller('NotebookSelectParentController', function ($scope, $uibModalInstance, parents, Principal, localStorageService) {
         $scope.parents = parents;
         $scope.selectedParent = '';
 
@@ -13,4 +13,24 @@ angular.module('indigoeln')
         function cancelPressed () {
             $uibModalInstance.dismiss();
         }
+
+        //EPMLSOPELN-415 Remember last selected parent and template
+        Principal.identity()
+            .then(function(user) {
+                var pkey = user.id + '.' + 'lastSelectedProjectId',
+                    pval = localStorageService.get(pkey);
+                if (pval) {
+                    $scope.selectedParent = parents.filter(function(p) {
+                        return p.id == pval;
+                    })[0]
+                }
+                var unsubscribe = $scope.$watchGroup(['selectedParent'], function() {
+                    if ($scope.selectedParent) {
+                        localStorageService.set(pkey, $scope.selectedParent.id)
+                    }
+                })
+                $scope.$on('$destroy', function() {
+                    unsubscribe();
+                });
+            });
     });
