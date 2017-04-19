b9f143a83da40063102b74cb79152e6d0e9b3459
averbovs <andrei_verbovskii@epam.com>
2017-04-19 20:56:04 +0300

    EPMLSOPELN-415

--- IndigoELN/src/main/webapp/scripts/app/entities/experiment/experiment-select-parent-template.controller.js
+++ IndigoELN/src/main/webapp/scripts/app/entities/experiment/experiment-select-parent-template.controller.js
@@ -1,5 +1,5 @@
 angular.module('indigoeln')
-    .controller('ExperimentSelectParentTemplateController', function ($scope, $rootScope, $state, $uibModalInstance, parents, templates, Experiment) {
+    .controller('ExperimentSelectParentTemplateController', function ($scope, $rootScope, $state, $uibModalInstance, parents, templates, Experiment, Principal, localStorageService) {
         $scope.parents = parents;
         $scope.selectedParent = '';
         $scope.templates = templates;
@@ -37,6 +37,36 @@ angular.module('indigoeln')
         function cancelPressed() {
             $uibModalInstance.dismiss();
         }
+
+        //EPMLSOPELN-415 Remember last selected parent and template
+        Principal.identity()
+            .then(function(user) {
+                var tkey = user.id + '.' + 'lastSelectedTemplateId',
+                    tval = localStorageService.get(tkey),
+                    pkey = user.id + '.' + 'lastSelectedExperimentId',
+                    pval = localStorageService.get(pkey);
+                if (tval) {
+                    $scope.selectedTemplate = templates.filter(function(t) {
+                        return t.id == tval;
+                    })[0]
+                }
+                if (pval) {
+                    $scope.selectedParent = parents.filter(function(p) {
+                        return p.id == pval;
+                    })[0]
+                }
+                var unsubscribe = $scope.$watchGroup(['selectedTemplate', 'selectedParent'], function() {
+                    if ($scope.selectedTemplate) {
+                        localStorageService.set(tkey, $scope.selectedTemplate.id)
+                    }
+                    if ($scope.selectedParent) {
+                        localStorageService.set(pkey, $scope.selectedParent.id)
+                    }
+                })
+                $scope.$on('$destroy', function() {
+                    unsubscribe();
+                });
+            });
 
         $scope.ok = save;
         $scope.cancel = cancelPressed;
