ab82ec11e0f5625898e27a204f8fe135b11f2afb
averbovs <andrei_verbovskii@epam.com>
2017-05-18 13:10:21 +0300

    EPMLSOPELN-491

--- src/main/webapp/scripts/app/entities/experiment/common/experiment-pdf-creator.factory.js
+++ src/main/webapp/scripts/app/entities/experiment/common/experiment-pdf-creator.factory.js
@@ -6,12 +6,14 @@ angular.module('indigoeln').factory('experimentPdfCreator',
             $printFormClone.find('div.print-component').each(function () {
                 var $this = $(this);
                 var $checkbox = $this.find('.need-to-print');
-                $this.find('div.col-xs-11').removeClass('col-xs-11').addClass('col-xs-12');
-                if (!$checkbox.find('.checked').length) {
-                    $this.remove();
-                } else if ($checkbox.length) {
-                    $checkbox.remove();
-                }
+                $checkbox.remove();
+                //$this.find('div.col-xs-11').removeClass('col-xs-11').addClass('col-xs-12');
+                // if (!$checkbox.find('.checked').length) {
+                //     $this.remove();
+                // } else if ($checkbox.length) {
+                //     $checkbox.remove();
+                // }
+
             });
             return $printFormClone;
         };
@@ -43,20 +45,17 @@ angular.module('indigoeln').factory('experimentPdfCreator',
                 var $form = preparedPrintForm();
                // $.get('/assets/print.css', function (data) {
                    // var html = '<style>'  + data + '</style>'+ $form.html() + $form.html() + '<!--ADD_PAGE--><h1 style="page-break-before: always">Numbers</h1>' + $form.html() + '<div class="pb"> </div>' + $form.html() + '<div class="pb"> </div>' + $form.html();
-                    var data = '.for-print-only {font-size: 10px; width: 570px; color: #000; } .for-print-only table {width: 100% } .for-print-only table td, .for-print-only table th {border: 1px solid #ccc; font-size: 10px } .pb {page-break-before: always } ';
-                    $form.find('img').remove();
+                    var data = '.for-print-only img {width:100%} .for-print-only {font-size: 10px; width: 570px; color: #000; } .for-print-only table {width: 100% } .for-print-only table td, .for-print-only table th {border: 1px solid #ccc; font-size: 10px } .pb {page-break-before: always } ';
                     var html = '<style>'  + data + '</style> <div class="for-print-only">' +  $form.html() + '</div>';
-                    var pdf = new jsPDF('p', 'pt', 'a4');
-                    var canvas = pdf.canvas;
-                    canvas.height = 72 * 11;
-                    canvas.width = 72 * 8.5;;
-
-                    console.warn('start', pdf, html)
-
-                    html2pdf(html, pdf, function(_pdf) {
-                        _pdf.output('dataurlnewwindow');
-                        pdf.save(fileName);
-                        console.warn('completed pdf', pdf)
+                    console.warn('start', html)
+                    var header = $('#print-form').find('.print-header').parent().html(); 
+                    console.warn('header',  header)
+                    PdfService.create({
+                        html: '<!DOCTYPE html>' + html + '</html>',
+                        //header: header,
+                        fileName: fileName
+                    }).$promise.then(function (response) {
+                        actionToApply(response);
                     });
                     var w = window.open('', 'wnd');
                     w.document.body.innerHTML = html;
