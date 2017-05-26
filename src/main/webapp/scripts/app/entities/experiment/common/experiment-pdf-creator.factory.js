4129bf50e0b25bc12ed984ba7cd18cdb0b314446
averbovski <averbovski@gmail.com>
2017-05-26 16:59:46 +0300

    EPMLSOPELN-511

--- src/main/webapp/scripts/app/entities/experiment/common/experiment-pdf-creator.factory.js
+++ src/main/webapp/scripts/app/entities/experiment/common/experiment-pdf-creator.factory.js
@@ -19,8 +19,8 @@ angular.module('indigoeln').factory('experimentPdfCreator',
                 var  $tr =  $this.find('.print-table').children().children();
                 $tr.each(function() {
                     var $tb = $('<table>').appendTo($this).append($(this))
-                    var $td = $tb.find('td,th')
-                    $td.css('width', Math.round(1/$td.length * 100) + '%')
+                    //var $td = $tb.find('td,th')
+                    //$td.css('width', Math.round(1/$td.length * 100) + '%')
                     $tb.wrap('<div class="pba"><div>');
                 })
 
@@ -55,28 +55,25 @@ angular.module('indigoeln').factory('experimentPdfCreator',
         return {
             createPDF: function (fileName, actionToApply) {
                 var $form = preparedPrintForm();
-               // $.get('/assets/print.css', function (data) {
-                   // var html = '<style>'  + data + '</style>'+ $form.html() + $form.html() + '<!--ADD_PAGE--><h1 style="page-break-before: always">Numbers</h1>' + $form.html() + '<div class="pb"> </div>' + $form.html() + '<div class="pb"> </div>' + $form.html();
-                    var data = '.for-print-only img {width: 100% } .for-print-only {font-size: 13px; color: #000; font-family: arial; } .print-component {page-break-before: auto; page-break-inside: avoid; margin: 20px 0; } .print-component.first{  margin-top: 0 } .for-print-only table {width: 100%; border-spacing: 0; table-layout:fixed;   border-collapse: collapse ; } table { page-break-after:auto } tr    { page-break-inside:avoid; } .pba    { page-break-inside:avoid;} thead { display:table-header-group } tfoot { display:table-footer-group } .for-print-only table td, .for-print-only table th {border: 1px solid #ccc; font-size: 13px; padding: 4px;     word-wrap: break-word; page-break-inside:avoid; }';
+                var cssForPrint =  $('#css-for-print').html()
+                
+                //BODY ZOOM FOR LINUX VERSION ONLY! SHOULD BE MOVED TO SERVER     
+                cssForPrint = 'body { zoom: 0.75; } ' + cssForPrint;
 
-                    //BODY ZOOM FOR LINUX VERSION ONLY! SHOULD BE MOVED TO SERVER     
-                    data = 'body { zoom: 0.75; } ' + data;
-
-                    var html = '<style>'  + data + '</style> <div class="for-print-only">' +  $form.html() + '</div>';
-                    var $origHeader = $('#print-form').find('.print-header').parent().html();
-                    var header = '<style>'  + data + '</style> <div class="for-print-only">' +  $origHeader + '</div>';
-                    //console.warn('header',  header)
-                    PdfService.create({
-                        html: '<!DOCTYPE html>' + html + '</html>',
-                        header: header,
-                        fileName: fileName,
-                        headerHeight : '70mm'
-                    }).$promise.then(function (response) {
-                        actionToApply(response);
-                    });
-                    var w = window.open('', 'wnd');
-                    w.document.body.innerHTML = header + html;
-                //});
+                var html = '<style>'  + cssForPrint + '</style> <div class="for-print-only">' +  $form.html() + '</div>';
+                var $origHeader = $('#print-form').find('.print-header').parent().html();
+                var header = '<style>'  + cssForPrint + '</style> <div class="for-print-only">' +  $origHeader + '</div>';
+                //console.warn('header',  header)
+                PdfService.create({
+                    html: '<!DOCTYPE html>' + html + '</html>',
+                    header: header,
+                    fileName: fileName,
+                    headerHeight : '70mm'
+                }).$promise.then(function (response) {
+                    actionToApply(response);
+                });
+                var w = window.open('', 'wnd');
+                w.document.body.innerHTML = header + html;
             }
         };
     });
