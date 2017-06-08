60b98f693b9da19ff88d847c7170edaf481bf914
Roman_Nikolaev <Roman_Nikolaev@epam.com>
2017-06-08 15:05:40 +0300

    Add properties file

--- IndigoELN/src/main/webapp/scripts/components/sd/sd-export.service.js
+++ IndigoELN/src/main/webapp/scripts/components/sd/sd-export.service.js
@@ -1,5 +1,5 @@
 angular.module('indigoeln')
-    .factory('SdExportService', function (SdService) {
+    .factory('SdExportService', function (SdService, sdProperties) {
 
         var getSubProperty = function (item, prop, subProp) {
             var subItem = item[prop];
@@ -10,25 +10,27 @@ angular.module('indigoeln')
 
         var convert = function (items) {
             return _.map(items, function (item) {
+
+                console.log("It is keys: ", sdProperties.keys);
+
                 return {
                     molfile: item.structure.molfile,
                     properties: {
-                        REGISTRATION_STATUS: item.registrationStatus,
-                        CONVERSATIONAL_BATCH_NUMBER: item.conversationalBatchNumber,
-                        VIRTUAL_COMPOUND_ID: item.virtualCompoundId,
-                        COMPOUND_SOURCE_CODE: getSubProperty(item, 'source', 'name'),
-                        COMPOUND_SOURCE_DETAIL_CODE: getSubProperty(item, 'sourceDetail', 'name'),
-                        STEREOISOMER_CODE: getSubProperty(item, 'stereoisomer', 'name'),
-                        GLOBAL_SALT_CODE: getSubProperty(item, 'saltCode', 'regValue'),
-                        GLOBAL_SALT_EQ: getSubProperty(item, 'saltEq', 'value'),
-                        STRUCTURE_COMMENT: item.structureComments,
-                        COMPOUND_STATE: getSubProperty(item, 'compoundState', 'name'),
-                        PRECURSORS: item.precursors
+                        REGISTRATION_STATUS             :       item.registrationStatus,
+                        CONVERSATIONAL_BATCH_NUMBER     :       item.conversationalBatchNumber,
+                        VIRTUAL_COMPOUND_ID             :       item.virtualCompoundId,
+                        COMPOUND_SOURCE_CODE            :       getSubProperty(item, 'source', 'name'),
+                        COMPOUND_SOURCE_DETAIL_CODE     :       getSubProperty(item, 'sourceDetail', 'name'),
+                        STEREOISOMER_CODE               :       getSubProperty(item, 'stereoisomer', 'name'),
+                        GLOBAL_SALT_CODE                :       getSubProperty(item, 'saltCode', 'regValue'),
+                        GLOBAL_SALT_EQ                  :       getSubProperty(item, 'saltEq', 'value'),
+                        STRUCTURE_COMMENT               :       item.structureComments,
+                        COMPOUND_STATE                  :       getSubProperty(item, 'compoundState', 'name'),
+                        PRECURSORS                      :       item.precursors
                         //TODO: other properties
                     }
                 };
             });
-
         };
 
         var exportItems = function (items) {
