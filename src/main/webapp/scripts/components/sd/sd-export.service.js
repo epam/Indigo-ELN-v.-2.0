55203ec6d226b8afb68fd6994a65b8a040f5db3f
Roman_Nikolaev <Roman_Nikolaev@epam.com>
2017-06-13 18:19:56 +0300

    EPMLSOPELN-326: Refactor

--- src/main/webapp/scripts/components/sd/sd-export.service.js
+++ src/main/webapp/scripts/components/sd/sd-export.service.js
@@ -1,41 +1,8 @@
 angular.module('indigoeln')
     .factory('SdExportService', function (SdService, sdPropertiesInfo) {
 
-        var getSubProperty = function (item, prop, subProp) {
-            var subItem = item[prop];
-            if (subItem) {
-                return subItem[subProp];
-            }
-        };
-
-        var generateExportProperties = function(item) {
-
-            var keys = Object.keys(sdPropertiesInfo);
-            var properties = {};
-
-            keys.forEach(function(key, i, keys){
-                if (typeof(sdPropertiesInfo[key]) === 'object') {
-                    properties[key] = getSubProperty(item, sdPropertiesInfo[key].prop, sdPropertiesInfo[key].subProp);
-                } else {
-                    properties[key] = item[sdPropertiesInfo[key]];
-                }
-            });
-
-            return properties;
-        };
-
-        var convert = function (items) {
-            return _.map(items, function (item) {
-                generateExportProperties(item);
-                return {
-                    molfile: item.structure.molfile,
-                    properties: generateExportProperties(item)
-                };
-            });
-        };
-
         var exportItems = function (items) {
-            return SdService.export({}, convert(items)).$promise;
+            return SdService.export({}, sdPropertiesInfo.convert(items)).$promise;
         };
 
         return {
