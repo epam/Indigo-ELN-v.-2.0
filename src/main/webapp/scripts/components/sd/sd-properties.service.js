bbd59628118a42cf178b5e8eb3ea7cfb55245ac9
Roman_Nikolaev <Roman_Nikolaev@epam.com>
2017-06-15 15:11:23 +0300

    Add some functionality

---
+++ src/main/webapp/scripts/components/sd/sd-properties.service.js
@@ -0,0 +1,119 @@
+angular.module('indigoeln')
+    .factory('sdProperties', function(AppValues) {
+
+        var getItem = function (list, prop, value) {
+            return _.find(list, function (item) {
+                return item[prop].toUpperCase() === value.toUpperCase();
+            });
+        };
+
+        var getWord = function (dicts, dictName, prop, value) {
+            var dict = _.find(dicts, function (dict) {
+                return dict.name === dictName;
+            });
+            if (dict) {
+                return getItem(dict.words, prop, value);
+            }
+        };
+
+        var constants = [
+            {   export : {  name: 'REGISTRATION_STATUS',
+                            prop: 'registrationStatus',
+                            subProp: undefined},
+                import : {  name: 'registrationStatus',
+                            code: 'REGISTRATION_STATUS',
+                            format: undefined}
+            },
+            {   export : {  name: 'CONVERSATIONAL_BATCH_NUMBER',
+                            prop: 'conversationalBatchNumber',
+                            subProp: undefined},
+                import : {  name: 'conversationalBatchNumber',
+                            code: 'CONVERSATIONAL_BATCH_NUMBER',
+                            format: undefined}
+            },
+            {   export : {  name: 'VIRTUAL_COMPOUND_ID',
+                            prop: 'virtualCompoundId',
+                            subProp: undefined},
+                import : {  name: 'virtualCompoundId',
+                            code: 'VIRTUAL_COMPOUND_ID',
+                            format: undefined}
+            },
+            {   export : {  name: 'COMPOUND_SOURCE_CODE',
+                            prop: 'source',
+                            subProp: 'name'},
+                import : {  name: 'source',
+                            code: 'COMPOUND_SOURCE_CODE',
+                            format: function (dicts, value) {
+                                        return getWord(dicts, 'Source', 'name', value);
+                                    }}
+            },
+            {   export : {  name: 'COMPOUND_SOURCE_DETAIL_CODE',
+                            prop: 'sourceDetail',
+                            subProp: 'name'},
+                import : {  name: 'sourceDetail',
+                            code: 'COMPOUND_SOURCE_DETAIL_CODE',
+                            format: function (dicts, value) {
+                                        return getWord(dicts, 'Source Details', 'name', value);
+                                    }}
+            },
+            {   export : {  name: 'STEREOISOMER_CODE',
+                            prop: 'stereoisomer',
+                            subProp: 'name'},
+                import : {  name: 'stereoisomer',
+                            code: 'STEREOISOMER_CODE',
+                            format: function (dicts, value) {
+                                        return getWord(dicts, 'Stereoisomer Code', 'name', value);
+                                     }}
+            },
+            {   export : {  name: 'GLOBAL_SALT_CODE',
+                            prop: 'saltCode',
+                            subProp: 'regValue'},
+                import : {  name: 'saltCode',
+                            code: 'GLOBAL_SALT_CODE',
+                            format: function (dicts, value) {
+                                        return getItem(AppValues.getSaltCodeValues(), 'regValue', value);
+                                     }}
+            },
+            {   export : {  name: 'GLOBAL_SALT_EQ',
+                            prop: 'saltEq',
+                            subProp: 'value'},
+                import : {  name: 'saltEq',
+                            code: 'GLOBAL_SALT_EQ',
+                            format: function (dicts, value) {
+                                         return {
+                                             value: parseInt(value),
+                                             entered: false
+                                         };
+                                     }}
+            },
+            {   export : {  name: 'STRUCTURE_COMMENT',
+                            prop: 'structureComments',
+                            subProp: undefined},
+                import : {  name: 'structureComments',
+                            code: 'STRUCTURE_COMMENT',
+                            format: undefined}
+            },
+            {   export : {  name: 'COMPOUND_STATE',
+                            prop: 'compoundState',
+                            subProp: 'name'},
+                import : {  name: 'compoundState',
+                            code: 'COMPOUND_STATE',
+                            format: function (dicts, value) {
+                                        return getWord(dicts, 'Compound State', 'name', value);
+                                    }}
+            },
+            {   export : {  name: 'PRECURSORS',
+                            prop: 'precursors',
+                            subProp: undefined},
+                import : {  name: 'precursors',
+                            code: 'PRECURSORS',
+                            format: undefined}
+            }
+            //TODO:
+        ];
+
+        return {
+            constants: constants
+        };
+
+    });
