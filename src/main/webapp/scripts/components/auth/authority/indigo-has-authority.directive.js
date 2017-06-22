47d7aa07d554967854aaf4b522b017699ff8bae7
stepan_orlov <stepan_orlov@epam.com>
2017-06-22 15:19:11 +0300

    refactored directives

--- src/main/webapp/scripts/components/auth/authority/indigo-has-authority.directive.js
+++ src/main/webapp/scripts/components/auth/authority/indigo-has-authority.directive.js
@@ -7,37 +7,28 @@
     function indigoHasAuthority(Principal) {
         return {
             restrict: 'A',
-            link: angular.bind({Principal: Principal}, link)
-        };
-    }
+            link: function link($scope, $element, $attrs) {
+                var authority = $attrs.indigoHasAuthority.replace(/\s+/g, '');
 
-    /* @ngInject */
-    function link($scope, $element, $attrs) {
-        var Principal = this.Principal,
-            authority = $attrs.indigoHasAuthority.replace(/\s+/g, '');
+                if (authority.length > 0) {
+                    defineVisibility(true);
+                }
 
-        var setVisible = function () {
-            $element.removeClass('hidden');
-        };
-        var setHidden = function () {
-            $element.addClass('hidden');
-        };
-        var defineVisibility = function (reset) {
-            if (reset) {
-                setVisible();
-            }
-            Principal.hasAuthority(authority)
-                .then(function (result) {
-                    if (result) {
+                function setVisible() {
+                    $element.removeClass('hidden');
+                }
+
+                function defineVisibility(reset) {
+                    if (reset) {
                         setVisible();
-                    } else {
-                        setHidden();
                     }
-                });
-        };
 
-        if (authority.length > 0) {
-            defineVisibility(true);
-        }
+                    Principal.hasAuthority(authority).then(function(result) {
+                        // TODO: should remove element if hasn't access
+                        $element.toggleClass('hidden', !result);
+                    });
+                }
+            }
+        };
     }
 })();
