96694c971b4df2041941c4553ecc616b813fc328
Vladislav Alekseev <vladislav_alekseev@epam.com>
2017-07-31 19:55:52 +0300

    EPMLSOPELN-596 Added locking registration jobs to prevent multiple servers to handle one registration job -- Sonar fixes

--- src/main/java/com/epam/indigoeln/core/model/RegistrationJob.java
+++ src/main/java/com/epam/indigoeln/core/model/RegistrationJob.java
@@ -55,8 +55,12 @@ public class RegistrationJob implements Serializable {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         RegistrationJob that = (RegistrationJob) o;
         return Objects.equals(id, that.id) &&
                 registrationStatus == that.registrationStatus &&
