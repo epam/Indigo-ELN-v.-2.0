503fc2cb26a6bf34abe445d2f21d1fbc82e33617
Vladislav Alekseev <vladislav_alekseev@epam.com>
2017-08-08 16:37:26 +0300

    EPMLSOPELN-626 - BatchModel - updated

--- src/main/java/com/epam/indigoeln/core/chemistry/domain/MonomerBatchModel.java
+++ src/main/java/com/epam/indigoeln/core/chemistry/domain/MonomerBatchModel.java
@@ -1,5 +1,8 @@
 package com.epam.indigoeln.core.chemistry.domain;
 
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
 public class MonomerBatchModel extends BatchModel {
 
     public static final long serialVersionUID = 7526472295622776147L;
