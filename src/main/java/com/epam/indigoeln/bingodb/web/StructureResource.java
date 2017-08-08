7843baa0da83a84e30d2b19b017f942941596036
Vladislav Alekseev <vladislav_alekseev@epam.com>
2017-08-08 18:37:08 +0300

    BingoDB improvements

--- src/main/java/com/epam/indigoeln/bingodb/web/StructureResource.java
+++ src/main/java/com/epam/indigoeln/bingodb/web/StructureResource.java
@@ -14,17 +14,21 @@ import java.util.Collections;
 @RequestMapping("/api/structures")
 public class StructureResource {
 
-    private static final Logger log = LoggerFactory.getLogger(StructureResource.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(StructureResource.class);
+
+    private final BingoService bingoService;
 
     @Autowired
-    private BingoService bingoService;
+    public StructureResource(BingoService bingoService) {
+        this.bingoService = bingoService;
+    }
 
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<ResponseDTO> insert(@RequestBody String s) {
         try {
             return ResponseEntity.ok().body(new ResponseDTO(Collections.singletonList(bingoService.insert(s))));
         } catch (Exception e) {
-            log.warn("Cannot insert structure: " + e.getMessage());
+            LOGGER.warn("Cannot insert structure: " + e.getMessage());
         }
         return ResponseEntity.badRequest().body(new ResponseDTO("Cannot insert structure"));
     }
@@ -34,7 +38,7 @@ public class StructureResource {
         try {
             return ResponseEntity.ok().body(new ResponseDTO(Collections.singletonList(bingoService.update(id, s))));
         } catch (Exception e) {
-            log.warn("Cannot update structure with id=" + id + ": " + e.getMessage());
+            LOGGER.warn("Cannot update structure with id=" + id + ": " + e.getMessage());
         }
         return ResponseEntity.badRequest().body(new ResponseDTO("Cannot update structure with id=" + id));
     }
@@ -45,7 +49,7 @@ public class StructureResource {
             bingoService.delete(id);
             return ResponseEntity.ok().body(null);
         } catch (Exception e) {
-            log.warn("Cannot delete structure with id=" + id + ": " + e.getMessage());
+            LOGGER.warn("Cannot delete structure with id=" + id + ": " + e.getMessage());
         }
         return ResponseEntity.badRequest().body(new ResponseDTO("Cannot delete structure with id=" + id));
     }
@@ -55,7 +59,7 @@ public class StructureResource {
         try {
             return ResponseEntity.ok().body(new ResponseDTO(Collections.singletonList(bingoService.getById(id))));
         } catch (Exception e) {
-            log.warn("Cannot load structure with id=" + id + ": " + e.getMessage());
+            LOGGER.warn("Cannot load structure with id=" + id + ": " + e.getMessage());
         }
         return ResponseEntity.badRequest().body(new ResponseDTO("Cannot load structure with id=" + id));
     }
