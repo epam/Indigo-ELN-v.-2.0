c9ea22d63f91340a5944efed9692ae4994283dd4
stepan_orlov <stepan_orlov@epam.com>
2017-08-09 11:59:28 +0300

    extract notebookSummaryExperiments to own file

--- IndigoELN/src/main/webapp/scripts/components/sidebar/sidebar.service.js
+++ IndigoELN/src/main/webapp/scripts/components/sidebar/sidebar.service.js
@@ -25,36 +25,4 @@ angular.module('indigoeln')
                     method: 'GET', isArray: true
                 }
             });
-    })
-    .factory('NotebookSummaryExperiments', function($resource) {
-        function toModel(experiment) {
-            if (_.isArray(experiment.components)) {
-                var components = {};
-                _.each(experiment.components, function(component) {
-                    components[component.name] = component.content;
-                });
-                experiment.components = components;
-            }
-
-            return experiment;
-        }
-
-        return $resource('api/projects/:projectId/notebooks/:notebookId/experiments/notebook-summary',
-            {
-                projectId: '@projectId',
-                notebookId: '@notebookId'
-            }, {
-                query: {
-                    method: 'GET',
-                    isArray: true,
-                    transformResponse: function(data) {
-                        data = angular.fromJson(data);
-                        _.each(data, function(item, key) {
-                            data[key] = toModel(item);
-                        });
-
-                        return data;
-                    }
-                }
-            });
     });
