064f744d04b5e8163247ca13abd248afaf837f9a
stepan_orlov <stepan_orlov@epam.com>
2017-08-10 19:10:57 +0300

    create recovery module

---
+++ src/main/webapp/scripts/components/autorecovery/autorecovery.service.js
@@ -0,0 +1,33 @@
+angular
+    .module('indigoeln.autorecovery')
+    .factory('recoveryCacheFactory', recoveryCacheFactory);
+
+/* @ngInject */
+function recoveryCacheFactory(CacheFactory, TabKeyUtils) {
+    var cache = CacheFactory('recoveryCache', {
+        storageMode: 'localStorage'
+    });
+
+    return {
+        put: put,
+        get: get,
+        remove: removeByParams,
+        clearAll: clearAll
+    };
+
+    function put(stateParams, data) {
+        cache.put(TabKeyUtils.getTabKeyFromParams(stateParams), data);
+    }
+
+    function get(stateParams) {
+        return cache.get(TabKeyUtils.getTabKeyFromParams(stateParams));
+    }
+
+    function removeByParams(stateParams) {
+        cache.remove(TabKeyUtils.getTabKeyFromParams(stateParams));
+    }
+
+    function clearAll() {
+        cache.clearAll();
+    }
+}
