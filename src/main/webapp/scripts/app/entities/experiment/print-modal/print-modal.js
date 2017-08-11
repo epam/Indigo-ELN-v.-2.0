181c7f3c9c31755597ca24fd74bad0ec94176797
averbovski <averbovski@gmail.com>
2017-08-11 14:42:57 +0300

    PrintModal: refactoring

--- src/main/webapp/scripts/app/entities/experiment/print-modal/print-modal.js
+++ src/main/webapp/scripts/app/entities/experiment/print-modal/print-modal.js
@@ -1,40 +1,36 @@
 angular.module('indigoeln')
-    .constant('printModal', {
-        url: '/print',
-        onEnter: ['$rootScope', '$stateParams', '$state', '$uibModal', '$window',
-            function($rootScope, $stateParams, $state, $uibModal, $window) {
-                $uibModal.open({
-                    animation: true,
-                    templateUrl: 'scripts/app/entities/experiment/print-modal/print-modal.html',
-                    controller: 'PrintModalController',
-                    controllerAs: 'vm'
-                }).result.then(function(result) {
-                    //this won't work while response is not an object
-                    // entity.print(angular.extend($stateParams, result)).$promise.then(function(data) {
-                    //     //window.open("data:application/pdf," + escape(data)); 
-                    // });
+    .factory('printModal', function($rootScope, $stateParams, $state, $uibModal, $window, $httpParamSerializer) {
+        console.log(1)
+        return {
+            showPopup: showPopup
+        };
 
-                    //This will go away when response of print service will be {response : 'pdfString'}
-                    var qs = serialize(result);
-                    var url = 'http://ecse00100843.epam.com/api/print/project/' + $stateParams.projectId;
-                    if ($stateParams.notebookId >= 0) {
-                        url += '/notebook/' + $stateParams.notebookId;
-                    }
-                    if ($stateParams.experimentId >= 0) {
-                        url += '/experiment/' + $stateParams.experimentId;
-                    }
-                    url += qs;
-                    $window.open(url);
-                    $state.go('^');
-                    function serialize(obj) {
-                        return '?' + Object.keys(obj).reduce(function(a, k) {
-                            a.push(k + '=' + encodeURIComponent(obj[k]));
-                            return a;
-                        }, []).join('&');
-                    }
-                }, function() {
-                    $state.go('^');
-                });
-            }
-        ]
+        function showPopup() {
+            $uibModal.open({
+                animation: true,
+                templateUrl: 'scripts/app/entities/experiment/print-modal/print-modal.html',
+                controller: 'PrintModalController',
+                controllerAs: 'vm'
+            }).result.then(function(result) {
+                //this won't work while response is not an object
+                // entity.print(angular.extend($stateParams, result)).$promise.then(function(data) {
+                //     //window.open("data:application/pdf," + escape(data)); 
+                // });
+
+                //This will go away when response of print service will be {response : 'pdfString'}
+                var qs = $httpParamSerializer(result);
+                var url = 'http://ecse00100843.epam.com/api/print/project/' + $stateParams.projectId;
+                if ($stateParams.notebookId >= 0) {
+                    url += '/notebook/' + $stateParams.notebookId;
+                }
+                if ($stateParams.experimentId >= 0) {
+                    url += '/experiment/' + $stateParams.experimentId;
+                }
+                url += '?' + qs;
+                $window.open(url);
+                $state.go('^');
+            }, function() {
+                $state.go('^');
+            });
+        }
     });
