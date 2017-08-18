09998353b3abfcfab5854a73ec3c9112a7e4795d
Sergei Komissarov <Sergei_Komissarov@epam.com>
2017-08-18 17:59:38 +0300

    ISO8601 serializer

--- IndigoELN/src/main/java/com/epam/indigoeln/config/JacksonConfiguration.java
+++ IndigoELN/src/main/java/com/epam/indigoeln/config/JacksonConfiguration.java
@@ -1,14 +1,15 @@
 package com.epam.indigoeln.config;
 
-import com.epam.indigoeln.core.util.JSR310DateTimeSerializer;
-import com.epam.indigoeln.core.util.JSR310LocalDateDeserializer;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 import java.time.*;
+import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class JacksonConfiguration {
@@ -16,11 +17,11 @@ public class JacksonConfiguration {
     @Bean
     Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
         JavaTimeModule module = new JavaTimeModule();
-        module.addSerializer(OffsetDateTime.class, JSR310DateTimeSerializer.INSTANCE);
-        module.addSerializer(ZonedDateTime.class, JSR310DateTimeSerializer.INSTANCE);
-        module.addSerializer(LocalDateTime.class, JSR310DateTimeSerializer.INSTANCE);
-        module.addSerializer(Instant.class, JSR310DateTimeSerializer.INSTANCE);
-        module.addDeserializer(LocalDate.class, JSR310LocalDateDeserializer.INSTANCE);
+        module.addSerializer(OffsetDateTime.class, OffsetDateTimeSerializer.INSTANCE);
+        module.addSerializer(ZonedDateTime.class, new ZonedDateTimeSerializer(DateTimeFormatter.ISO_INSTANT));
+        module.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ISO_INSTANT));
+        module.addSerializer(Instant.class, InstantSerializer.INSTANCE);
+        module.addDeserializer(LocalDate.class, LocalDateDeserializer.INSTANCE);
         return new Jackson2ObjectMapperBuilder()
                 .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
                 .findModulesViaServiceLoader(true)
