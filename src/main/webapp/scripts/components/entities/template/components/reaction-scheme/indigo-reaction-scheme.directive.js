0d8b0e991f4344d4227c71f179d5fd536c1703ba
stepan_orlov <stepan_orlov@epam.com>
2017-08-23 14:11:00 +0300

    fixed wrong open the analyzeRXN modal fixed createRXN

--- src/main/webapp/scripts/components/entities/template/components/reaction-scheme/indigo-reaction-scheme.directive.js
+++ src/main/webapp/scripts/components/entities/template/components/reaction-scheme/indigo-reaction-scheme.directive.js
@@ -12,6 +12,8 @@
             bindToController: true,
             scope: {
                 model: '=',
+                reaction: '=',
+                reactionTrigger: '=',
                 experiment: '=',
                 isReadonly: '=',
                 onChanged: '&'
@@ -27,6 +29,7 @@
 
         function init() {
             vm.onChangedStructure = onChangedStructure;
+            vm.modelTrigger = 0;
 
             checkReaction();
             bindEvents();
@@ -42,9 +45,12 @@
         }
 
         function bindEvents() {
-            $scope.$on('new-reaction-scheme', function(event, data) {
-                vm.model.reaction.image = data.image;
-                vm.model.reaction.molfile = data.molfile;
+            $scope.$on('new-reaction-scheme', function(event, reactionData) {
+                if (reactionData.structure !== _.get(vm.model.reaction, 'molfile')) {
+                    vm.model.reaction.molfile = reactionData.structure;
+                    vm.model.reaction.image = null;
+                    vm.modelTrigger++;
+                }
             });
         }
 
@@ -52,21 +58,25 @@
             return $q.all(_.map(molfiles, CalculationService.getMoleculeInfo));
         }
 
+        function updateReaction(response) {
+            vm.model.reaction.molfile = response.structure;
+            vm.model.reaction.molReactants = response.reactants;
+            vm.model.reaction.molProducts = response.products;
+
+            return $q.all([
+                getInfo(vm.model.reaction.molReactants),
+                getInfo(vm.model.reaction.molProducts)
+            ]).then(function(results) {
+                vm.model.reaction.infoReactants = results[0];
+                vm.model.reaction.infoProducts = results[1];
+                $rootScope.$broadcast('REACTION_CHANGED', vm.model.reaction);
+                vm.onChanged();
+            });
+        }
+
         function onChangedStructure(structure) {
             vm.loading = CalculationService.getReactionProductsAndReactants(structure.molfile).then(function(response) {
-                vm.model.reaction = structure;
-                vm.model.reaction.molReactants = response.reactants;
-                vm.model.reaction.molProducts = response.products;
-
-                return $q.all([
-                    getInfo(vm.model.reaction.molReactants),
-                    getInfo(vm.model.reaction.molProducts)
-                ]).then(function(results) {
-                    vm.model.reaction.infoReactants = results[0];
-                    vm.model.reaction.infoProducts = results[1];
-                    $rootScope.$broadcast('REACTION_CHANGED', structure);
-                    vm.onChanged();
-                });
+                return updateReaction(response, structure);
             });
 
             return vm.loading;
