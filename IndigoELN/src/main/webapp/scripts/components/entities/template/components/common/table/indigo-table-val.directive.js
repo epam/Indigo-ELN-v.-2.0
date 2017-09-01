df62af63230883c11f1bf2a548bac57b07fcf137
Roman_Nikolaev@epam.com <Roman_Nikolaev@epam.com>
2017-09-01 16:19:59 +0300

    EPMLSOPELN-606: fix problem with changing fields in registered batches

--- IndigoELN/src/main/webapp/scripts/components/entities/template/components/common/table/indigo-table-val.directive.js
+++ IndigoELN/src/main/webapp/scripts/components/entities/template/components/common/table/indigo-table-val.directive.js
@@ -14,6 +14,7 @@
                 indigoColumn: '=',
                 indigoRow: '=',
                 indigoRowIndex: '=',
+                isEditableField: '=',
                 isEditableColumn: '&',
                 toggleEditableColumn: '&',
                 setClosePrevious: '&',
