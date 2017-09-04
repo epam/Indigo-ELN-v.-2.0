8bbc9487d9d55559d26f60fb0c0275674cdb89f1
Ivan Osipov <ivan_osipov@epam.com>
2017-09-04 18:01:23 +0300

    EPMLSOPELN-642 (round Mol)

--- IndigoELN/src/main/webapp/scripts/components/util/round.filter.js
+++ IndigoELN/src/main/webapp/scripts/components/util/round.filter.js
@@ -2,17 +2,28 @@ angular.module('indigoeln')
     .filter('round', function(StoichTableCache, CalculationService) {
         var DEFAULT_SIG_DIGITS = 3;
 
-        function getNumberSignificantFigures(digits) {
-            var sigFigNumber = _.toString(parseInt(digits.toString().replace(/[^\d]/g, ''), 10));
+        return function(value, sigDigits, column, row) {
+            var significantDigits = sigDigits;
+            if (column && row) {
+                significantDigits = getSigDigitsForMol(column, row);
+            }
 
-            return sigFigNumber.length;
-        }
+            return +toStringWithSignificantDigits(+value, significantDigits || DEFAULT_SIG_DIGITS);
+        };
+
+        /**
+         * This method gets number, desired accuracy of the number and converts a number with a given accuracy
+         * @param value
+         * @param sigDigits
+         * @returns {*}
+         */
 
         function toStringWithSignificantDigits(value, sigDigits) {
             var delta = 0;
-            if (!(value + '').startsWith('0.')) {
+            if (isFractionalNumber(value) && !isNumberStartsWithZero(value)) {
                 delta = (value + '').indexOf('.');
             }
+
             sigDigits += delta;
             if (value === 0) {
                 return value.toFixed(sigDigits - 1);
@@ -20,8 +31,9 @@ angular.module('indigoeln')
             // makes little sense for 0
             var numDigits = Math.ceil(Math.log10(Math.abs(value)));
             var rounded = Math.round(value * Math.pow(10, sigDigits - numDigits)) * Math.pow(10, numDigits - sigDigits);
+            var fixNumber = fixNumberToGivenAccuracy(sigDigits, numDigits);
 
-            return rounded.toFixed(Math.max(sigDigits - numDigits, 0));
+            return rounded.toFixed(Math.max(fixNumber ? sigDigits : sigDigits - numDigits, 0));
         }
 
         function getSigDigitsForMol(column, row) {
@@ -31,25 +43,37 @@ angular.module('indigoeln')
             if (column.id === 'mol' && column.isIntended) {
                 sourceBatch = CalculationService.findLimiting(StoichTableCache.getStoicTable());
             }
-            var weightOrVolume = angular.copy(sourceBatch.weight || sourceBatch.volume);
-            if (weightOrVolume && weightOrVolume.value && weightOrVolume.entered) {
-                sigDigitsForMol = getNumberSignificantFigures(weightOrVolume.value);
+
+            if (column.id !== 'molWeight') {
+                var weightOrVolume = angular.copy(sourceBatch.weight || sourceBatch.volume);
+                if (weightOrVolume && weightOrVolume.value && weightOrVolume.entered) {
+                    sigDigitsForMol = getNumberSignificantFigures(weightOrVolume.value);
+                }
             }
 
             return sigDigitsForMol;
         }
 
-        return function(value, sigDigits, column, row) {
-            var isEntered = false;
-            var significantDigits = sigDigits;
-            if (column && row) {
-                isEntered = row[column.id].entered;
-                significantDigits = getSigDigitsForMol(column, row);
-            }
-            if (isEntered || !value) {
-                return value;
-            }
+        /**
+         * This method gets value of Weight or Volume and returns length of a number
+         * @param digits
+         * @returns {Number}
+         */
+        function getNumberSignificantFigures(digits) {
+            var sigFigNumber = _.toString(parseInt(digits.toString().replace(/[^\d]/g, ''), 10));
 
-            return +toStringWithSignificantDigits(+value, significantDigits || DEFAULT_SIG_DIGITS);
-        };
+            return sigFigNumber.length;
+        }
+
+        function isFractionalNumber(value) {
+            return (value + '').indexOf('.') >= 0;
+        }
+
+        function isNumberStartsWithZero(value) {
+            return (value + '').startsWith('0.');
+        }
+
+        function fixNumberToGivenAccuracy(sigDigits, numDigits) {
+            return sigDigits + numDigits > 0;
+        }
     });
