b0cb3ce885b82bf2be06e888d3fbfd6dd09e6519
Maria_Kuznetsova <Maria_Kuznetsova@epam.com>
2017-09-04 13:10:59 +0300

    After merging fix

--- IndigoELN/src/main/webapp/scripts/app/common/constants/app-units.js
+++ IndigoELN/src/main/webapp/scripts/app/common/constants/app-units.js
@@ -38,24 +38,30 @@ angular
 
         defaultBatch: {
             limiting: false,
-            weight: {value: 0, unit: '', entered: false},
-            volume: {value: 0, unit: '', entered: false},
-            density: {value: 0, unit: '', entered: false},
-            molarity: {value: 0, unit: '', entered: false},
-            mol: {value: 0, unit: '', entered: false},
+            weight: {value: 0, unit: 'mg', entered: false},
+            volume: {value: 0, unit: 'mL', entered: false},
+            density: {value: 0, unit: 'g/mL', entered: false},
+            molarity: {value: 0, unit: 'M', entered: false},
+            mol: {value: 0, unit: 'mmol', entered: false},
             loadFactor: {value: 0, unit: '', entered: false},
             theoWeight: {value: 0, unit: '', entered: false},
             theoMoles: {value: 0, unit: '', entered: false},
-            totalVolume: {value: 0, unit: '', entered: false},
-            totalWeight: {value: 0, unit: '', entered: false},
-            totalMoles: {value: 0, unit: '', entered: false},
+            totalVolume: {value: 0, unit: 'mL', entered: false},
+            totalWeight: {value: 0, unit: 'mg', entered: false},
+            totalMoles: {value: 0, unit: 'mmol', entered: false},
             rxnRole: {name: 'REACTANT', entered: false},
             saltCode: {name: '00 - Parent Structure', value: '0', regValue: '00', entered: false},
             molWeight: {value: 0, entered: false},
+            structure: {
+                image: null,
+                molfile: null,
+                structureId: null
+            },
             stoicPurity: {value: 100, entered: false},
             saltEq: {value: 0, entered: false},
             eq: {value: 1, entered: false},
             yield: 0,
-            prevMolarAmount: {value: 0, unit: '', entered: false}
+            prevMolarAmount: {value: 0, unit: 'M', entered: false},
+            precursors: ''
         }
     });
