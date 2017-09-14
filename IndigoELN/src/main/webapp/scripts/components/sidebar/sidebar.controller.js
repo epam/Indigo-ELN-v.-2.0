5999e34789ccc071baccd8808be1068b4edea0dc
stepan_orlov <stepan_orlov@epam.com>
2017-09-14 13:19:15 +0300

    added sidebar cache minor code improvements

--- IndigoELN/src/main/webapp/scripts/components/sidebar/sidebar.controller.js
+++ IndigoELN/src/main/webapp/scripts/components/sidebar/sidebar.controller.js
@@ -3,23 +3,8 @@
         .module('indigoeln')
         .controller('SidebarController', SidebarController);
 
-    function SidebarController($scope, $state, localStorageService, Project, Notebook, Experiment,
-                               AllProjects, AllNotebooks, AllExperiments, Principal, $rootScope, user) {
+    function SidebarController($scope, $state, sidebarCache, Project, Notebook, Experiment, AllExperiments) {
         var vm = this;
-        var USER_PREFIX = user.id + '.';
-        var FIELD_NAME = 'name';
-        var FIELD_ID = 'id';
-
-        var BOOKMARKS = 'bookmarks';
-        var SIDEBAR_BOOKMARKS = USER_PREFIX + 'sidebar.bookmarks';
-        var SIDEBAR_BOOKMARKS_NOTEBOOKS = USER_PREFIX + 'sidebar.bookmarks.notebooks';
-        var SIDEBAR_BOOKMARKS_EXPERIMENTS = USER_PREFIX + 'sidebar.bookmarks.experiments';
-
-        var SIDEBAR_MYPROJECTS = USER_PREFIX + 'sidebar.myprojects';
-        var SIDEBAR_MYPROJECTS_NOTEBOOKS = USER_PREFIX + 'sidebar.myprojects.notebooks';
-        var SIDEBAR_MYPROJECTS_EXPERIMENTS = USER_PREFIX + 'sidebar.myprojects.experiments';
-
-        var SIDEBAR_ADMINISTRATION = USER_PREFIX + 'sidebar.administration';
 
         vm.CONTENT_EDITOR = 'CONTENT_EDITOR';
         vm.USER_EDITOR = 'USER_EDITOR';
@@ -28,8 +13,6 @@
         vm.DICTIONARY_EDITOR = 'DICTIONARY_EDITOR';
         vm.POPOVER_TEMPLATE = 'scripts/components/sidebar/sidebar-popover-template.html';
         vm.ADMINISTRATION_AUTHORITIES = [vm.USER_EDITOR, vm.ROLE_EDITOR, vm.TEMPLATE_EDITOR, vm.DICTIONARY_EDITOR].join(',');
-        vm.myBookmarks = {};
-        vm.allProjects = {};
         vm.$state = $state;
 
         vm.getTreeItemById = getTreeItemById;
@@ -41,18 +24,20 @@
         init();
 
         function init() {
-            vm.allProjectIsCollapsed = localStorageService.get(USER_PREFIX + 'allProjectIsCollapsed');
-            vm.bookmarksIsCollapsed = localStorageService.get(USER_PREFIX + 'bookmarksIsCollapsed');
+            vm.allProjectIsCollapsed = sidebarCache.get('allProjectIsCollapsed');
+            vm.bookmarksIsCollapsed = sidebarCache.get('bookmarksIsCollapsed');
 
-            vm.selectedFullId = localStorageService.get(USER_PREFIX + 'selectedFullId');
-            vm.adminToggled = JSON.parse(localStorageService.get(SIDEBAR_ADMINISTRATION));
+            vm.selectedFullId = sidebarCache.get('selectedFullId');
+            vm.adminToggled = sidebarCache.get('adminToggled');
 
             bindEvents();
         }
 
         function onSelectNode(fullId) {
-            vm.selectedFullId = fullId;
-            localStorageService.set(USER_PREFIX + 'selectedFullId', fullId);
+            if (vm.selectedFullId !== fullId) {
+                vm.selectedFullId = fullId;
+                sidebarCache.put('selectedFullId', fullId);
+            }
         }
 
         function getTreeItemById(items, itemId) {
@@ -63,218 +48,17 @@
 
         function toggleAdministration() {
             vm.adminToggled = !vm.adminToggled;
-            var data = {
-                id: 'adm', isOpen: vm.adminToggled
-            };
-            localStorageService.set(SIDEBAR_ADMINISTRATION, JSON.stringify(data));
+            sidebarCache.put('adminToggled', vm.adminToggled);
         }
 
         function toggleProjects() {
             vm.allProjectIsCollapsed = !vm.allProjectIsCollapsed;
-            localStorageService.set(USER_PREFIX + 'allProjectIsCollapsed', vm.allProjectIsCollapsed);
+            sidebarCache.put('allProjectIsCollapsed', vm.allProjectIsCollapsed);
         }
 
         function toggleMyProjects() {
             vm.bookmarksIsCollapsed = !vm.bookmarksIsCollapsed;
-            localStorageService.set(USER_PREFIX + 'bookmarksIsCollapsed', vm.bookmarksIsCollapsed);
-        }
-
-        // function toggleProjects(parent, needAll, fun, file) {
-        //     var initialProject = JSON.parse(localStorageService.get(file));
-        //     if (!parent.projects) {
-        //         var agent = needAll ? AllProjects : Project;
-        //         fun(agent, parent);
-        //     } else {
-        //         parent.projects = null;
-        //         var data = {
-        //             id: initialProject.id, isOpen: false
-        //         };
-        //         localStorageService.set(file, JSON.stringify(data));
-        //         // close notebooks + experiments
-        //         closeNotebooksAndExperimentInLocalStorage(file);
-        //     }
-        // }
-
-        function toggleNotebooks(project, needAll, fun, file) {
-            $state.go('entities.project-detail', {
-                projectId: project.id
-            });
-            if (project.isOpen) {
-                project.children = null;
-                project.isOpen = false;
-                saveInStorage({
-                    id: project.id, isOpen: false
-                }, file);
-                // close experiments with id == experiment.projectId
-                closeExperimentsInLocalStorage(file, project.id);
-            } else if (Principal.hasAnyAuthority(['CONTENT_EDITOR', 'NOTEBOOK_READER'])) {
-                var agent = needAll ? AllNotebooks : Notebook;
-                fun(agent, project);
-            } else {
-                project.children = null;
-                project.isOpen = true;
-                saveInStorage({
-                    id: project.id, isOpen: true
-                }, file);
-            }
-        }
-
-        function toggleExperiments(notebook, project, needAll, fun, file) {
-            $state.go('entities.notebook-detail', {
-                notebookId: notebook.id, projectId: project.id
-            });
-            if (notebook.isOpen) {
-                notebook.children = null;
-                notebook.isOpen = false;
-                saveInStorage({
-                    name: notebook.name,
-                    isOpen: false,
-                    projectId: notebook.projectId
-                }, file, FIELD_NAME);
-            } else if (Principal.hasAnyAuthority(['CONTENT_EDITOR', 'EXPERIMENT_READER'])) {
-                var agent = needAll ? AllExperiments : Experiment;
-                fun(agent, notebook, project, true);
-            } else {
-                notebook.children = null;
-                notebook.isOpen = true;
-                saveInStorage({
-                    name: notebook.name,
-                    isOpen: true,
-                    projectId: notebook.projectId
-                }, file, FIELD_NAME);
-            }
-        }
-
-        function promiseToggleMyprojectsProject(agent, parent) {
-            return promiseToggleProject(agent, parent, SIDEBAR_MYPROJECTS);
-        }
-
-        function promiseToggleMyprojectsNotebook(agent, project) {
-            return promiseToggleNotebook(agent, project, SIDEBAR_MYPROJECTS_NOTEBOOKS);
-        }
-
-        function promiseToggleMyprojectsExperiment(agent, notebook, project, save) {
-            return promiseToggleExperiment(agent, notebook, project, SIDEBAR_MYPROJECTS_EXPERIMENTS, save);
-        }
-
-        function promiseToggleBookmarksProject(agent, parent) {
-            return promiseToggleProject(agent, parent, SIDEBAR_BOOKMARKS);
-        }
-
-        function promiseToggleBookmarksNotebook(agent, project) {
-            return promiseToggleNotebook(agent, project, SIDEBAR_BOOKMARKS_NOTEBOOKS);
-        }
-
-        function promiseToggleBookmarksExperiment(agent, notebook, project, save) {
-            return promiseToggleExperiment(agent, notebook, project, SIDEBAR_BOOKMARKS_EXPERIMENTS, save);
-        }
-
-        function promiseToggleProject(agent, parent, file) {
-            return agent.query({}, function(result) {
-                if (result.length) {
-                    parent.projects = result;
-                    var data = {
-                        id: result[0].id, isOpen: true
-                    };
-                    localStorageService.set(file, JSON.stringify(data));
-                }
-            });
-        }
-
-        function promiseToggleNotebook(agent, project, file) {
-            return agent.query({
-                projectId: project.id
-            }, function(result) {
-                project.children = result;
-                project.isOpen = true;
-                saveInStorage({
-                    id: project.id, isOpen: true
-                }, file);
-            });
-        }
-
-        function promiseToggleExperiment(agent, notebook, project, file, save) {
-            return agent.query({
-                notebookId: notebook.id, projectId: project.id
-            }, function(result) {
-                notebook.children = result;
-                notebook.isOpen = true;
-                if (save) {
-                    saveInStorage({
-                        name: notebook.name,
-                        isOpen: true,
-                        projectId: notebook.projectId
-                    }, file, FIELD_NAME);
-                }
-            });
-        }
-
-        function saveInStorage(data, file, field) {
-            if (_.isUndefined(typeof (field))) {
-                field = FIELD_ID;
-            }
-            var initialValue = JSON.parse(localStorageService.get(file));
-            if (initialValue) {
-                var index = _.findIndex(initialValue, function(item) {
-                    return item[field] === data[field];
-                });
-                if (index !== -1) {
-                    initialValue[index] = data;
-                } else {
-                    initialValue.push(data);
-                }
-            } else {
-                initialValue = [];
-                initialValue.push(data);
-            }
-
-            localStorageService.set(file, JSON.stringify(initialValue));
-        }
-
-        function closeNotebooksAndExperimentInLocalStorage(file) {
-            var ls = file.split('.')[2];
-            var notebooks;
-            var experiments;
-            if (ls === BOOKMARKS) {
-                notebooks = JSON.parse(localStorageService.get(SIDEBAR_BOOKMARKS_NOTEBOOKS));
-                experiments = JSON.parse(localStorageService.get(SIDEBAR_BOOKMARKS_EXPERIMENTS));
-            } else {
-                notebooks = JSON.parse(localStorageService.get(SIDEBAR_MYPROJECTS_NOTEBOOKS));
-                experiments = JSON.parse(localStorageService.get(SIDEBAR_MYPROJECTS_EXPERIMENTS));
-            }
-            _.forEach(notebooks, function(item) {
-                item.isOpen = false;
-            });
-            _.forEach(experiments, function(item) {
-                item.isOpen = false;
-            });
-            if (ls === BOOKMARKS) {
-                localStorageService.set(SIDEBAR_BOOKMARKS_EXPERIMENTS, JSON.stringify(experiments));
-                localStorageService.set(SIDEBAR_BOOKMARKS_NOTEBOOKS, JSON.stringify(notebooks));
-            } else {
-                localStorageService.set(SIDEBAR_MYPROJECTS_EXPERIMENTS, JSON.stringify(experiments));
-                localStorageService.set(SIDEBAR_MYPROJECTS_NOTEBOOKS, JSON.stringify(notebooks));
-            }
-        }
-
-        function closeExperimentsInLocalStorage(file, id) {
-            var ls = file.split('.')[2];
-            var experiments;
-            if (ls === BOOKMARKS) {
-                experiments = JSON.parse(localStorageService.get(SIDEBAR_BOOKMARKS_EXPERIMENTS));
-            } else {
-                experiments = JSON.parse(localStorageService.get(SIDEBAR_MYPROJECTS_EXPERIMENTS));
-            }
-            _.forEach(experiments, function(item) {
-                if (item.projectId === id) {
-                    item.isOpen = false;
-                }
-            });
-            if (ls === BOOKMARKS) {
-                localStorageService.set(SIDEBAR_BOOKMARKS_EXPERIMENTS, JSON.stringify(experiments));
-            } else {
-                localStorageService.set(SIDEBAR_MYPROJECTS_EXPERIMENTS, JSON.stringify(experiments));
-            }
+            sidebarCache.put('bookmarksIsCollapsed', vm.bookmarksIsCollapsed);
         }
 
         function updateTreeForExperiments(event, data) {
@@ -344,12 +128,15 @@
             updateExperiments(vm.allProjects.projects, updStatus);
         }
 
+        function getFullIdFromParams(toParams) {
+            return _.compact([toParams.projectId, toParams.notebookId, toParams.experimentId])
+                .join('-')
+                .toString();
+        }
+
         function bindEvents() {
             $scope.$on('$stateChangeSuccess', function(event, toState, toParams) {
-                vm.onSelectNode(_.compact([toParams.projectId, toParams.notebookId, toParams.experimentId])
-                    .join('-')
-                    .toString());
-
+                vm.onSelectNode(getFullIdFromParams(toParams));
             });
 
             $scope.$on('project-created', function($event, id) {
