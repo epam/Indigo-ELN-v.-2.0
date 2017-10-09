ea80b2fa59b9214b971f6a55441879617f0abdb7
stepan_orlov <stepan_orlov@epam.com>
2017-10-09 14:12:44 +0300

    added cache for Dictionary service

--- IndigoELN/src/main/webapp/scripts/components/dictionary/dictionary.service.js
+++ IndigoELN/src/main/webapp/scripts/components/dictionary/dictionary.service.js
@@ -1,17 +1,27 @@
 angular.module('indigoeln')
-    .factory('Dictionary', function($resource) {
+    .factory('Dictionary', function($resource, CacheFactory) {
+        var dictionaryCache = CacheFactory.createCache('dictionaryCache', {
+            maxAge: 5 * 60 * 1000,
+            deleteOnExpire: 'passive'
+        });
+
         return $resource('api/dictionaries/:id', {}, {
             query: {
-                method: 'GET', isArray: true
+                method: 'GET',
+                isArray: true
             },
             get: {
                 method: 'GET'
             },
             all: {
-                url: 'api/dictionaries/all', method: 'GET', isArray: true
+                url: 'api/dictionaries/all',
+                method: 'GET',
+                isArray: true
             },
             getByName: {
-                url: 'api/dictionaries/byName/:name', method: 'GET'
+                url: 'api/dictionaries/byName/:name',
+                method: 'GET',
+                cache: dictionaryCache
             },
             save: {
                 method: 'POST'
