8688fec3347e99f4dba75ee20c79938047a28518
Maria_Kuznetsova <Maria_Kuznetsova@epam.com>
2017-10-17 11:02:40 +0300

    Experiment page: remember scroll location when reload

--- IndigoELN/src/main/webapp/scripts/app/common/directives/custom-scroll.directive.js
+++ IndigoELN/src/main/webapp/scripts/app/common/directives/custom-scroll.directive.js
@@ -4,7 +4,7 @@
         .directive('customScroll', customScroll);
 
     /* @ngInject */
-    function customScroll($state, Principal, simpleLocalCache) {
+    function customScroll($state, Principal, simpleLocalCache, $timeout) {
         return {
             restrict: 'A',
             link: link
@@ -18,10 +18,8 @@
                     var key = user.id + "." + $state.params.projectId + '-' + $state.params.notebookId + '-' + $state.params.experimentId + '.' + currentTab + '.scrollPos';
                     var scrollLocation = simpleLocalCache.getByKey(key) || 0;
 
-                    $scope.$watch('vm.activeTabIndex', function(newValue) {
-                        if (newValue == currentTab) {
-                            $element.scrollTop(scrollLocation);
-                        }
+                    $timeout(function () {
+                        $element.scrollTop(scrollLocation);
                     });
 
                     $element.on('scroll', function() {
