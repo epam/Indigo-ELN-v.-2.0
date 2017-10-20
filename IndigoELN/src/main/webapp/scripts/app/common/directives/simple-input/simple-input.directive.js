ec26fd02cb8915e15ac6347ddc4c02cdf55d595f
stepan_orlov <stepan_orlov@epam.com>
2017-10-20 14:57:06 +0300

    WIP: EPMLSOPELN-681
    created a directive simple-input
    replaced indigo-input to simple-input in some places

--- IndigoELN/src/main/webapp/scripts/app/common/directives/simple-input/simple-input.directive.js
+++ IndigoELN/src/main/webapp/scripts/app/common/directives/simple-input/simple-input.directive.js
@@ -1,18 +1,35 @@
 (function() {
     angular
         .module('indigoeln')
-        .directive('simpleInput', indigoInput);
+        .run(run)
+        .directive('simpleInput', simpleInput);
 
-    /* @ngInject */
-    function indigoInput() {
+    simpleInput.$inject = ['$templateCache'];
+
+    function simpleInput($templateCache) {
         return {
             restrict: 'E',
-            scope: {
-                label: '@',
-                model: '=',
-                isReadonly: '='
+            transclude: true,
+            compile: function($element) {
+                $element.addClass('form-group');
+                var $input = $element.find('input');
+
+                if ($input.attr('ng-required')) {
+                    $input.after($templateCache.get('inputModelInvalid'));
+                }
             },
-            templateUrl: 'scripts/app/common/directives/simple-input/simple-input.html'
+            template: '<label ng-transclude></label>'
         };
+    }
+
+    run.$inject = ['$templateCache'];
+
+    function run($templateCache) {
+        $templateCache.put('inputModelInvalid', '<div ng-if="ngModelCtrl.$invalid">' +
+            '<p class="help-block" ng-if="ngModelCtrl.$error.required">This field is required. </p>' +
+            '<p class="help-block" ng-if="ngModelCtrl.$error.maxlength">This field can\t be longer than' +
+            '<span ng-bind="ngModelCtrl.maxlength"></span> characters.</p>' +
+            '<p class="help-block" ng-if="ngModelCtrl.$error.pattern"' +
+            ' ng-bind="indigoValidationPatternText"></p></div>');
     }
 })();
