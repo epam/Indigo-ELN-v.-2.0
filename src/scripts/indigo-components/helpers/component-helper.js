b794946682788d2ea08549db25f5a53bad8314a7
stepan_orlov <stepan_orlov@epam.com>
2017-11-02 15:16:29 +0300

    added not found message fixed styles

--- src/scripts/indigo-components/helpers/component-helper.js
+++ src/scripts/indigo-components/helpers/component-helper.js
@@ -2,25 +2,15 @@
     angular.module('indigoeln')
         .factory('componentHelper', componentHelper);
 
-    componentHelper.$inject = ['Dictionary', 'notifyService', '$state'];
+    componentHelper.$inject = ['Dictionary'];
 
-    function componentHelper(Dictionary, notifyService, $state) {
+    function componentHelper(Dictionary) {
         var loadExperimentsPromise;
         var experimentsPromise;
 
         return {
-            onAddLinkedExperiment: onAddLinkedExperiment,
-            getExperiments: getExperiments,
-            onLinkedExperimentClick: onLinkedExperimentClick
+            getExperiments: getExperiments
         };
-
-        function onAddLinkedExperiment(tag) {
-            return loadExperiments().then(function(experiments) {
-                return _.isObject(_.find(experiments, function(experiment) {
-                    return experiment.name === tag.text;
-                }));
-            });
-        }
 
         function loadExperiments() {
             if (loadExperimentsPromise) {
@@ -42,24 +32,6 @@
             experimentsPromise = loadExperiments();
 
             return experimentsPromise;
-        }
-
-        function onLinkedExperimentClick(tag) {
-            return loadExperiments().then(function(experiments) {
-                var foundExperiment = _.find(experiments, function(experiment) {
-                    return experiment.name === tag.text;
-                });
-                if (!foundExperiment) {
-                    notifyService.error('Can not find a experiment with the name: ' + tag.text);
-
-                    return;
-                }
-                $state.go('entities.experiment-detail', {
-                    experimentId: foundExperiment.id,
-                    notebookId: foundExperiment.notebookId,
-                    projectId: foundExperiment.projectId
-                });
-            });
         }
     }
 })();
