50afce4e35d399bfc75306f502481f1ab9c65d62
stepan_orlov <stepan_orlov@epam.com>
2017-11-10 18:01:49 +0300

    changed favicon

--- webpack/copy.js
+++ webpack/copy.js
@@ -6,6 +6,10 @@ function copy(DIRS) {
             context: path.join(DIRS.src, 'vendors/ketcher'),
             from: '**/*',
             to: path.join(DIRS.dist, 'vendors/ketcher')
+        },
+        {
+            from: path.join(DIRS.src, 'assets/images/favicon.ico'),
+            to: path.join(DIRS.dist, 'assets/images')
         }
     ];
 }
