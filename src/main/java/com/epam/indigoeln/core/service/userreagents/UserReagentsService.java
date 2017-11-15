77df8ef1e6a9da880c2a0f5a16729a2dba32a8dd
Zemfira Marganova <Zemfira_Marganova@epam.com>
2017-11-15 16:10:52 +0300

    EPMLSOPELN-706 Added javadoc for UserReagentsService

--- src/main/java/com/epam/indigoeln/core/service/userreagents/UserReagentsService.java
+++ src/main/java/com/epam/indigoeln/core/service/userreagents/UserReagentsService.java
@@ -16,11 +16,24 @@ public class UserReagentsService {
     @Autowired
     private UserReagentsRepository userReagentsRepository;
 
+    /**
+     * Retrieves user reagents from DB for this user if user exists
+     *
+     * @param user User for whom the reagents will be retrieved
+     * @return Reagents for this user if user exists and {@code null} otherwise.
+     */
     public BasicDBList getUserReagents(User user) {
         final UserReagents userReagents = userReagentsRepository.findByUser(user);
         return userReagents == null ? null : userReagents.getReagents();
     }
 
+    /**
+     * Saves user reagents. Updates reagents if regents exists for this user or
+     * creates new reagents if there are no reagents for this user.
+     *
+     * @param user User for whom the reagents will be saved
+     * @param reagents Reagents to save
+     */
     public void saveUserReagents(User user, BasicDBList reagents) {
         UserReagents userReagents = userReagentsRepository.findByUser(user);
         if (userReagents == null) {
@@ -30,5 +43,4 @@ public class UserReagentsService {
         userReagents.setReagents(reagents);
         userReagentsRepository.save(userReagents);
     }
-
 }
