64d352a9fdbe29da6fe10c51e4fc71e358d36609
Elena_Fedorovskaia <elena_fedorovskaia@epam.com>
2017-11-15 15:37:50 +0300

    [EPMLSOPELN-706] - Added javadoc to AlreadyInUseException and ChildReferenceException

--- src/main/java/com/epam/indigoeln/core/service/exception/ChildReferenceException.java
+++ src/main/java/com/epam/indigoeln/core/service/exception/ChildReferenceException.java
@@ -5,9 +5,16 @@ import com.epam.indigoeln.web.rest.errors.CustomParametrizedException;
 
 /**
  * Custom exception, which displays that parent Entity has child reference.
+ *
+ * @author Timofey Zubkov
  */
 public class ChildReferenceException extends CustomParametrizedException {
 
+    /**
+     * Creates instance of ChildReferenceException with parent's id
+     *
+     * @param parentId parent's identifier
+     */
     public ChildReferenceException(String parentId) {
         super("The parent with id = " + parentId + " has child reference", parentId);
     }
--- src/main/java/com/epam/indigoeln/core/service/exception/ConcurrencyException.java
+++ src/main/java/com/epam/indigoeln/core/service/exception/ConcurrencyException.java
@@ -7,24 +7,32 @@ import com.epam.indigoeln.web.rest.errors.CustomParametrizedException;
  */
 public class ConcurrencyException extends CustomParametrizedException {
 
-    public ConcurrencyException(String message, Exception e, String params) {
+    private static final String EXCEPTION_MESSAGE = " already exists. Please reopen it.";
+
+    private ConcurrencyException(String message, Exception e, String params) {
         super(message, e, params);
     }
 
+    /**
+     * Creates instance of ConcurrencyException with project name
+     * @param projectName
+     * @param e
+     * @return
+     */
     public static ConcurrencyException createWithProjectName(String projectName, Exception e) {
-        return new ConcurrencyException("Newer version of project " + projectName + " already exists. Please reopen it.", e, projectName);
+        return new ConcurrencyException("Newer version of project " + projectName + EXCEPTION_MESSAGE, e, projectName);
     }
 
     public static ConcurrencyException createWithTemplateName(String templateName, Exception e) {
-        return new ConcurrencyException("Newer version of template " + templateName + " already exists. Please reopen it.", e, templateName);
+        return new ConcurrencyException("Newer version of template " + templateName + EXCEPTION_MESSAGE, e, templateName);
     }
 
     public static ConcurrencyException createWithNotebookName(String notebookName, Exception e) {
-        return new ConcurrencyException("Newer version of notebook " + notebookName + " already exists. Please reopen it.", e, notebookName);
+        return new ConcurrencyException("Newer version of notebook " + notebookName + EXCEPTION_MESSAGE, e, notebookName);
     }
 
     public static ConcurrencyException createWithExperimentName(String experimentName, Exception e) {
-        return new ConcurrencyException("Newer version of experiment " + experimentName + " already exists. Please reopen it.", e, experimentName);
+        return new ConcurrencyException("Newer version of experiment " + experimentName + EXCEPTION_MESSAGE, e, experimentName);
     }
 
 }
