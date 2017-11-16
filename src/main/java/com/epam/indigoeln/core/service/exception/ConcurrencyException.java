4f76ff622f0394f148f7cdf73e6e1a485cf0bf66
Elena_Fedorovskaia <elena_fedorovskaia@epam.com>
2017-11-16 12:14:49 +0300

    [EPMLSOPELN-706] - Added javadoc to ConcurrencyException, DocumentUploadException, DuplicateFieldException

--- src/main/java/com/epam/indigoeln/core/service/exception/ConcurrencyException.java
+++ src/main/java/com/epam/indigoeln/core/service/exception/ConcurrencyException.java
@@ -4,6 +4,8 @@ import com.epam.indigoeln.web.rest.errors.CustomParametrizedException;
 
 /**
  * Custom exception which displays that newer version of entity already exists.
+ *
+ * @author Evgeny Romanov
  */
 public class ConcurrencyException extends CustomParametrizedException {
 
@@ -14,23 +16,45 @@ public class ConcurrencyException extends CustomParametrizedException {
     }
 
     /**
-     * Creates instance of ConcurrencyException with project name
-     * @param projectName
-     * @param e
-     * @return
+     * Creates instance of ConcurrencyException with project's name
+     *
+     * @param projectName Project's name
+     * @param e           Exception
+     * @return Instance of ConcurrencyException with project name
      */
     public static ConcurrencyException createWithProjectName(String projectName, Exception e) {
         return new ConcurrencyException("Newer version of project " + projectName + EXCEPTION_MESSAGE, e, projectName);
     }
 
+    /**
+     * Creates instance of ConcurrencyException with template's name
+     *
+     * @param templateName Template's name
+     * @param e            Exception
+     * @return Instance of ConcurrencyException with template's name
+     */
     public static ConcurrencyException createWithTemplateName(String templateName, Exception e) {
         return new ConcurrencyException("Newer version of template " + templateName + EXCEPTION_MESSAGE, e, templateName);
     }
 
+    /**
+     * Creates instance of ConcurrencyException with notebook's name
+     *
+     * @param notebookName Notebook's name
+     * @param e            Exception
+     * @return Instance of ConcurrencyException with notebook's name
+     */
     public static ConcurrencyException createWithNotebookName(String notebookName, Exception e) {
         return new ConcurrencyException("Newer version of notebook " + notebookName + EXCEPTION_MESSAGE, e, notebookName);
     }
 
+    /**
+     * Creates instance of ConcurrencyException with experiment's name
+     *
+     * @param experimentName Experiment's name
+     * @param e              Exception
+     * @return Instance of ConcurrencyException with experiment's name
+     */
     public static ConcurrencyException createWithExperimentName(String experimentName, Exception e) {
         return new ConcurrencyException("Newer version of experiment " + experimentName + EXCEPTION_MESSAGE, e, experimentName);
     }
--- src/main/java/com/epam/indigoeln/core/service/exception/DocumentUploadException.java
+++ src/main/java/com/epam/indigoeln/core/service/exception/DocumentUploadException.java
@@ -2,16 +2,35 @@ package com.epam.indigoeln.core.service.exception;
 
 import com.epam.indigoeln.web.rest.errors.CustomParametrizedException;
 
+/**
+ * Custom exception which displays that uploading is failed.
+ *
+ * @author Anton Pikhtin
+ */
 public class DocumentUploadException extends CustomParametrizedException {
 
-    public DocumentUploadException(String message, String params) {
+    private DocumentUploadException(String message, String params) {
         super(message, params);
     }
 
+    /**
+     * Creates instance of DocumentUploadException class with experiment's id
+     * if uploading is failed
+     *
+     * @param experimentId Experiment's identifier
+     * @return Instance of DocumentUploadException class with experiment's id
+     */
     public static DocumentUploadException createFailedUploading(String experimentId) {
         return new DocumentUploadException("Document for the experiment " + experimentId + " failed uploading to signature service.", experimentId);
     }
 
+    /**
+     * Creates instance of DocumentUploadException class with experiment's id
+     * if document for the experiment is missed
+     *
+     * @param experimentId Experiment's identifier
+     * @return Instance of DocumentUploadException class with experiment's id
+     */
     public static DocumentUploadException createNullDocumentId(String experimentId) {
         return new DocumentUploadException("Document for the experiment " + experimentId + " is missed.", experimentId);
     }
