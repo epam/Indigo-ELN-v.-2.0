19973e6e7ad22acc1abbf08ecde04ab8e4d13261
Elena_Fedorovskaia <elena_fedorovskaia@epam.com>
2017-11-17 14:26:25 +0300

    [EPMLSOPELN-706] - Added javadoc to OperationDeniedException and PermissionIncorrectException

--- src/main/java/com/epam/indigoeln/core/service/exception/OperationDeniedException.java
+++ src/main/java/com/epam/indigoeln/core/service/exception/OperationDeniedException.java
@@ -120,48 +120,112 @@ public class OperationDeniedException extends CustomParametrizedException {
         return new OperationDeniedException("The open experiment can't be reopened", entityId);
     }
 
+    /**
+     * Creates instance of OperationDeniedException with entity's id
+     * if user tried to create notebook
+     * @param entityId Entity's identifier
+     * @return Instance of OperationDeniedException with entity's id
+     */
     public static OperationDeniedException createNotebookSubEntityCreateOperation(String entityId) {
         return createSubEntityCreateOperation(entityId, NOTEBOOK_ENTITY_NAME);
     }
 
+    /**
+     * Creates instance of OperationDeniedException with entity's id
+     * if user tried to update project
+     * @param entityId Entity's identifier
+     * @return Instance of OperationDeniedException with entity's id
+     */
     public static OperationDeniedException createProjectUpdateOperation(String entityId) {
         return createEntityUpdateOperation(entityId, PROJECT_ENTITY_NAME);
     }
 
+    /**
+     * Creates instance of OperationDeniedException with entity's id
+     * if user tried to update notebook
+     * @param entityId Entity's identifier
+     * @return Instance of OperationDeniedException with entity's id
+     */
     public static OperationDeniedException createNotebookUpdateOperation(String entityId) {
         return createEntityUpdateOperation(entityId, NOTEBOOK_ENTITY_NAME);
     }
 
+    /**
+     * Creates instance of OperationDeniedException with entity's id
+     * if user tried to update experiment
+     * @param entityId Entity's identifier
+     * @return Instance of OperationDeniedException with entity's id
+     */
     public static OperationDeniedException createExperimentUpdateOperation(String entityId) {
         return createEntityUpdateOperation(entityId, "experiment");
     }
 
+    /**
+     * Creates instance of OperationDeniedException with entity's id and name
+     * if user tried to read sub entities
+     * @param entityId Entity's identifier
+     * @param entityName Entity's name
+     * @return Instance of OperationDeniedException with entity's id and name
+     */
     private static OperationDeniedException createSubEntitiesReadOperation(String entityId, String entityName) {
         return new OperationDeniedException(
                 String.format("The current user doesn't have permissions to read sub-entities of %s with id = %s",
                         entityName, entityId), entityId);
     }
 
+    /**
+     * Creates instance of OperationDeniedException with entity's id and name
+     * if user tried to read entity
+     * @param entityId Entity's identifier
+     * @param entityName Entity's name
+     * @return Instance of OperationDeniedException with entity's id and name
+     */
     private static OperationDeniedException createEntityReadOperation(String entityId, String entityName) {
         return new OperationDeniedException(String.format("The current user doesn't have permissions" +
                 " to read %s with id = %s", entityName, entityId), entityId);
     }
 
+    /**
+     * Creates instance of OperationDeniedException with entity's id and name
+     * if user tried to create sub entity
+     * @param entityId Entity's identifier
+     * @param entityName Entity's name
+     * @return Instance of OperationDeniedException with entity's id and name
+     */
     private static OperationDeniedException createSubEntityCreateOperation(String entityId, String entityName) {
         return new OperationDeniedException(String.format("The current user doesn't have permissions" +
                 " to create sub-entity of %s with id = %s", entityName, entityId), entityId);
     }
 
+    /**
+     * Creates instance of OperationDeniedException with entity's id and name
+     * if user tried to update
+     * @param entityId Entity's identifier
+     * @param entityName Entity's name
+     * @return Instance of OperationDeniedException with entity's id and name
+     */
     private static OperationDeniedException createEntityUpdateOperation(String entityId, String entityName) {
         return new OperationDeniedException(String.format("The current user doesn't have permissions " +
                 "to update %s with id = %s", entityName, entityId), entityId);
     }
 
+    /**
+     * Creates instance of OperationDeniedException with entity's id
+     * if user tried to create version of experiment
+     * @param entityId Entity's identifier
+     * @return Entity's name
+     */
     public static OperationDeniedException createVersionExperimentOperation(String entityId) {
         return new OperationDeniedException("A new version of experiment can't be created" +
                 " while there is open experiment", entityId);
     }
 
+    /**
+     * Creates instance of OperationDeniedException with entity's ids
+     * if user tried to update notebook if there is at least one batch
+     * @param entityIds Entity's identifiers
+     * @return Instance of OperationDeniedException with entity's ids
+     */
     public static OperationDeniedException createNotebookUpdateNameOperation(String... entityIds) {
         return new OperationDeniedException("The notebook can't be updated if there is at least one batch", entityIds);
     }
