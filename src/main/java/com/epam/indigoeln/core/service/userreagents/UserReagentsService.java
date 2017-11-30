9a1cf1a66cbf712f3cbefd0cbd6a1fdbdcdc93ea
Elena_Fedorovskaia <elena_fedorovskaia@epam.com>
2017-11-30 17:16:17 +0300

    dev - Fixed UserReagentsService

--- src/main/java/com/epam/indigoeln/core/service/userreagents/UserReagentsService.java
+++ src/main/java/com/epam/indigoeln/core/service/userreagents/UserReagentsService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
  * Service class for managing user reagents.
  */
 @Service
-public final class UserReagentsService {
+public class UserReagentsService {
 
     @Autowired
     private UserReagentsRepository userReagentsRepository;
