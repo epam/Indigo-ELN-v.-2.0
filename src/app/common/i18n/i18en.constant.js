720628fb7ff05e53f7fd06a18f560c6ecc3a11f3
stepan_orlov <stepan_orlov@epam.com>
2017-12-20 20:21:50 +0300

    EPMLSOPELN-892 replace text NO_DATA_FOUND

--- src/app/common/i18n/i18en.constant.js
+++ src/app/common/i18n/i18en.constant.js
@@ -285,6 +285,7 @@ var i18n = {
     UPPER: 'Upper',
     COMMENTS: 'Comments',
     VALUE: 'Value',
+    NO_DATA_FOUND: 'No data found',
 
     // Form hints
     LOGIN_HINT: 'Login should not contain letters in upper case or special symbols.',
