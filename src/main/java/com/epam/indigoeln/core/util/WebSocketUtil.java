3f3e44ad9814cc9b18457d9df7786fdce4c02c14
Vladislav Alekseev <vladislav_alekseev@epam.com>
2018-02-07 14:13:02 +0300

    EPMLSOPELN-1030 Send WebSocket to UI after request is completed

--- src/main/java/com/epam/indigoeln/core/util/WebSocketUtil.java
+++ src/main/java/com/epam/indigoeln/core/util/WebSocketUtil.java
@@ -4,6 +4,7 @@ import com.epam.indigoeln.core.model.Experiment;
 import com.epam.indigoeln.core.model.Notebook;
 import com.epam.indigoeln.core.model.Project;
 import com.epam.indigoeln.core.model.User;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,8 +20,11 @@ import java.util.Map;
 /**
  * Utility class for getting host name and updating entities.
  */
+@Slf4j
 @Component
 public class WebSocketUtil {
+
+    private static final long SLEEP_MILLIS = 100;
 
     private final SimpMessagingTemplate template;
     private static final String PROJECT_ID = "projectId";
@@ -45,13 +49,15 @@ public class WebSocketUtil {
      * @param project Project
      */
     public void updateProject(User user, Project project) {
-        Map<String, Object> data = new HashMap<>();
-        data.put(USER, user.getId());
-        Map<String, Object> entity = new HashMap<>();
-        entity.put(PROJECT_ID, project.getId());
-        data.put(ENTITY, entity);
-        data.put(VERSION, project.getVersion());
-        template.convertAndSend(ENTITY_DESTINATION, data);
+        new Thread(new SleepRunnable(() -> {
+            Map<String, Object> data = new HashMap<>();
+            data.put(USER, user.getId());
+            Map<String, Object> entity = new HashMap<>();
+            entity.put(PROJECT_ID, project.getId());
+            data.put(ENTITY, entity);
+            data.put(VERSION, project.getVersion());
+            template.convertAndSend(ENTITY_DESTINATION, data);
+        })).start();
     }
 
     /**
@@ -62,14 +68,16 @@ public class WebSocketUtil {
      * @param notebook  Notebook
      */
     public void updateNotebook(User user, String projectId, Notebook notebook) {
-        Map<String, Object> data = new HashMap<>();
-        data.put(USER, user.getId());
-        Map<String, Object> entity = new HashMap<>();
-        entity.put(PROJECT_ID, projectId);
-        entity.put(NOTEBOOK_ID, SequenceIdUtil.extractShortId(notebook));
-        data.put(ENTITY, entity);
-        data.put(VERSION, notebook.getVersion());
-        template.convertAndSend(ENTITY_DESTINATION, data);
+        new Thread(new SleepRunnable(() -> {
+            Map<String, Object> data = new HashMap<>();
+            data.put(USER, user.getId());
+            Map<String, Object> entity = new HashMap<>();
+            entity.put(PROJECT_ID, projectId);
+            entity.put(NOTEBOOK_ID, SequenceIdUtil.extractShortId(notebook));
+            data.put(ENTITY, entity);
+            data.put(VERSION, notebook.getVersion());
+            template.convertAndSend(ENTITY_DESTINATION, data);
+        })).start();
     }
 
     /**
@@ -81,15 +89,17 @@ public class WebSocketUtil {
      * @param experiment Experiment
      */
     public void updateExperiment(User user, String projectId, String notebookId, Experiment experiment) {
-        Map<String, Object> data = new HashMap<>();
-        data.put(USER, user.getId());
-        Map<String, Object> entity = new HashMap<>();
-        entity.put(PROJECT_ID, projectId);
-        entity.put(NOTEBOOK_ID, notebookId);
-        entity.put(EXPERIMENT_ID, SequenceIdUtil.extractShortId(experiment));
-        data.put(ENTITY, entity);
-        data.put(VERSION, experiment.getVersion());
-        template.convertAndSend(ENTITY_DESTINATION, data);
+        new Thread(new SleepRunnable(() -> {
+            Map<String, Object> data = new HashMap<>();
+            data.put(USER, user.getId());
+            Map<String, Object> entity = new HashMap<>();
+            entity.put(PROJECT_ID, projectId);
+            entity.put(NOTEBOOK_ID, notebookId);
+            entity.put(EXPERIMENT_ID, SequenceIdUtil.extractShortId(experiment));
+            data.put(ENTITY, entity);
+            data.put(VERSION, experiment.getVersion());
+            template.convertAndSend(ENTITY_DESTINATION, data);
+        })).start();
     }
 
     /**
@@ -98,8 +108,30 @@ public class WebSocketUtil {
      * @param login User's login
      */
     public void updateUser(String login) {
-        template.convertAndSendToUser(login, USER_DESTINATION,
-                Collections.singletonMap("message", "User's permissions were changed. User should relogin"));
+        new Thread(new SleepRunnable(() -> {
+            template.convertAndSendToUser(login, USER_DESTINATION,
+                    Collections.singletonMap("message", "User's permissions were changed. User should relogin"));
+        })).start();
+    }
+
+    private static class SleepRunnable implements Runnable {
+
+        private final Runnable runnable;
+
+        private SleepRunnable(Runnable runnable) {
+            this.runnable = runnable;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(SLEEP_MILLIS);
+            } catch (InterruptedException e) {
+                log.warn("Thread sleep was interrupted: " + e.getMessage());
+                Thread.currentThread().interrupt();
+            }
+            runnable.run();
+        }
     }
 
     /**
