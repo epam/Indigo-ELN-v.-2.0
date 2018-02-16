0efb8ba4f799c2ef98fc681ecc65b125f69ee743
Alexey Arsenyev <alexey_arsenyev@epam.com>
2018-02-16 14:00:29 +0300

    EPMLSOPELN-1053 Correct make up on "session expired" notification

--- src/app/common/components/timer/timer-dialog.controller.js
+++ src/app/common/components/timer/timer-dialog.controller.js
@@ -2,13 +2,9 @@
 function CountdownDialogController(countdown, idleTime) {
     var vm = this;
 
-    $onInit();
-
-    function $onInit() {
-        vm.countdown = countdown;
-        vm.countdownMax = countdown;
-        vm.idleTime = idleTime;
-    }
+    vm.countdown = countdown;
+    vm.countdownMax = countdown;
+    vm.idleTime = idleTime;
 }
 
 module.exports = CountdownDialogController;
