e2263fa7b095a03d2361877535b3df4ce161839b
Kalyani Rajawat <kalyani_rajawat@epam.com>
2025-03-13 20:16:41 +0530

    added ng content instead of innerhtml and removed items static from here

--- indigo-frontend/src/core/components/common/accordion/accordion.component.ts
+++ indigo-frontend/src/core/components/common/accordion/accordion.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import {CdkAccordionModule} from '@angular/cdk/accordion';
+import { Component, ContentChild, ElementRef, TemplateRef } from '@angular/core';
+import { CdkAccordionModule } from '@angular/cdk/accordion';
 
 @Component({
   selector: 'app-accordion',
@@ -7,8 +7,6 @@ import {CdkAccordionModule} from '@angular/cdk/accordion';
   templateUrl: './accordion.component.html'
 })
 export class AccordionComponent {
-  items = [{title: 'Item 1', description: '<table><thead><tr><th>Header 1</th><th>Header 2</th><th>Header 3</th></tr></thead><tbody><tr><td>Data 1</td><td>Data 2</td><td>Data 3</td></tr></tbody></table>'},
-    {title: 'Item 2', description: '<div class="text-lg text-gray-600">Description</div>'}
-  ];
-  expandedIndex = 0;
+  @ContentChild('header') header: TemplateRef<unknown> | null = null;
+  @ContentChild('body') body: TemplateRef<unknown> | null = null;
 }
