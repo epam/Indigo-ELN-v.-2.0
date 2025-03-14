6278930314d60163d78bf990a50d8a12ba3b23f3
Evgeniia Peregudova <evgeniia_peregudova@epam.com>
2025-03-14 14:38:39 +0200

    feat: add suffix component

---
+++ indigo-frontend/src/core/components/common/input-suffix/input-suffix.component.ts
@@ -0,0 +1,95 @@
+import { Component, forwardRef, input, output, signal } from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormControl,
+  FormGroup,
+  NG_VALUE_ACCESSOR,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { SelectComponent } from '../select/select.component';
+import { InputComponent } from '../input/input.component';
+import { DropdownMenuItem } from '../dropdown-menu/dropdown-menu.i';
+import { ClassPickerPipe } from '../../../pipes/classPicker.pipe';
+import { InputSuffixValue } from '../../../types/input-suffix.i';
+
+@Component({
+  selector: 'app-input-suffix',
+  templateUrl: './input-suffix.component.html',
+  standalone: true,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputSuffixComponent),
+      multi: true,
+    },
+  ],
+  imports: [
+    SelectComponent,
+    InputComponent,
+    ClassPickerPipe,
+    ReactiveFormsModule,
+  ],
+})
+export class InputSuffixComponent implements ControlValueAccessor {
+  label = input.required<string>();
+  placeholder = input<[string, string]>();
+  required = input<boolean>(false);
+  hasError = input<boolean>(false);
+  items = input<DropdownMenuItem[]>([]);
+  valueChange = output<InputSuffixValue>();
+  value = signal<InputSuffixValue>({
+    suffix: '',
+    input: '',
+  });
+  disabled = signal<boolean>(false);
+
+  formGroup!: FormGroup;
+
+  ngOnInit() {
+    this.formGroup = new FormGroup({
+      suffix: new FormControl({
+        value: this.value().suffix,
+        disabled: this.disabled(),
+      }),
+      input: new FormControl({
+        value: this.value().input,
+        disabled: this.disabled(),
+      }),
+    });
+  }
+
+  onChange = (value: InputSuffixValue) => {
+    this.value.set(value);
+  };
+
+  onTouched = () => {};
+
+  writeValue(value: InputSuffixValue): void {
+    this.value.set(value);
+    this.formGroup.patchValue({ suffix: value.suffix, input: value.input });
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled.set(isDisabled);
+    if (isDisabled) {
+      this.formGroup.get('suffix')?.disable();
+      this.formGroup.get('input')?.disable();
+    }
+  }
+
+  getPlaceholder(item: 'suffix' | 'input') {
+    const placeholderTuple = this.placeholder();
+    if (placeholderTuple?.length) {
+      item === 'suffix' ? placeholderTuple[0] : placeholderTuple[1];
+    }
+    return item === 'suffix' ? 'Select an option' : this.label();
+  }
+}
