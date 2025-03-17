e3818df675a17518239c9452b1bcd0f55d233d56
Serhat Kaya <serhatkayadev@gmail.com>
2025-03-17 16:17:30 +0300

    fix: format apiservice

--- indigo-frontend/src/core/services/api.service.ts
+++ indigo-frontend/src/core/services/api.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
   private readonly headers = new HttpHeaders({
@@ -15,40 +15,79 @@ export class ApiService {
   constructor(private readonly httpClient: HttpClient) {}
 
   // POST
-  post<ResponseType>(url: string, body: any, options: { headers?: HttpHeaders | { [header: string]: string | string[] } } = {}): Observable<ResponseType> {
+  post<ResponseType>(
+    url: string,
+    body: any,
+    options: {
+      headers?: HttpHeaders | Record<string, string | string[]>;
+    } = {},
+  ): Observable<ResponseType> {
     const mergedHeaders = this.mergeHeaders(options.headers);
-    return this.httpClient.post<ResponseType>(url, body, { headers: mergedHeaders });
+    return this.httpClient.post<ResponseType>(url, body, {
+      headers: mergedHeaders,
+    });
   }
 
   // GET
-  get<ResponseType>(url: string, params?: any, options: { headers?: HttpHeaders | { [header: string]: string | string[] } } = {}): Observable<ResponseType> {
+  get<ResponseType>(
+    url: string,
+    params?: any,
+    options: {
+      headers?: HttpHeaders | Record<string, string | string[]>;
+    } = {},
+  ): Observable<ResponseType> {
     const mergedHeaders = this.mergeHeaders(options.headers);
     const httpParams = this.createHttpParams(params);
-    return this.httpClient.get<ResponseType>(url, { headers: mergedHeaders, params: httpParams });
+    return this.httpClient.get<ResponseType>(url, {
+      headers: mergedHeaders,
+      params: httpParams,
+    });
   }
 
   // PUT
-  put<ResponseType>(url: string, body: any, options: { headers?: HttpHeaders | { [header: string]: string | string[] } } = {}): Observable<ResponseType> {
+  put<ResponseType>(
+    url: string,
+    body: any,
+    options: {
+      headers?: HttpHeaders | Record<string, string | string[]>;
+    } = {},
+  ): Observable<ResponseType> {
     const mergedHeaders = this.mergeHeaders(options.headers);
-    return this.httpClient.put<ResponseType>(url, body, { headers: mergedHeaders });
+    return this.httpClient.put<ResponseType>(url, body, {
+      headers: mergedHeaders,
+    });
   }
 
   // DELETE
-  delete<ResponseType>(url: string, options: { headers?: HttpHeaders | { [header: string]: string | string[] }; params?: any; } = {}): Observable<ResponseType> {
+  delete<ResponseType>(
+    url: string,
+    options: {
+      headers?: HttpHeaders | Record<string, string | string[]>;
+      params?: any;
+    } = {},
+  ): Observable<ResponseType> {
     const mergedHeaders = this.mergeHeaders(options.headers);
     const httpParams = this.createHttpParams(options.params);
-    return this.httpClient.delete<ResponseType>(url, { headers: mergedHeaders, params: httpParams });
+    return this.httpClient.delete<ResponseType>(url, {
+      headers: mergedHeaders,
+      params: httpParams,
+    });
   }
 
-
-  private mergeHeaders(customHeaders?: HttpHeaders | { [header: string]: string | string[] }): HttpHeaders {
+  private mergeHeaders(
+    customHeaders?: HttpHeaders | Record<string, string | string[]>,
+  ): HttpHeaders {
     let mergedHeaders = this.headers;
     if (customHeaders) {
       if (customHeaders instanceof HttpHeaders) {
-        customHeaders.keys().forEach(key => {
-          mergedHeaders = mergedHeaders.set(key, customHeaders.getAll(key) || []);
+        customHeaders.keys().forEach((key) => {
+          mergedHeaders = mergedHeaders.set(
+            key,
+            customHeaders.getAll(key) || [],
+          );
         });
-      } else { // Handle plain object
+      } else {
+        // Handle plain object
         for (const key in customHeaders) {
           if (customHeaders.hasOwnProperty(key)) {
             mergedHeaders = mergedHeaders.set(key, customHeaders[key]);
@@ -60,17 +99,17 @@ export class ApiService {
   }
 
   private createHttpParams(params?: any): HttpParams {
-      let httpParams = new HttpParams();
-      if (params) {
-          for (const key in params) {
-              if (params.hasOwnProperty(key)) {
-                  const value = params[key];
-                  if (value !== null && value !== undefined) {
-                      httpParams = httpParams.set(key, value);
-                  }
-              }
+    let httpParams = new HttpParams();
+    if (params) {
+      for (const key in params) {
+        if (params.hasOwnProperty(key)) {
+          const value = params[key];
+          if (value !== null && value !== undefined) {
+            httpParams = httpParams.set(key, value);
           }
+        }
       }
-      return httpParams;
+    }
+    return httpParams;
   }
 }
