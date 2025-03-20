755b05857bed76e804b630e0f49357acde9abefe
Serhat Kaya <serhatkayadev@gmail.com>
2025-03-20 11:42:38 +0300

    feat: add url mapping and create func to api service

--- indigo-frontend/src/core/services/api.service.ts
+++ indigo-frontend/src/core/services/api.service.ts
@@ -5,21 +5,53 @@ import { PagedRequest } from '../types/request/paged-request.i';
 import { PaginatedResponse } from '../types/response/paginated-response.i';
 import { getRequestParams } from '../utils/http.util';
 
+interface UrlMapType {
+  getPagedUrl?: string;
+  getSingleUrl?: string;
+  createUrl?: string;
+  updateUrl?: string;
+  deleteUrl?: string;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService<T> {
+  // In order to support some urls like /notebooks/{notebookId}/experiments
+  protected urlMap: UrlMapType = {
+    getPagedUrl: '',
+    getSingleUrl: '',
+    createUrl: '',
+    updateUrl: '',
+    deleteUrl: '',
+  };
+
   controller = 'unknown';
   protected httpClient = inject(HttpClient);
 
-  setup(controller: string) {
+  public setup(controller: string, urlOverrides?: UrlMapType) {
     this.controller = controller;
+    if (urlOverrides) {
+      Object.keys(urlOverrides).forEach(
+        (key) => (this.urlMap[key] = urlOverrides[key]),
+      );
+    }
   }
 
-  getPaged(pager: PagedRequest, filter?): Observable<PaginatedResponse<T>> {
-    return this.httpClient.get<PaginatedResponse<T>>(this.buildUrl(), {
-      params: getRequestParams(filter, pager),
-    });
+  public getPaged(
+    pager: PagedRequest,
+    filter?,
+  ): Observable<PaginatedResponse<T>> {
+    return this.httpClient.get<PaginatedResponse<T>>(
+      this.buildUrl(this.urlMap.getPagedUrl),
+      {
+        params: getRequestParams(filter, pager),
+      },
+    );
+  }
+
+  public create(body: unknown): Observable<T> {
+    return this.httpClient.post<T>(this.buildUrl(this.urlMap.createUrl), body);
   }
 
   private buildUrl = (str?: string) =>
