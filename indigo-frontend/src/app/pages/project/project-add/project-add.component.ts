669bbe703ab1011c8fd4c0dde8cf97fb6a4ace87
Serhat Kaya <serhatkayadev@gmail.com>
2025-05-08 17:20:16 +0300

    feat(129): Update project component prefixes and enhance project overview widget
    - Changed Angular prefix from "app" to "eln" in angular.json and relevant components.
    - Introduced project overview widget directive and service for dynamic template management.
    - Updated project-info and project-list components to utilize new widget structure.
    - Enhanced project-list component to open project-add modal on button click.

--- indigo-frontend/src/app/pages/project/project-add/project-add.component.ts
+++ indigo-frontend/src/app/pages/project/project-add/project-add.component.ts
@@ -1,21 +1,37 @@
-import { Component, inject, OnInit, ViewChild } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
 import { ButtonComponent } from '@/core/components/common/button/button.component';
+import { ChipComponent } from '@/core/components/common/chip/chip.component';
 import { FileUploadComponent } from '@/core/components/common/file-upload/file-upload.component';
 import { InputComponent } from '@/core/components/common/input/input.component';
 import { ModalComponent } from '@/core/components/common/modal/modal.component';
 import { ApiService } from '@/core/services/api.service';
-import { ChipComponent } from '@/core/components/common/chip/chip.component';
-import { ProjectRefreshService } from '../project-refresh.service';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 import { catchError, of, switchMap, tap } from 'rxjs';
+import { ProjectRefreshService } from '../project-refresh.service';
 
 @Component({
   standalone: true,
   selector: 'app-project-add',
-  imports: [ModalComponent, ButtonComponent, ChipComponent, InputComponent, MatInputModule, FileUploadComponent, FormsModule, ReactiveFormsModule, CommonModule],
-  templateUrl: './project-add.component.html'
+  imports: [
+    ModalComponent,
+    ButtonComponent,
+    ChipComponent,
+    InputComponent,
+    MatInputModule,
+    FileUploadComponent,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+  ],
+  templateUrl: './project-add.component.html',
 })
 export class ProjectAddComponent<T> implements OnInit {
   @ViewChild('addProjectModal') modalComponent!: ModalComponent;
@@ -25,8 +41,11 @@ export class ProjectAddComponent<T> implements OnInit {
   chips: string[] = [];
   isSubmitted = false;
 
-  constructor(private service: ApiService<T>, private fb: FormBuilder, private projectRefreshService: ProjectRefreshService) {
-  }
+  constructor(
+    private service: ApiService<T>,
+    private fb: FormBuilder,
+    private projectRefreshService: ProjectRefreshService,
+  ) {}
 
   ngOnInit(): void {
     this.service.setup('projects');
@@ -34,14 +53,14 @@ export class ProjectAddComponent<T> implements OnInit {
       name: ['', [Validators.required]],
       keywords: [''],
       literature: [''],
-      description: ['']
+      description: [''],
     });
   }
 
   addChip(event: any) {
     if (this.formGroup.value.keywords.trim() && event.key === 'Enter') {
       this.chips.push(this.formGroup.value.keywords.trim());
-      this.formGroup.patchValue({'keywords': ''});
+      this.formGroup.patchValue({ keywords: '' });
     }
   }
 
@@ -69,43 +88,46 @@ export class ProjectAddComponent<T> implements OnInit {
     this.isSubmitted = true;
     if (this.formGroup.valid) {
       const data = { ...this.formGroup.value, keywords: this.chips };
-  
-      this.service.create(data).pipe(
-        switchMap((res: any) => {
-          if (res.id && this.files.length > 0) {
-            this.service.setup('projects', {
-              createUrl: '{projectId}/attachments'
-            });
-  
-            const formData = new FormData();
-            this.files.forEach((file) => {
-              formData.append('file', file[0]);
-            });
-  
-            return this.service.uploadAttachment(res.id, formData).pipe(
-              tap(() => {
-                this.close('projectAdded');
-                this.projectRefreshService.triggerRefresh();
-              }),
-              catchError((uploadError) => {
-                this.close('projectAdded');
-                this.projectRefreshService.triggerRefresh();
-                alert(uploadError.message);
-                return of(null);
-              })
-            );
-          } else {
-            this.close('projectAdded');
-            this.projectRefreshService.triggerRefresh();
+
+      this.service
+        .create(data)
+        .pipe(
+          switchMap((res: any) => {
+            if (res.id && this.files.length > 0) {
+              this.service.setup('projects', {
+                createUrl: '{projectId}/attachments',
+              });
+
+              const formData = new FormData();
+              this.files.forEach((file) => {
+                formData.append('file', file[0]);
+              });
+
+              return this.service.uploadAttachment(res.id, formData).pipe(
+                tap(() => {
+                  this.close('projectAdded');
+                  this.projectRefreshService.triggerRefresh();
+                }),
+                catchError((uploadError) => {
+                  this.close('projectAdded');
+                  this.projectRefreshService.triggerRefresh();
+                  alert(uploadError.message);
+                  return of(null);
+                }),
+              );
+            } else {
+              this.close('projectAdded');
+              this.projectRefreshService.triggerRefresh();
+              return of(null);
+            }
+          }),
+          catchError((createError) => {
+            this.close('projectAddError');
+            alert(createError.message);
             return of(null);
-          }
-        }),
-        catchError((createError) => {
-          this.close('projectAddError');
-          alert(createError.message);
-          return of(null);
-        })
-      ).subscribe();
+          }),
+        )
+        .subscribe();
     }
   }
 }
