8503df4efb9e08c9dd6d0364a4894a5e640267db
Serhat Kaya <serhatkayadev@gmail.com>
2025-05-09 10:10:36 +0300

    refactor: Remove unused project links and router dependency from ProjectsOverviewWidgetComponent

--- indigo-frontend/src/core/components/project/projects-overview-widget/projects-overview-widget.component.ts
+++ indigo-frontend/src/core/components/project/projects-overview-widget/projects-overview-widget.component.ts
@@ -1,11 +1,8 @@
 import { ClassPickerPipe } from '@/core/pipes/classPicker.pipe';
 import { AsyncPipe, NgIf, NgTemplateOutlet } from '@angular/common';
 import { Component, inject, Input } from '@angular/core';
-import { Router } from '@angular/router';
 import { CardComponent } from '../../common/card/card.component';
 import { ProjectsOverviewWidgetService } from './services/projects-overview-widget.service';
-
-const regex = /^\/projects\/[a-zA-Z0-9]+$/;
 
 interface ProjectsOverviewWidgetData {
   openExperiments: number;
@@ -25,6 +22,7 @@ interface ProjectsOverviewWidgetData {
 })
 export class ProjectsOverviewWidgetComponent {
   public projectsOverviewWidgetService = inject(ProjectsOverviewWidgetService);
+
   @Input() data: ProjectsOverviewWidgetData = {
     openExperiments: 0,
     waitingSignature: 0,
@@ -34,10 +32,4 @@ export class ProjectsOverviewWidgetComponent {
     notebooks: 0,
     experiments: 0,
   };
-
-  constructor(protected router: Router) {}
-
-  get shouldRenderProjectLinks() {
-    return regex.test(this.router.url);
-  }
 }
