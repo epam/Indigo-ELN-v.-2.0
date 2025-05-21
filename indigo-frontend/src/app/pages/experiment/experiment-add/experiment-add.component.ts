1ba24762154f4f18b8173d2b39bb250a22201376
Kalyani Rajawat <kalyani_rajawat@epam.com>
2025-05-21 17:02:57 +0530

    fix: added formly to the Add Experiment form

--- indigo-frontend/src/app/pages/experiment/experiment-add/experiment-add.component.ts
+++ indigo-frontend/src/app/pages/experiment/experiment-add/experiment-add.component.ts
@@ -1,54 +1,38 @@
-import { ButtonComponent } from '@/core/components/common/button/button.component';
-import { ChipComponent } from '@/core/components/common/chip/chip.component';
 import { DropdownMenuItem } from '@/core/components/common/dropdown-menu/dropdown-menu.i';
-import { InputComponent } from '@/core/components/common/input/input.component';
-import { ModalComponent } from '@/core/components/common/modal/modal.component';
-import { SelectComponent } from '@/core/components/common/select/select.component';
+import { FormDialogComponent } from '@/core/components/common/form-dialog/form-dialog.component';
 import { ApiService } from '@/core/services/api.service';
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, viewChild } from '@angular/core';
-import {
-  FormBuilder,
-  FormGroup,
-  FormsModule,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
+import { FormlyFieldConfig } from '@ngx-formly/core';
 import { map, Observable } from 'rxjs';
 
 @Component({
   standalone: true,
   selector: 'app-experiment-add',
   imports: [
-    ModalComponent,
-    ButtonComponent,
-    InputComponent,
-    SelectComponent,
     MatInputModule,
     FormsModule,
     ReactiveFormsModule,
-    ChipComponent,
     CommonModule,
+    FormDialogComponent,
   ],
   templateUrl: './experiment-add.component.html',
 })
 export class ExperimentAddComponent<T> implements OnInit {
-  readonly modalComponent =
-    viewChild.required<ModalComponent>('addExperimentModal');
-  formGroup!: FormGroup;
-  chips: string[] = [];
-  linkedExperiments: string[] = [];
-  batchCreators: string[] = [];
-  isSubmitted = false;
+  projectCodeItem$: Observable<DropdownMenuItem[]>;
+  theraputicItem$: Observable<DropdownMenuItem[]>;
+  dialogRef = inject(MatDialogRef);
   dropdownMenuItem = [
     {
       label: 'Project',
       value: 'project',
     },
   ];
-  projectCodeItem$: Observable<DropdownMenuItem[]>;
-  theraputicItem$: Observable<DropdownMenuItem[]>;
+
+  fields: FormlyFieldConfig[];
 
   constructor(
     private service: ApiService<T>,
@@ -56,76 +40,124 @@ export class ExperimentAddComponent<T> implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.service.setup('projects');
     this.getDictionaries();
-    this.formGroup = this.fb.group({
-      name: ['', [Validators.required]],
-      template: [['', true]],
-      therapeuticArea: [''],
-      contFromRxn: [''],
-      projectCode: [''],
-      contToRxn: [''],
-      coAuthors: [''],
-      literature: [''],
-      projectAlias: [''],
-      linkedExperiments: [''],
-      batchCreators: [''],
-    });
+    this.fields = [
+      {
+        type: 'select',
+        key: 'Select Template',
+        className: 'flex-1',
+        props: {
+          label: 'Select Template',
+          placeholder: 'Select Template',
+          options: [{ value: 'project', label: 'project' }],
+        },
+      },
+      {
+        type: 'input',
+        key: 'Experiment Name',
+        className: 'w-full',
+        props: {
+          label: 'Experiment Name',
+          placeholder: 'Experiment Name',
+          required: true,
+        },
+      },
+      {
+      fieldGroupClassName: 'flex flex-row gap-4',
+      fieldGroup: [
+      {
+        type: 'select',
+        key: 'Therapeutic Area',
+        className: 'w-1/2',
+        props: {
+          label: 'Therapeutic Area',
+          placeholder: 'Text',
+          options: this.theraputicItem$,
+        },
+      },
+      {
+        type: 'select',
+        key: 'Cont. from Rxn',
+        className: 'w-1/2',
+        props: {
+          label: 'Cont. from Rxn',
+          placeholder: 'Text',
+          options: this.dropdownMenuItem,
+        },
+      }
+    ]},
+    {
+      fieldGroupClassName: 'flex flex-row gap-4',
+      fieldGroup: [
+      {
+        type: 'select',
+        key: 'Project Code & Name',
+        className: 'w-1/2',
+        props: {
+          label: 'Project Code & Name',
+          placeholder: 'Text',
+          options: this.projectCodeItem$,
+        },
+      },
+      {
+        type: 'select',
+        key: 'Cont. TO Rxn',
+        className: 'w-1/2',
+        props: {
+          label: 'Cont. TO Rxn',
+          placeholder: 'Text',
+          options: this.dropdownMenuItem,
+        },
+      }
+    ]},
+    {
+      fieldGroupClassName: 'flex flex-row gap-4',
+      fieldGroup: [
+      {
+        type: 'chip-grid',
+        key: 'Batch Creator',
+        className: 'w-1/2',
+        props: {
+          label: 'Batch Creator',
+          placeholder: 'Add Text',
+        },
+      },
+      {
+        type: 'input',
+        key: 'Project Alias Name',
+        className: 'w-1/2',
+        props: {
+          label: 'Project Alias Name',
+          placeholder: 'Text',
+          options: this.dropdownMenuItem,
+        },
+      }
+    ]},
+    {
+      type: 'input',
+      key: 'literature',
+      className: 'flex-1',
+      props: {
+        label: 'Literature',
+        placeholder: 'Literature',
+      },
+    }
+  ];
   }
 
   getDictionaries() {
-    this.service.setup('dictionaries');
     this.theraputicItem$ = this.service
-      .getDictionary<{ id: string; name: string }[]>('THERAPEUTIC_AREA')
+      .getDictionary<
+        { id: string; name: string }[]
+      >('dictionaries/THERAPEUTIC_AREA')
       .pipe(map((items) => items.map((e) => ({ label: e.name, value: e.id }))));
     this.projectCodeItem$ = this.service
-      .getDictionary<{ id: string; name: string }[]>('PROJECT_CODE')
+      .getDictionary<
+        { id: string; name: string }[]
+      >('dictionaries/PROJECT_CODE')
       .pipe(map((items) => items.map((e) => ({ label: e.name, value: e.id }))));
   }
 
-  addChip(event: any, field: 'linkedExperiments' | 'batchCreators') {
-    if (this.formGroup.value[field].trim() && event.key === 'Enter') {
-      this[field].push(this.formGroup.value[field].trim());
-      this.formGroup.controls[field].setValue('');
-    }
-  }
-
-  removeChip(index: number, field: 'linkedExperiments' | 'batchCreators') {
-    this[field].splice(index, 1);
-  }
-
-  async open() {
-    return await this.modalComponent().open();
-  }
-
-  close(reason) {
-    this.formGroup.reset();
-    this.isSubmitted = false;
-    this.linkedExperiments = [];
-    this.batchCreators = [];
-    this.modalComponent().close(reason);
-  }
-
-  createExperiment() {
-    this.isSubmitted = true;
-    if (this.formGroup.valid) {
-      const data = this.formGroup.value;
-      data.linkedExperiments = this.linkedExperiments;
-      data.batchCreators = this.batchCreators;
-      this.service.setup('notebooks', {
-        createUrl: '{notebookId}/experiments',
-      });
-      this.service.create(data).subscribe({
-        next: (res: any) => {
-          if (res?.id) {
-            this.close('experimentAdded');
-          }
-        },
-        error: (error) => {
-          this.close('experimentAddError');
-          alert(error.message); // Display error message to user
-        },
-      });
-    }
+  createExperiment(data: any) {
   }
 }
