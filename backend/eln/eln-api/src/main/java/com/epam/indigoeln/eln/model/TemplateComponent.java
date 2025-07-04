5967ac76e593fee95ffff736188a478eaad7db0b
Denis Dzenskevich <denis.dzenskevich@gmail.com>
2025-07-04 12:21:11 +0300

    Actualize template component list

--- backend/eln/eln-api/src/main/java/com/epam/indigoeln/eln/model/TemplateComponent.java
+++ backend/eln/eln-api/src/main/java/com/epam/indigoeln/eln/model/TemplateComponent.java
@@ -4,24 +4,52 @@ import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 import lombok.Data;
+import lombok.Getter;
 
 @RegisterForReflection
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
 @JsonSubTypes({
-        @JsonSubTypes.Type(value = TemplateComponent.Attachments.class, name = "attachments"),
+        @JsonSubTypes.Type(value = TemplateComponent.ReactionScheme.class, name = "reactionScheme"),
         @JsonSubTypes.Type(value = TemplateComponent.StoichiometryTable.class, name = "stoichiometryTable"),
+        @JsonSubTypes.Type(value = TemplateComponent.Batches.class, name = "batches"),
+        @JsonSubTypes.Type(value = TemplateComponent.Attachments.class, name = "attachments"),
+        @JsonSubTypes.Type(value = TemplateComponent.ExperimentDescription.class, name = "experimentDescription"),
+        @JsonSubTypes.Type(value = TemplateComponent.ExperimentDetails.class, name = "experimentDetails"),
+        @JsonSubTypes.Type(value = TemplateComponent.ConceptDetails.class, name = "conceptDetails"),
 })
-public sealed abstract class TemplateComponent permits
+public sealed interface TemplateComponent permits
+        TemplateComponent.ReactionScheme,
+        TemplateComponent.StoichiometryTable,
+        TemplateComponent.Batches,
         TemplateComponent.Attachments,
-        TemplateComponent.StoichiometryTable
+        TemplateComponent.ExperimentDescription,
+        TemplateComponent.ExperimentDetails,
+        TemplateComponent.ConceptDetails
 {
 
-    public static final class Attachments extends TemplateComponent {
+    final class ReactionScheme implements TemplateComponent {
     }
 
-    public static final class StoichiometryTable extends TemplateComponent {
-        private boolean reactionScheme;
+    @Data
+    final class StoichiometryTable implements TemplateComponent {
         private boolean reactantsReagentsSolvents;
         private boolean reactionProduct;
+    }
+
+    // preferred compound details - for later
+
+    final class Batches implements TemplateComponent {
+    }
+
+    final class Attachments implements TemplateComponent {
+    }
+
+    final class ExperimentDescription implements TemplateComponent {
+    }
+
+    final class ExperimentDetails implements TemplateComponent {
+    }
+
+    final class ConceptDetails implements TemplateComponent {
     }
 }
