e110f94f681ede9f93ff163febf51865fefcde8f
Denis Dzenskevich <denis.dzenskevich@gmail.com>
2025-07-08 23:03:29 +0300

    Fix integration tests

--- backend/eln/eln-core/src/main/java/com/epam/indigoeln/compound/config/IndigoProvider.java
+++ backend/eln/eln-core/src/main/java/com/epam/indigoeln/compound/config/IndigoProvider.java
@@ -4,15 +4,15 @@ import com.epam.indigo.Indigo;
 import com.epam.indigo.IndigoRenderer;
 import com.epam.indigoeln.indigowrapper.IndigoAPI;
 import com.epam.indigoeln.indigowrapper.IndigoRendererAPI;
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Produces;
 
 @ApplicationScoped
 public class IndigoProvider {
 
-    @Produces
-    @ApplicationScoped
-    Indigo getIndigo() {
+    private final Supplier<Indigo> indigoSupplier = Suppliers.memoize(() -> {
         String path = System.getenv("NATIVE_LIB_PATH");
         if (path == null) {
             throw new RuntimeException("NATIVE_LIB_PATH not defined");
@@ -20,17 +20,18 @@ public class IndigoProvider {
         Indigo indigo = new Indigo(path);
         indigo.setOption("ignore-stereochemistry-errors", "true");
         return indigo;
+    });
+
+    @Produces
+    @ApplicationScoped
+    IndigoAPI getIndigoAPI() {
+        return new IndigoAPI(indigoSupplier.get());
     }
 
     @Produces
     @ApplicationScoped
-    IndigoAPI getIndigoAPI(Indigo indigo) {
-        return new IndigoAPI(indigo);
-    }
-
-    @Produces
-    @ApplicationScoped
-    IndigoRendererAPI getIndigoRendererAPI(Indigo indigo) {
+    IndigoRendererAPI getIndigoRendererAPI() {
+        Indigo indigo = indigoSupplier.get();
         IndigoRenderer indigoRenderer = new IndigoRenderer(indigo);
         indigo.setOption("render-label-mode", "hetero");
         indigo.setOption("render-output-format", "svg");
