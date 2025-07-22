f43af668a1723f87f830c487e575aef6734ddbf2
Denis Dzenskevich <denis.dzenskevich@gmail.com>
2025-07-22 16:11:43 +0300

    Fix lambda networking and enable Cognito integration

--- backend/eln-quarkus-extension/deployment/src/main/java/com/epam/indigoeln/quarkusextension/OverrideIPV4Processor.java
+++ backend/eln-quarkus-extension/deployment/src/main/java/com/epam/indigoeln/quarkusextension/OverrideIPV4Processor.java
@@ -7,6 +7,9 @@ import io.quarkus.deployment.builditem.SystemPropertyBuildItem;
 import io.quarkus.deployment.pkg.steps.NativeBuild;
 import jakarta.annotation.Priority;
 
+// The point of this extension is to override the default IPv4-only set by io.quarkus:quarkus-amazon-lambda-common.
+// Which likely enforces IPv4-only because previosly AWS Lambda did not support IPv6.
+// However, it works fine now, and we need IPv6 to be able to use Egress-only internet gateway instead of NAT gateway.
 public class OverrideIPV4Processor {
 
     private static final String FEATURE = "override-ipv4";
@@ -20,13 +23,11 @@ public class OverrideIPV4Processor {
     @BuildStep(onlyIf = NativeBuild.class)
     void overrideIpv4Only(BuildProducer<SystemPropertyBuildItem> systemProperty) {
         systemProperty.produce(new SystemPropertyBuildItem("java.net.preferIPv4Stack", "false"));
-        systemProperty.produce(new SystemPropertyBuildItem("java.net.preferIPv4Stack1", "false"));
     }
 
     @Priority(Integer.MAX_VALUE)
     @BuildStep(onlyIf = NativeBuild.class)
     void override2Ipv4Only(BuildProducer<SystemPropertyBuildItem> systemProperty) {
         systemProperty.produce(new SystemPropertyBuildItem("java.net.preferIPv4Stack", "false"));
-        systemProperty.produce(new SystemPropertyBuildItem("java.net.preferIPv4Stack1", "false"));
     }
 }
