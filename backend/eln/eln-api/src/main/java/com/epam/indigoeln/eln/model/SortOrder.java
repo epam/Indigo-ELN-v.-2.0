cbc02b6b09d3aca3a8942351a5702d4993f6b59b
Mikhail Markau <mikhailmarkau@gmail.com>
2025-08-08 14:14:48 +0300

    Clean SortOrder

--- backend/eln/eln-api/src/main/java/com/epam/indigoeln/eln/model/SortOrder.java
+++ backend/eln/eln-api/src/main/java/com/epam/indigoeln/eln/model/SortOrder.java
@@ -2,15 +2,8 @@ package com.epam.indigoeln.eln.model;
 
 import lombok.Getter;
 
-@Getter
 public enum SortOrder {
-    EARLIEST("Earliest"),
-    LATEST("Latest"),
-    DEFAULT("Default");
-
-    private final String value;
-
-    SortOrder(String value) {
-        this.value = value;
-    }
+    EARLIEST,
+    LATEST,
+    DEFAULT;
 }
