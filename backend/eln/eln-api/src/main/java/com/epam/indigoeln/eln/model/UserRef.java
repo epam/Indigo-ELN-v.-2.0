ac07ff5d392ff6562528aa06e2c82616527dc686
Denis Dzenskevich <denis.dzenskevich@gmail.com>
2025-08-08 08:09:09 +0300

    UserRef.username

--- backend/eln/eln-api/src/main/java/com/epam/indigoeln/eln/model/UserRef.java
+++ backend/eln/eln-api/src/main/java/com/epam/indigoeln/eln/model/UserRef.java
@@ -4,18 +4,19 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Value;
 
 import java.util.UUID;
 
-@Data
-@NoArgsConstructor(onConstructor_ = @JsonCreator)
-@AllArgsConstructor
+@Value
+@AllArgsConstructor(onConstructor_ = @JsonCreator)
 public class UserRef {
 
     @NotNull
     UUID id;
+
+    @NotEmpty
+    String username;
 
     @NotEmpty
     String displayName;
