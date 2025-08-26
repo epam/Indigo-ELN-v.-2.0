24f650d63a852f46152526dfcd11f3353b4d38df
Denis Dzenskevich <denis.dzenskevich@gmail.com>
2025-08-27 01:36:18 +0300

    Attempts to fix JasperReports native

--- backend/eln/eln-core/build.gradle.kts
+++ backend/eln/eln-core/build.gradle.kts
@@ -12,6 +12,7 @@ plugins {
     `eln-conventions`
     id("io.quarkus")
     id("org.flywaydb.flyway") version "11.3.2"
+    id("io.github.f-cramer.jasperreports") version "0.0.4"
 }
 
 dependencies {
@@ -27,7 +28,18 @@ dependencies {
 
     implementation("io.quarkiverse.amazonservices:quarkus-amazon-cognito-user-pools")
     implementation("software.amazon.awssdk:url-connection-client")
-    implementation("io.quarkiverse.jasperreports:quarkus-jasperreports:1.0.7")
+//    implementation("io.quarkiverse.jasperreports:quarkus-jasperreports:1.0.7")
+
+    implementation("net.sf.jasperreports:jasperreports:7.0.3") {
+        exclude(group = "org.apache.xmlgraphics")
+    }
+    implementation("net.sf.jasperreports:jasperreports-pdf:7.0.3") {
+        exclude(group = "org.apache.xmlgraphics")
+    }
+    implementation("io.quarkiverse.openpdf:quarkus-openpdf:3.3.0")
+    implementation("com.ibm.icu:icu4j:77.1") // used by jasperreports
+    jasperreportsClasspath("net.sf.jasperreports:jasperreports-jdt:7.0.3")
+    jasperreportsClasspath("net.sf.jasperreports:jasperreports-json:7.0.3")
 
     testImplementation(project(":common:common-test"))
 
@@ -83,15 +95,25 @@ val copyNativeLibs by tasks.registering(Copy::class) {
     destinationDir = File("${projectDir}/build/nativelibs")
 }
 
-tasks.named("processResources") {
-    dependsOn(copyNativeLibs)
-}
+tasks.named("processResources") { dependsOn(copyNativeLibs) }
+
+tasks.named("jar") { dependsOn("compileAllReports") }
+tasks.named("quarkusDependenciesBuild") { dependsOn("compileAllReports") }
+tasks.named("compileTestJava") { dependsOn("compileAllReports") }
 
 tasks.withType<Test> {
     environment("NATIVE_LIB_PATH", "${projectDir}/build/nativelibs")
+//    jvmArgs("-agentlib:native-image-agent=config-output-dir=${projectDir}/build/native-config")
 }
 
 tasks.withType<io.quarkus.gradle.tasks.QuarkusDev> {
     dependsOn(copyNativeLibs)
     environmentVariables.set(mapOf("NATIVE_LIB_PATH" to "${projectDir}/build/nativelibs"))
+}
+
+jasperreports {
+    classpath.from(configurations.compileClasspath)
+    classpath.from(project.sourceSets.main.get().output)
+    srcDir = file("src/main/reports")
+    outDir = file("build/resources/main/reports")
 }
--- backend/eln/eln-core/src/main/java/com/epam/indigoeln/eln/service/ReportingService.java
+++ backend/eln/eln-core/src/main/java/com/epam/indigoeln/eln/service/ReportingService.java
@@ -4,7 +4,6 @@ import com.epam.indigoeln.common.util.ModelUtil;
 import com.epam.indigoeln.eln.config.DataAccess;
 import com.epam.indigoeln.eln.entity.ExperimentEntity;
 import com.epam.indigoeln.eln.repository.ExperimentRepository;
-import io.quarkiverse.jasperreports.repository.ReadOnlyStreamingService;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
@@ -18,6 +17,7 @@ import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import net.sf.jasperreports.engine.util.JRLoader;
 
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -29,8 +29,8 @@ public class ReportingService {
 
     @Inject
     ExperimentRepository experimentRepository;
-    @Inject
-    ReadOnlyStreamingService readOnlyStreamingService;
+//    @Inject
+//    ReadOnlyStreamingService readOnlyStreamingService;
 
     public Response printReport(UUID experimentId) {
         byte[] bytes = generateExperimentReport(experimentId);
@@ -49,10 +49,16 @@ public class ReportingService {
     @SneakyThrows
     @Transactional(Transactional.TxType.NOT_SUPPORTED)
     byte[] generateReport(List<ExperimentEntity> experiments) {
+//        JasperReport jasperReport = (JasperReport) readOnlyStreamingService.getResource("/reports/ExperimentReport.jasper");
+        InputStream xa = ModelUtil.class.getResourceAsStream("/reports/ExperimentReport.jasper");
+        InputStream xb = ModelUtil.class.getResourceAsStream("/reports/logo_new_blue.png");
+        log.info("!!! /reports/ExperimentReport.jasper = " + xa);
+        log.info("!!! /reports/logo_new_blue.png = " + xb);
         JasperReport jasperReport = (JasperReport) JRLoader.loadObject(ModelUtil.loadResourceAsStream(getClass(), "/reports/ExperimentReport.jasper"));
         JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(experiments);
         Map<String, Object> params = new HashMap<>();
-        JasperPrint jasperPrint = JasperFillManager.getInstance(readOnlyStreamingService.getContext()).fill(jasperReport, params, dataSource);
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, dataSource);
+//                JasperFillManager.getInstance(readOnlyStreamingService.getContext()).fill(jasperReport, params, dataSource);
         return JasperExportManager.exportReportToPdf(jasperPrint);
     }
 }
