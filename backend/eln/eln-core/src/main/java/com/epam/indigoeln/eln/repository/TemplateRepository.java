f3365a4b9749a932cc84ba99c6d948a827b50e7a
Mikhail Markau <mikhailmarkau@gmail.com>
2025-09-17 12:31:37 +0300

    #208: remove existsByName method

--- backend/eln/eln-core/src/main/java/com/epam/indigoeln/eln/repository/TemplateRepository.java
+++ backend/eln/eln-core/src/main/java/com/epam/indigoeln/eln/repository/TemplateRepository.java
@@ -36,14 +36,4 @@ public class TemplateRepository extends BaseRepository<TemplateEntity> {
                 templateMapper::entityToDetailsDTO
         );
     }
-
-    /**
-     * Checks if a template with the given name exists.
-     *
-     * @param name the name of the template.
-     * @return true if a template with the given name exists, false otherwise.
-     */
-    public boolean existsByName(String name) {
-        return find("name", name).firstResultOptional().isPresent();
-    }
 }
