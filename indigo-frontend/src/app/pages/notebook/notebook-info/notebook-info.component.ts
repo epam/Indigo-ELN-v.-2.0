6decd202789f09b42aed94f30cd60c50ee29b62d
Sebastian Montes <sebasmontes0@gmail.com>
2025-09-22 03:45:35 -0500

    feat(263): Manage Notebook Members Implementation (#272)
    * Add team selection for notebooks
    
    * Feedback pt1
    
    * Feedback pt2
    
    * EOL
    
    * Feedback applied

--- indigo-frontend/src/app/pages/notebook/notebook-info/notebook-info.component.ts
+++ indigo-frontend/src/app/pages/notebook/notebook-info/notebook-info.component.ts
@@ -4,15 +4,22 @@ import { NotebookDetail } from '@/core/types/entities/notebook-detail.i';
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { NotebookService } from '../../../../core/services/notebook/notebook.service';
+import { TeamComponent } from '@/core/components/common/team/team.component';
+import { TeamComponentConfig } from '@/core/components/common/team/team.config';
 
 @Component({
     selector: 'eln-notebook-info',
     standalone: true,
-    imports: [CommonModule, ButtonComponent, CardComponent],
+    imports: [CommonModule, ButtonComponent, CardComponent, TeamComponent],
     templateUrl: './notebook-info.component.html',
 })
 export class NotebookInfoComponent {
     private store = inject(NotebookService);
+
+    notebookTeamConfig: TeamComponentConfig = {
+      title: 'Notebook Team',
+      buildAccessEndpoint: (id: string) => `notebooks/${id}/access`,
+    };
 
     get notebook(): NotebookDetail | null {
         return this.store.notebook();
