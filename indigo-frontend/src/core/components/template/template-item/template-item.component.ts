d733b9aab991a02915b185692a515b871f9d1cf4
nshan20 <58071671+nshan20@users.noreply.github.com>
2025-10-13 16:25:58 +0400

    WIP: staged changes before rebase

--- indigo-frontend/src/core/components/template/template-item/template-item.component.ts
+++ indigo-frontend/src/core/components/template/template-item/template-item.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from '@core/components/common/card/card.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { AvatarComponent } from '@core/components/common/avatar/avatar.component';
-import { ItemTemplate, RootTemplate } from '@core/types/entities/template.i';
+import { ItemTemplate } from '@core/types/entities/template.i';
 
 
 @Component({
@@ -20,14 +20,12 @@ import { ItemTemplate, RootTemplate } from '@core/types/entities/template.i';
   ],
   templateUrl: './template-item.component.html',
 })
-export class TemplateItemComponent implements OnInit {
+export class TemplateItemComponent {
 
-  ngOnInit(){
-  }
   mock_users = {
     link: 'assets/avatar1.png'
   }
 
   @Input() template: ItemTemplate;
-  @Input() variant: 'grid' | 'list' = 'grid';
+  variant = 'list';
 }
