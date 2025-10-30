5cc9abdc7044ece6d3dd80679234a338c27c5e64
Denis Dzenskevich <denis.dzenskevich@gmail.com>
2025-10-30 12:18:31 +0300

    #224 Add material from reagent catalog - fix compilation

--- indigo-frontend/src/core/components/experiment/reaction-inputs-table/reaction-inputs-table.component.ts
+++ indigo-frontend/src/core/components/experiment/reaction-inputs-table/reaction-inputs-table.component.ts
@@ -1,7 +1,17 @@
-import { Component, input, computed, inject } from '@angular/core';
-import { Reaction, ReactionInput } from '@core/types/entities/experiments/experiment.i';
-import { ReactionInputRole } from '@core/types/entities/experiments/experiment-shared.i';
-import {  EnteredValue } from '@core/types/entities/values.i';
+import { Component, computed, inject, input } from '@angular/core';
+import {
+  Reaction,
+  ReactionInput,
+} from '@core/types/entities/experiments/experiment.i';
+import {
+  DensityUnit,
+  MolarityUnit,
+  MolUnit,
+  ReactionRole,
+  VolumeUnit,
+  WeightUnit,
+} from '@core/types/entities/experiments/experiment-shared.i';
+import { EnteredValue } from '@core/types/entities/values.i';
 import {
   MatCell,
   MatCellDef,
@@ -43,135 +53,140 @@ export class ReactionInputsTableComponent {
       id: 'compoundId',
       header: 'Compound ID',
       type: 'text',
-      field: (row: ReactionInput) => row.compound.strCode?.stringForm || row.anchor
+      field: (row: ReactionInput) => row.compound.strCode || row.anchor,
     },
     {
       id: 'casNumber',
       header: 'CAS Number',
       type: 'text',
-      field: () => null
+      field: () => null,
     },
     {
       id: 'chemicalName',
       header: 'Chemical Name',
       type: 'text',
-      field: () => null
+      field: () => null,
     },
     {
       id: 'nbkBatch',
       header: 'NBK Batch #',
       type: 'text',
-      field: () => null
+      field: () => null,
     },
     {
       id: 'molWeight',
       header: 'Mol. Weight',
       type: 'text',
-      field: (row: ReactionInput) => row.compound.molWeight?.value
-        ? `${row.compound.molWeight.value} ${row.compound.molWeight?.unit}`
-        : null
+      field: (row: ReactionInput) =>
+        row.compound.molWeight?.value
+          ? `${row.compound.molWeight.value} ${row.compound.molWeight?.unit}`
+          : null,
     },
     {
       id: 'weight',
       header: 'Weight',
       type: 'text',
-      field: (row: ReactionInput) => row.samples[0]?.weight?.value
-        ? `${row.samples[0].weight.value} ${row.samples[0].weight.unit}`
-        : null
+      field: (row: ReactionInput) =>
+        row.samples[0]?.weight?.value
+          ? `${row.samples[0].weight.value} ${row.samples[0].weight.unit}`
+          : null,
     },
     {
       id: 'volume',
       header: 'Volume',
       type: 'text',
-      field: (row: ReactionInput) => row.samples[0]?.volume?.value
-        ? `${row.samples[0].volume.value} ${row.samples[0].volume.unit}`
-        : null
+      field: (row: ReactionInput) =>
+        row.samples[0]?.volume?.value
+          ? `${row.samples[0].volume.value} ${row.samples[0].volume.unit}`
+          : null,
     },
     {
       id: 'mol',
       header: 'Mol',
       type: 'text',
-      field: (row: ReactionInput) => row.mol?.value
-        ? `${row.mol.value} ${row.mol.unit}`
-        : null
+      field: (row: ReactionInput) =>
+        row.mol?.value ? `${row.mol.value} ${row.mol.unit}` : null,
     },
     {
       id: 'eq',
       header: 'EQ',
       type: 'text',
-      field: (row: ReactionInput) => row.eq?.value?.toString()
+      field: (row: ReactionInput) => row.eq?.value?.toString(),
     },
     {
       id: 'limiting',
       header: 'Limiting',
       type: 'text',
-      field: (row: ReactionInput) => row.limiting ? '✓' : null //
+      field: (row: ReactionInput) => (row.limiting ? '✓' : null), //
     },
     {
       id: 'rxnRole',
       header: 'Rxn Role',
       type: 'text',
-      field: (row: ReactionInput) => row.role || 'REACTANT'
+      field: (row: ReactionInput) => row.role || 'REACTANT',
     },
     {
       id: 'density',
       header: 'Density',
       type: 'text',
-      field: (row: ReactionInput) => row.samples[0]?.density?.value
-        ? `${row.samples[0].density.value} ${row.samples[0].density.unit}`
-        : null
+      field: (row: ReactionInput) =>
+        row.samples[0]?.density?.value
+          ? `${row.samples[0].density.value} ${row.samples[0].density.unit}`
+          : null,
     },
     {
       id: 'molarity',
       header: 'Molarity',
       type: 'text',
-      field: (row: ReactionInput) => row.samples[0]?.molarity?.value
-        ? `${row.samples[0].molarity.value} ${row.samples[0].molarity.unit}`
-        : null
+      field: (row: ReactionInput) =>
+        row.samples[0]?.molarity?.value
+          ? `${row.samples[0].molarity.value} ${row.samples[0].molarity.unit}`
+          : null,
     },
     {
       id: 'purity',
       header: 'Purity',
       type: 'text',
-      field: (row: ReactionInput) => row.samples[0]?.purity?.value?.toString()
+      field: (row: ReactionInput) => row.samples[0]?.purity?.value?.toString(),
     },
     {
       id: 'molFormula',
       header: 'Mol Formula',
       type: 'text',
-      field: (row: ReactionInput) => row.compound.formula
+      field: (row: ReactionInput) => row.compound.formula,
     },
     {
       id: 'saltCode',
       header: 'Salt Code',
       type: 'text',
-      field: (row: ReactionInput) => row.compound.saltCode?.code
+      field: (row: ReactionInput) => row.compound.saltCode?.code,
     },
     {
       id: 'saltEQ',
       header: 'Salt EQ',
       type: 'text',
-      field: (row: ReactionInput) => row.compound.saltEQ?.toString()
+      field: (row: ReactionInput) => row.compound.saltEQ?.toString(),
     },
     {
       id: 'hazardComments',
       header: 'Hazard Comments',
       type: 'text',
-      field: (row: ReactionInput) => row.samples[0]?.healthHazard?.map(h => h.name).join(', ')
+      field: (row: ReactionInput) =>
+        row.samples[0]?.healthHazard?.map((h) => h.name).join(', '),
     },
     {
       id: 'comments',
       header: 'Comments',
       type: 'text',
-      field: () => null
+      field: () => null,
     },
   ];
 
-  displayedColumns = this.columns.map(col => col.id);
+  displayedColumns = this.columns.map((col) => col.id);
 
-  setRole(row: ReactionInput, value: ReactionInputRole) {
+  setRole(row: ReactionInput, value: ReactionRole) {
     this.experimentService.mutateModel({
-      type: 'SetInputRole',
+      type: 'SetInputRowRole',
       anchor: row.anchor,
       role: value,
     });
@@ -182,7 +197,7 @@ export class ReactionInputsTableComponent {
       type: 'SetInputWeight',
       anchor: row.samples[0].anchor,
       weight: value?.value,
-      unit: value?.unit,
+      unit: value?.unit as WeightUnit,
     });
   }
 
@@ -191,7 +206,7 @@ export class ReactionInputsTableComponent {
       type: 'SetInputVolume',
       anchor: row.samples[0].anchor,
       volume: value?.value,
-      unit: value?.unit,
+      unit: value?.unit as VolumeUnit,
     });
   }
 
@@ -200,13 +215,13 @@ export class ReactionInputsTableComponent {
       type: 'SetInputMol',
       anchor: row.anchor,
       mol: value?.value,
-      unit: value?.unit,
+      unit: value?.unit as MolUnit,
     });
   }
 
   setEQ(row: ReactionInput, value: EnteredValue) {
     this.experimentService.mutateModel({
-      type: 'SetInputEQ',
+      type: 'SetInputRowEQ',
       anchor: row.anchor,
       eq: value?.value,
     });
@@ -217,7 +232,7 @@ export class ReactionInputsTableComponent {
       type: 'SetInputDensity',
       anchor: row.samples[0].anchor,
       density: value?.value,
-      unit: value?.unit,
+      unit: value?.unit as DensityUnit,
     });
   }
 
@@ -226,7 +241,7 @@ export class ReactionInputsTableComponent {
       type: 'SetInputMolarity',
       anchor: row.samples[0].anchor,
       molarity: value?.value,
-      unit: value?.unit,
+      unit: value?.unit as MolarityUnit,
     });
   }
 
