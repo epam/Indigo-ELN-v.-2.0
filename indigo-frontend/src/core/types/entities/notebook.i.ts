9322f97e9752b3caf420fbb5e5d263b846246e86
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-12-14 21:20:01 +0400

    fix: the improper UI regarding the members amount is properly handled
    In this commit you can see the partially fixed first part of the bug. Now, It properly resolves the API about the users amount and shows the right amount of members with the properly handled logic about how many of them should be displayed and how. But why partially? Because by this API there are some datas which should be added in the future, one of them is the icons url, but now I already added the future proof solution for that. I just need to have some additional communication with our BE team (Serhat specifically) regarding to add the same icon property with the proper naming convention which matches the front-end one in the types file.

--- indigo-frontend/src/core/types/entities/notebook.i.ts
+++ indigo-frontend/src/core/types/entities/notebook.i.ts
@@ -1,9 +1,20 @@
 import { BaseEntity } from './base-entity.i';
 
+interface NotebookAcl {
+  userId: string;
+  displayName: string;
+  level: string;
+  avatarUrl: string;
+}
+
+type NotebookAcls = NotebookAcl[];
 export interface Notebook extends BaseEntity {
   name: string;
   description: string;
   projectId: string;
+
+  aclCount?: number;
+  acl?: NotebookAcls;
 
   [key: string]: any;
 }
