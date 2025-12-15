da0fe8841ff9d8d809655d563a2880eb75ac13db
Denis Dzenskevich <denis.dzenskevich@gmail.com>
2025-12-15 12:22:13 +0300

    Use materialized view for projects/notebooks/experiments

--- backend/database/flyway/src/main/resources/db/migration/V1.0.10__static_object_counters.sql
+++ backend/database/flyway/src/main/resources/db/migration/V1.0.10__static_object_counters.sql
@@ -1,32 +1,25 @@
-CREATE TABLE Total_Counts (
-    id UUID PRIMARY KEY,
-    projects INT NOT NULL,
-    notebooks INT NOT NULL,
-    experiments_by_status Experiment_Count[] NOT NULL
-);
-INSERT INTO Total_Counts VALUES (gen_random_uuid(), -1, -1, '{}');
-
-ALTER TABLE Project ADD COLUMN notebook_count INT NOT NULL DEFAULT 0;
-ALTER TABLE Project ADD COLUMN experiment_count Experiment_Count[] NOT NULL DEFAULT '{}';
-ALTER TABLE Notebook ADD COLUMN experiment_count Experiment_Count[] NOT NULL DEFAULT '{}';
-
-CREATE FUNCTION update_total_counters()
-RETURNS VOID AS $$
-BEGIN
-    UPDATE Total_Counts SET
-        projects = (SELECT COUNT(*) FROM Project),
-        notebooks = (SELECT COUNT(*) FROM Notebook),
-        experiments_by_status = (
+CREATE MATERIALIZED VIEW Total_Counts_View
+AS
+    SELECT
+        (
+            SELECT COUNT(*) FROM Project
+        ) projects,
+        (
+            SELECT COUNT(*) FROM Notebook
+        ) notebooks,
+        (
             SELECT coalesce(array_agg(ROW(status, count_)::Experiment_Count), '{}')
             FROM (
                 SELECT e.status, COUNT(*) count_
                 FROM Experiment e
                 GROUP BY e.status
             ) t
-        )
-    WHERE true;
-END;
-$$ LANGUAGE plpgsql;
+        ) experiments_by_status
+;
+
+ALTER TABLE Project ADD COLUMN notebook_count INT NOT NULL DEFAULT 0;
+ALTER TABLE Project ADD COLUMN experiment_count Experiment_Count[] NOT NULL DEFAULT '{}';
+ALTER TABLE Notebook ADD COLUMN experiment_count Experiment_Count[] NOT NULL DEFAULT '{}';
 
 CREATE FUNCTION update_Project_counters(
     current_project_id UUID
@@ -72,7 +65,7 @@ $$ LANGUAGE plpgsql;
 CREATE FUNCTION update_total_counters_trigger()
 RETURNS TRIGGER AS $$
 BEGIN
-    PERFORM update_total_counters();
+    REFRESH MATERIALIZED VIEW Total_Counts_View;
     RETURN NULL;
 END;
 $$ LANGUAGE plpgsql;
@@ -80,9 +73,8 @@ $$ LANGUAGE plpgsql;
 CREATE FUNCTION update_Project_counters_trigger()
 RETURNS TRIGGER AS $$
 BEGIN
-    PERFORM
-        update_Project_counters(coalesce(NEW.project_id, OLD.project_id)),
-        update_total_counters();
+    PERFORM update_Project_counters(coalesce(NEW.project_id, OLD.project_id));
+    REFRESH MATERIALIZED VIEW Total_Counts_View;
     RETURN NULL;
 END;
 $$ LANGUAGE plpgsql;
@@ -92,8 +84,8 @@ RETURNS TRIGGER AS $$
 BEGIN
     PERFORM
         update_Notebook_counters(coalesce(NEW.notebook_id, OLD.notebook_id)),
-        update_Project_counters(coalesce(NEW.project_id, OLD.project_id)),
-        update_total_counters();
+        update_Project_counters(coalesce(NEW.project_id, OLD.project_id));
+    REFRESH MATERIALIZED VIEW Total_Counts_View;
     RETURN NULL;
 END;
 $$ LANGUAGE plpgsql;
@@ -110,6 +102,5 @@ CREATE OR REPLACE TRIGGER trigger_update_Notebook_experiments_counter
 AFTER INSERT OR DELETE ON Experiment
 FOR EACH ROW EXECUTE FUNCTION update_Notebook_counters_trigger();
 
-SELECT update_total_counters();
 SELECT update_Project_counters(id) FROM Project;
 SELECT update_Notebook_counters(id) FROM Notebook;
--- backend/eln/eln-core/src/main/java/com/epam/indigoeln/eln/entity/TotalCountsEntity.java
+++ backend/eln/eln-core/src/main/java/com/epam/indigoeln/eln/entity/TotalCountsEntity.java
@@ -17,7 +17,7 @@ import java.util.Map;
 @Getter
 @Setter
 @Entity(name = "TotalCounts")
-@Table(name = "Total_Counts")
+@Table(name = "Total_Counts_View")
 public class TotalCountsEntity implements Serializable {
 
     @Id
