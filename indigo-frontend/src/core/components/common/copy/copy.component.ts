b31232f8eb68cbe5f6818367cf779eb77333e8c2
Denis Svetleishii <denissvetleishii@outlook.com>
2025-12-16 11:25:22 +0400

    feat: implement tooltip and toast message

--- indigo-frontend/src/core/components/common/copy/copy.component.ts
+++ indigo-frontend/src/core/components/common/copy/copy.component.ts
@@ -1,13 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'eln-copy',
   templateUrl: './copy.component.html',
+  imports: [MatTooltipModule],
 })
 export class CopyComponent {
   @Input() text = '';
 
+  private snackBar = inject(MatSnackBar);
+
   copy() {
     navigator.clipboard.writeText(this.text);
+    this.snackBar.open('Copied to clipboard', null,
+      {
+        duration: 5000,
+      }
+    );
   }
 }
