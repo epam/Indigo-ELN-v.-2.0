53952abf7731d6bea9effe280ec68ec7e17bb752
giorgi jajanidze <giorgi.jajanidze2003@gmail.com>
2026-01-07 16:35:14 +0400

    fix: change action name and styles according to figma

--- indigo-frontend/src/app/pages/project/project-item/project-item.component.ts
+++ indigo-frontend/src/app/pages/project/project-item/project-item.component.ts
@@ -21,6 +21,7 @@ import { CardComponent } from '@core/components/common/card/card.component';
     RouterLink,
   ],
   templateUrl: './project-item.component.html',
+  styleUrls: ['./project-item.component.scss'],
 })
 export class ProjectItemComponent {
   mock_users = [
