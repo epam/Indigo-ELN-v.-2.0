d8804b1b2d75de4b1561f99ec5b5835210c57c89
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2026-01-12 11:35:58 +0400

    fix: update the permission role

--- indigo-frontend/src/core/components/layout/partials/sidebar/sidebar.component.ts
+++ indigo-frontend/src/core/components/layout/partials/sidebar/sidebar.component.ts
@@ -43,7 +43,7 @@ export class SidebarComponent {
       name: 'Templates',
       icon: 'indicon-layers',
       path: '/templates',
-      requiredPermission: ApplicationPermission.MANAGE_DICTIONARIES,
+      requiredPermission: ApplicationPermission.MANAGE_TEMPLATES,
     },
     {
       name: 'Dictionaries',
