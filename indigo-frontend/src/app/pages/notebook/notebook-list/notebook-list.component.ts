acc70bf6365d4209f5a45266f1b259ce4b0d8603
Tigran Soghomonyan <tigran_soghomonyan@epam.com>
2026-01-14 23:03:53 +0400

    feat: enhance notebook list component with sorting, searching, and view change functionalities

--- indigo-frontend/src/app/pages/notebook/notebook-list/notebook-list.component.ts
+++ indigo-frontend/src/app/pages/notebook/notebook-list/notebook-list.component.ts
@@ -1,5 +1,6 @@
 import { ButtonComponent } from '@core/components/common/button/button.component';
-import { ListHeaderComponent } from '@core/components/common/list-header/list-header.component';
+import { DropdownMenuItem } from '@core/components/common/dropdown-menu/dropdown-menu.i';
+import { ListHeaderComponent, SortChangeEvent } from '@core/components/common/list-header/list-header.component';
 import { InfiniteLoaderComponent } from '@core/components/util/infinite-loader/infinite-loader.component';
 import { InfiniteScrollBase } from '@core/components/util/infinite-scroll.base';
 import { ClassPickerPipe } from '@core/pipes/classPicker.pipe';
@@ -50,14 +51,40 @@ export class NotebookListComponent
   selectedView: 'grid' | 'list' = 'grid';
   private refreshSub!: Subscription;
   projectId: string;
+  headerSortOptions: DropdownMenuItem[] = [];
 
   constructor(activatedRoute: ActivatedRoute) {
     super();
     activatedRoute.parent.params.pipe(take(1)).subscribe((params) => {
       this.projectId = params['id'];
       this.config.loadUrl = `projects/${this.projectId}/notebooks`;
-      this.initialize();
+      this.setup({
+        loadUrl: `projects/${this.projectId}/notebooks`,
+        sortOptions: [
+          {
+            label: 'Sort by: Earliest',
+            value: 'createdAt',
+            defaultOrder: 'asc',
+          },
+          {
+            label: 'Sort by: Latest',
+            value: 'createdAt',
+            defaultOrder: 'desc',
+          },
+        ],
+        defaultSort: {
+          sortBy: 'createdAt',
+          sortOrder: 'asc',
+        },
+      });
+
+      this.headerSortOptions = this.getSortOptions().map((option) => ({
+        label: `${option.label}`,
+        value: `${option.value}:${option.defaultOrder}`,
+        icon: 'indicon-sort',
+      }));
     });
+    this.initialize();
   }
 
   refreshList(): void {
@@ -79,5 +106,23 @@ export class NotebookListComponent
           this.refreshList();
         }
       });
+  }
+
+  onSearch(value: string) {
+    this.search(value);
+  }
+
+  onSortChange(event: SortChangeEvent) {
+    this.sort(event.sortBy, event.sortOrder);
+  }
+
+  onViewChange(view: string) {
+    this.selectedView = view as 'grid' | 'list';
+  }
+
+  onMyEntitiesOnlyChange(value: boolean) {
+    this.filters['createdByMe'] = value;
+    this.dataBh.next([]);
+    this.fetchDataAndUpdateQueryParams();
   }
 }
