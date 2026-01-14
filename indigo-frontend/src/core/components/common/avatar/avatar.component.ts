142f4133f6879386b89db5ca7505aca2563e2a62
giorgi jajanidze <giorgi.jajanidze2003@gmail.com>
2026-01-14 21:33:04 +0400

    fix: rewrote CSS styles into Tailwind

--- indigo-frontend/src/core/components/common/avatar/avatar.component.ts
+++ indigo-frontend/src/core/components/common/avatar/avatar.component.ts
@@ -10,6 +10,5 @@ import { Component, Input } from '@angular/core';
 })
 export class AvatarComponent {
   @Input() img = 'assets/avatar-placeholder.png';
-  // @Input() initials?: string;
   @Input() classList = '';
 }
