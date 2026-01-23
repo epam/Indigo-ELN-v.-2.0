493056a174c6a1f940b5a9dae5a2500d743af718
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2026-01-23 09:48:00 +0400

    fix: update the text color for buttons in the disaled state by adding the proper color in class variance authority configuration

--- indigo-frontend/src/core/components/common/button/button.variant.ts
+++ indigo-frontend/src/core/components/common/button/button.variant.ts
@@ -14,7 +14,7 @@ export const ButtonVariant = cva(
     variants: {
       variant: {
         green: ['bg-green-400', 'hover:bg-green-300'],
-        blue: ['bg-primary-400', 'hover:bg-primary-500', 'text-neutral'],
+        blue: ['bg-primary-400', 'hover:bg-primary-500', 'text-neutral', 'disabled:text-neutral-500'],
         'blue-outline': [
           'bg-neutral',
           'text-primary-400',
