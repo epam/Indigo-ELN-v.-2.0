3e97ac8842a2bc30ba0e0e8de04d049585e08794
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2026-02-16 18:18:50 +0400

    feat: add the scrollbar while the height of field is larger than 2 lines
    As I discussed with Daria (designed) we have to use scroll, because the maximum height of field should be equal to 2 lines.
    
    Also I integrate chips which we use across the app except of some default ngSelect styles

--- indigo-frontend/src/app/pages/experiment/experiment-info/experiment-info.component.ts
+++ indigo-frontend/src/app/pages/experiment/experiment-info/experiment-info.component.ts
@@ -64,7 +64,6 @@ export class ExperimentInfoComponent implements OnInit {
             placeholder: "Experiment Title",
             
           },
-          className: 'flex flex-wrap'
         },
         {
           type: "select-chips",
--- indigo-frontend/src/core/components/formly/fields/select-chips.component.ts
+++ indigo-frontend/src/core/components/formly/fields/select-chips.component.ts
@@ -1,18 +1,31 @@
 import { FieldType, FieldTypeConfig } from "@ngx-formly/core";
 import { Component } from "@angular/core";
-import { NgSelectComponent, NgOptionComponent } from "@ng-select/ng-select";
+import { NgOptionComponent, NgSelectModule } from "@ng-select/ng-select";
+import { ChipComponent } from "../../common/chip/chip.component";
+import { CommonModule } from "@angular/common";
 
 @Component ({
     selector: 'eln-formly-chip-grid',
     standalone: true,
-    imports: [NgSelectComponent, NgOptionComponent],
+    imports: [NgSelectModule, NgOptionComponent, ChipComponent, CommonModule],
     template: `
         <ng-select multiple="true">
             @for (item of items; track item) {
-            <ng-option [value]="item.id">
+            <ng-option [value]="item">
                 {{ item.name }}
             </ng-option>
             } 
+
+            <ng-template ng-multi-label-tmp let-items="items" let-clear="clear">
+                <div class="flex flex-wrap gap-[4px] max-h-[64px] overflow-y-auto">
+                    <eln-chip
+                    *ngFor="let item of items"
+                    [deleteable]="true"
+                    >
+                        {{ item.name }}
+                    </eln-chip>
+                </div>
+            </ng-template>
         </ng-select>
     `
 })
@@ -23,6 +36,10 @@ export class SelectChipsComponent extends FieldType<FieldTypeConfig> {
 		{ id: 2, name: 'option2'},
 		{ id: 3, name: 'option3' },
 		{ id: 4, name: 'option4' },
+        { id: 5, name: 'option5' },
+        { id: 6, name: 'option6' },
+        { id: 7, name: 'option7' },
+        { id: 8, name: 'option8' },
     ]
 
     ngOnInit() {}
