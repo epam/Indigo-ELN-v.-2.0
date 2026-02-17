94e020ca600867b1f8c1a6bec8db8f0885acbd8b
giga16 <92594588+GIGAKLIBADZE@users.noreply.github.com>
2026-02-17 18:21:54 +0400

    #Ng-67: [FE] Implement Experiment Details form (#520)
    * feat: implement the left column layout for the system part
    
    Additionally create the new component for the formly select
    
    * refactor: change the ng-select component with the already implemented eln-dictionary-select component
    
    * feat: add all of the fields with the proper positioning
    
    * feat: create the etire layout for the form and experiment info
    
    * refactor: remove the useles property and fix the property name in formly for 'dictionaryId'. By this hte requests happend properly
    
    * refactor: change the hardcoded values with the real data
    
    * style: add the styles, make the proper positioning
    
    * style: create the .scss file and move the styles which are used in the multiple places into there, fix improper margisn/paddings based on the mockups
    
    * refactor: remove the code block which is not necessary to be the part of 'Experiment info' anymore based on the latest requirement/mockups
    
    * refactor: delete the commented unnecessary code block which should not be part of 'Experiment Info' anymore
    
    * fix: remove the useles code and fix the label names
    
    * fix: update the two files based on the threads
    
    * refactor: instead of using chip or select type components, create the brand new one for the searchable/selectable fields
    
    * fix: restrict width expansion of chip inputs when adding chips
    
    * feat: add the scrollbar while the height of field is larger than 2 lines
    
    As I discussed with Daria (designed) we have to use scroll, because the maximum height of field should be equal to 2 lines.
    
    Also I integrate chips which we use across the app except of some default ngSelect styles
    
    * refactor: replace the ngSelect component with already existing eln-select component
    
    * remove the mock data

--- indigo-frontend/src/app/app.config.ts
+++ indigo-frontend/src/app/app.config.ts
@@ -22,6 +22,8 @@ import { FormlyPresetModule } from '@ngx-formly/core/preset';
 import { FormlyMaterialModule } from '@ngx-formly/material';
 import { FormlyMatDatepickerModule } from '@ngx-formly/material/datepicker';
 import { routes } from './app.routes';
+import { SelectFieldComponent } from '@/core/components/formly/fields/select-field.component';
+import { SelectChipsComponent } from '@/core/components/formly/fields/select-chips.component';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -44,6 +46,16 @@ export const appConfig: ApplicationConfig = {
             component: EditorFormlyFieldComponent,
             wrappers: ['raw'],
           },
+          {
+            name: 'select',
+            component: SelectFieldComponent,
+            wrappers: ['raw'],
+          },
+          {
+            name: 'select-chips',
+            component: SelectChipsComponent,
+            wrappers: ['raw']
+          }
         ],
         validationMessages: [
           {
