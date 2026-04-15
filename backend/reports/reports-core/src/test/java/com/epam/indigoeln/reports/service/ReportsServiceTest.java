e92dd3973b4bd0c76fc305b971270382119462b4
andrzej.radzik@onet.pl <andrzej.radzik@onet.pl>
2026-04-15 14:52:09 +0200

    Removed try-catch and commented out code form ReportsServiceTests

--- backend/reports/reports-core/src/test/java/com/epam/indigoeln/reports/service/ReportsServiceTest.java
+++ backend/reports/reports-core/src/test/java/com/epam/indigoeln/reports/service/ReportsServiceTest.java
@@ -41,33 +41,14 @@ public class ReportsServiceTest extends BaseTest {
         ProjectDTO project = new ProjectDTO();
         project.setName("Demo project");
         ExperimentDetailsDTO experiment = null;
-        try {
-            experiment = objectMapper.readValue(experimentJson, ExperimentDetailsDTO.class);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        /*
-        ExperimentDetailsDTO experiment = new ExperimentDetailsDTO();
-        experiment.setName("00000001-0001");
-        experiment.setCreatedBy(new UserRef(UUID.randomUUID(), "test", "Test User"));
-        experiment.setStatus(ExperimentStatus.OPEN);
-        experiment.setCreatedAt(ZonedDateTime.of(2025, 2, 19, 8, 41, 48, 0, ZoneId.of("UTC")));
-        experiment.setTherapeuticArea(new DictionaryItemRef(UUID.randomUUID(), "Diabet"));
-        experiment.setProjectCode(new DictionaryItemRef(UUID.randomUUID(), "Code 1"));
-        experiment.setDescription("To a suspension of salicylic acid (2 g) in acetic anhydride (4.5 mL) in a conical flask add anhydrous sodium acetate\n(0.4 g) with stirring.");
-        */
+        experiment = objectMapper.readValue(experimentJson, ExperimentDetailsDTO.class);
+
         return List.of(new ReportsAPI.ExperimentReportDataDTO(
                 project,
                 experiment,
                 new String(ModelUtil.loadResource(ReportsServiceTest.class, "/experiment-image.svg"), StandardCharsets.UTF_8),
                 ReagentDTO.allExperimentReagents(experiment.getModel())
         ));
-
-        // Reaction Details, Experiment Subject / Title = O-acetylation of salicylic acid
-//        experiment.setCreatedAt(ZonedDateTime.parse("2025-07-29T13:22:05Z"));
-        // Reaction Details, Therapeutic Area = Diabetes
-        // other Reaction Details fields?
-//        experiment.setPicture(ModelUtil.loadResource(ReportingServiceTest.class, "/experiment-image.svg"));
     }
 
     @Test
