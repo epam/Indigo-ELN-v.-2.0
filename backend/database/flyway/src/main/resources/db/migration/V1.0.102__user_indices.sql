2bb0adebb4ab17013772d8889b5a5cffbc1fa957
andrzej.radzik@onet.pl <andrzej.radzik@onet.pl>
2026-05-14 11:43:53 +0200

    Update user accounts indexes to non unique

--- backend/database/flyway/src/main/resources/db/migration/V1.0.102__user_indices.sql
+++ backend/database/flyway/src/main/resources/db/migration/V1.0.102__user_indices.sql
@@ -1,2 +1,4 @@
-CREATE UNIQUE INDEX ix_user_account_first_name ON User_Account (lower(first_name));
-CREATE UNIQUE INDEX ix_user_account_last_name ON User_Account (lower(last_name));
+DROP INDEX ix_user_account_display_name;
+CREATE INDEX ix_user_account_display_name ON User_Account (lower(display_name));
+CREATE INDEX ix_user_account_first_name ON User_Account (lower(first_name));
+CREATE INDEX ix_user_account_last_name ON User_Account (lower(last_name));
