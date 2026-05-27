7ac2acd5439bcc5eb4e47733a24ad613cc70d155
Aleksandr Amelkovich <aliaksandr_amialkovich@epam.com>
2026-05-27 12:17:27 +0300

    ng-371: update button name (#683)

--- indigo-frontend/src/app/pages/project/project-list/project-list.component.ts
+++ indigo-frontend/src/app/pages/project/project-list/project-list.component.ts
@@ -16,6 +16,7 @@ import { ProjectAddComponent } from '../project-add/project-add.component';
 import { ProjectItemComponent } from '@pages/project/project-item/project-item.component';
 import { ProjectOverviewWidgetDirective } from '@pages/project/projects-overview-widget/directives/project-overview-widget.directive';
 import { BreadcrumbsStateService } from '@core/services/breadcrumbs/breadcrumbs.state.service';
+import { SvgIconComponent } from '@core/components/common/svg-icon/svg-icon.component';
 
 @Component({
   selector: 'eln-project-list',
@@ -39,6 +40,7 @@ import { BreadcrumbsStateService } from '@core/services/breadcrumbs/breadcrumbs.
     ProjectOverviewWidgetDirective,
     ButtonComponent,
     ListHeaderComponent,
+    SvgIconComponent,
   ],
 })
 export class ProjectListComponent extends InfiniteScrollBase<Project> implements OnDestroy {
