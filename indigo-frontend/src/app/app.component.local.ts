070b82a9aa2123f51e373fe0e3505776ae1b1bc4
Leydy Tarazona <leydytarazona700@gmail.com>
2026-07-27 04:22:17 -0500

    feat(737): change title by route (#746)
    * feat(737): change title by route
    
    * feat(737) update current experiment-notebook-project-name in titles

--- indigo-frontend/src/app/app.component.local.ts
+++ indigo-frontend/src/app/app.component.local.ts
@@ -1,5 +1,7 @@
+import { ActivatedRoute, NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'eln-root',
@@ -10,15 +12,32 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent implements OnInit {
   title = 'indigo-frontend';
 
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private titleService: Title,
+  ) {}
+
   /**
    * Initialize Keycloak authentication when the component is loaded.
    */
   async ngOnInit(): Promise<void> {
-    //const authenticated = await provideKeycloak.init();
-    //if (authenticated) {
-    //    console.log('User is authenticated');
-    //} else {
-    //    console.log('User is not authenticated');
-    //}
+    this.setPageTitleOnNavigation();
+  }
+
+  private setPageTitleOnNavigation(): void {
+    this.router.events.pipe(filter((event): event is NavigationEnd => event instanceof NavigationEnd)).subscribe(() => {
+      const title = this.getDeepestRouteTitle(this.activatedRoute) ?? 'IndigoELN';
+      this.titleService.setTitle(title);
+    });
+  }
+
+  private getDeepestRouteTitle(route: ActivatedRoute): string | null {
+    let title = route.snapshot.data?.['title'] ?? null;
+    while (route.firstChild) {
+      route = route.firstChild;
+      title = route.snapshot.data?.['title'] ?? title;
+    }
+    return title;
   }
 }
